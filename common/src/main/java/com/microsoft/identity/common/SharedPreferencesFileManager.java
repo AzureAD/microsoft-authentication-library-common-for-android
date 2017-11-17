bd9ab3b8bc2b4129ce6989944bf3aaf835ac265b
Brian Melton <brianmel@microsoft.com>
2017-11-16 19:02:48 -0500

    CR feedback + tests

--- common/src/main/java/com/microsoft/identity/common/SharedPreferencesFileManager.java
+++ common/src/main/java/com/microsoft/identity/common/SharedPreferencesFileManager.java
@@ -3,39 +3,44 @@ package com.microsoft.identity.common;
 import android.content.Context;
 import android.content.SharedPreferences;
 
+import java.util.Map;
+
 /**
  * Convenience class for accessing {@link SharedPreferences}.
  */
-public class SharedPreferencesHelper {
+public class SharedPreferencesFileManager {
 
+    private final String mSharedPreferencesFileName;
     private final SharedPreferences mSharedPreferences;
 
     /**
-     * Constructs an instance of SharedPreferencesHelper.
+     * Constructs an instance of SharedPreferencesFileManager.
      * The default operating mode is {@link Context#MODE_PRIVATE}
      *
      * @param context Interface to global information about an application environment.
      * @param name    The desired {@link android.content.SharedPreferences} file. It will be created
      *                if it does not exist.
      */
-    public SharedPreferencesHelper(
+    public SharedPreferencesFileManager(
             final Context context,
             final String name) {
+        mSharedPreferencesFileName = name;
         mSharedPreferences = context.getSharedPreferences(name, Context.MODE_PRIVATE);
     }
 
     /**
-     * Constructs an instance of SharedPreferencesHelper.
+     * Constructs an instance of SharedPreferencesFileManager.
      *
      * @param context       Interface to global information about an application enviroment.
      * @param name          The desired {@link SharedPreferences} file. It will be created
      *                      if it does not exist.
      * @param operatingMode Operating mode {@link Context#getSharedPreferences(String, int)}.
      */
-    public SharedPreferencesHelper(
+    public SharedPreferencesFileManager(
             final Context context,
             final String name,
             final int operatingMode) {
+        mSharedPreferencesFileName = name;
         mSharedPreferences = context.getSharedPreferences(name, operatingMode);
     }
 
@@ -44,23 +49,8 @@ public class SharedPreferencesHelper {
      *
      * @param key   The name (key) of the Token to save.
      * @param value The Token's value (as a {@link String}).
-     * @return True, if the value was successfully written.
      */
-    public final boolean saveToken(
-            final String key,
-            final String value) {
-        final SharedPreferences.Editor editor = mSharedPreferences.edit();
-        editor.putString(key, value);
-        return editor.commit();
-    }
-
-    /**
-     * Asynchronously saves a Token (as a {@link String} to the {@link SharedPreferences} file.
-     *
-     * @param key   The name (key) of the Token to save.
-     * @param value The Token's value (as a {@link String}).
-     */
-    public final void saveTokenAsync(
+    public final void putString(
             final String key,
             final String value) {
         final SharedPreferences.Editor editor = mSharedPreferences.edit();
@@ -74,17 +64,56 @@ public class SharedPreferencesHelper {
      * @param key The name (key) of the Token.
      * @return The Token's value or null if no value could be found.
      */
-    public final String getToken(final String key) {
+    public final String getString(final String key) {
         return mSharedPreferences.getString(key, null);
     }
 
     /**
-     * Gets the underlying {@link SharedPreferences} instance.
+     * Returns the name of {@link SharedPreferences} file in use.
      *
-     * @return The SharedPreference instance used by this SharedPreferencesHelper.
+     * @return The name of the file.
      */
-    public final SharedPreferences getSharedPreferences() {
-        return mSharedPreferences;
+    public final String getSharedPreferencesFileName() {
+        return mSharedPreferencesFileName;
+    }
+
+    /**
+     * Retuns all entries in the {@link SharedPreferences} file.
+     *
+     * @return A Map of all entries.
+     */
+    public final Map<String, ?> getAll() {
+        return mSharedPreferences.getAll();
+    }
+
+    /**
+     * Tests if the {@link SharedPreferences} file contains an entry for the supplied key.
+     *
+     * @param key The key to consult.
+     * @return True, if the key has an associate entry.
+     */
+    public final boolean contains(final String key) {
+        return mSharedPreferences.contains(key);
+    }
+
+    /**
+     * Clears the contents of the {@link SharedPreferences} file.
+     */
+    public final void clear() {
+        final SharedPreferences.Editor editor = mSharedPreferences.edit();
+        editor.clear();
+        editor.apply();
+    }
+
+    /**
+     * Removes any associated entry for the supplied key.
+     *
+     * @param key The key whose value should be cleared.
+     */
+    public void remove(final String key) {
+        final SharedPreferences.Editor editor = mSharedPreferences.edit();
+        editor.remove(key);
+        editor.apply();
     }
 
 }
