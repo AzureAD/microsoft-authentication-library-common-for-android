8035489ca5174a2fca211447f4c073cfd697511c
Brian Melton <brianmel@microsoft.com>
2018-03-13 17:54:25 -0400

    Rename pkg 'generated' -> 'dto'

--- common/src/main/java/com/microsoft/identity/common/dto/CredentialType.java
+++ common/src/main/java/com/microsoft/identity/common/dto/CredentialType.java
@@ -21,7 +21,7 @@
 // OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
 // THE SOFTWARE.
 
-package com.microsoft.identity.common.generated;
+package com.microsoft.identity.common.dto;
 
 public enum CredentialType {
     RefreshToken,
