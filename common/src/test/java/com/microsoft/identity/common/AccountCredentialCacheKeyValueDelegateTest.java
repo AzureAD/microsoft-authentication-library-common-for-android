8c7433c74fe1a4fb1a261b2c75078954780fc4bc
Brian Melton <brianmel@microsoft.com>
2018-03-28 17:53:25 -0400

    Adding extra field persistence/restoration tests for Accounts + RefreshTokens

--- common/src/test/java/com/microsoft/identity/common/AccountCredentialCacheKeyValueDelegateTest.java
+++ common/src/test/java/com/microsoft/identity/common/AccountCredentialCacheKeyValueDelegateTest.java
@@ -338,6 +338,104 @@ public class AccountCredentialCacheKeyValueDelegateTest {
         assertEquals(LAST_NAME, jsonObject.getString("last_name"));
         assertEquals(AVATAR_URL, jsonObject.getString("avatar_url"));
     }
+
+    @Test
+    public void accountExtraValueSerialization() throws Exception {
+        final com.microsoft.identity.common.internal.dto.Account account
+                = new com.microsoft.identity.common.internal.dto.Account();
+        account.setUniqueId(UNIQUE_ID);
+        account.setEnvironment(ENVIRONMENT);
+        account.setRealm(REALM);
+        account.setAuthorityAccountId(AUTHORITY_ACCOUNT_ID);
+        account.setAuthorityType(AUTHORITY_TYPE);
+        account.setGuestId(GUEST_ID);
+        account.setFirstName(FIRST_NAME);
+        account.setLastName(LAST_NAME);
+        account.setAvatarUrl(AVATAR_URL);
+
+        final Map<String, JsonElement> additionalFields = new HashMap<>();
+
+        // Add some random Json to this object
+        JsonElement jsonStr = new JsonPrimitive("bar");
+        JsonArray jsonNumberArr = new JsonArray();
+        jsonNumberArr.add(1);
+        jsonNumberArr.add(2);
+        jsonNumberArr.add(3);
+        JsonObject jsonObject = new JsonObject();
+        jsonObject.add("object_key", new JsonPrimitive("object_value"));
+
+        additionalFields.put("foo", jsonStr);
+        additionalFields.put("numbers", jsonNumberArr);
+        additionalFields.put("object", jsonObject);
+
+        account.setAdditionalFields(additionalFields);
+
+        String serializedValue = mDelegate.generateCacheValue(account);
+        JSONObject derivedCacheValueJsonObject = new JSONObject(serializedValue);
+        assertEquals(UNIQUE_ID, derivedCacheValueJsonObject.getString("unique_id"));
+        assertEquals(ENVIRONMENT, derivedCacheValueJsonObject.getString("environment"));
+        assertEquals(REALM, derivedCacheValueJsonObject.getString("realm"));
+        assertEquals(AUTHORITY_ACCOUNT_ID, derivedCacheValueJsonObject.get("authority_account_id"));
+        assertEquals(AUTHORITY_TYPE, derivedCacheValueJsonObject.get("authority_type"));
+        assertEquals(GUEST_ID, derivedCacheValueJsonObject.get("guest_id"));
+        assertEquals(FIRST_NAME, derivedCacheValueJsonObject.get("first_name"));
+        assertEquals(LAST_NAME, derivedCacheValueJsonObject.get("last_name"));
+        assertEquals(AVATAR_URL, derivedCacheValueJsonObject.get("avatar_url"));
+        assertEquals("bar", derivedCacheValueJsonObject.getString("foo"));
+
+        final JSONArray jsonArr = derivedCacheValueJsonObject.getJSONArray("numbers");
+        assertEquals(3, jsonArr.length());
+
+        final JSONObject jsonObj = derivedCacheValueJsonObject.getJSONObject("object");
+        assertEquals("object_value", jsonObj.getString("object_key"));
+    }
+
+    @Test
+    public void accountExtraValueDeserialization() throws Exception {
+        final com.microsoft.identity.common.internal.dto.Account account
+                = new com.microsoft.identity.common.internal.dto.Account();
+        account.setUniqueId(UNIQUE_ID);
+        account.setEnvironment(ENVIRONMENT);
+        account.setRealm(REALM);
+        account.setAuthorityAccountId(AUTHORITY_ACCOUNT_ID);
+        account.setAuthorityType(AUTHORITY_TYPE);
+        account.setGuestId(GUEST_ID);
+        account.setFirstName(FIRST_NAME);
+        account.setLastName(LAST_NAME);
+        account.setAvatarUrl(AVATAR_URL);
+
+        String serializedValue = mDelegate.generateCacheValue(account);
+
+        // Turn the serialized value into a JSONObject and start testing field equality.
+        final JSONObject jsonObject = new JSONObject(serializedValue);
+
+        // Add more non-standard data to this object...
+        final JSONArray numbers = new JSONArray("[1, 2, 3]");
+        final JSONArray objects = new JSONArray("[{\"hello\" : \"hallo\"}, {\"goodbye\" : \"auf wiedersehen\"}]");
+
+        jsonObject.put("foo", "bar");
+        jsonObject.put("numbers", numbers);
+        jsonObject.put("objects", objects);
+
+        serializedValue = jsonObject.toString();
+
+        final com.microsoft.identity.common.internal.dto.Account deserializedValue
+                = mDelegate.fromCacheValue(serializedValue, com.microsoft.identity.common.internal.dto.Account.class);
+        assertNotNull(deserializedValue);
+        assertNull(deserializedValue.getAdditionalFields().get("environment"));
+        assertEquals(UNIQUE_ID, deserializedValue.getUniqueId());
+        assertEquals(ENVIRONMENT, deserializedValue.getEnvironment());
+        assertEquals(REALM, deserializedValue.getRealm());
+        assertEquals(AUTHORITY_ACCOUNT_ID, deserializedValue.getAuthorityAccountId());
+        assertEquals(AUTHORITY_TYPE, deserializedValue.getAuthorityType());
+        assertEquals(GUEST_ID, deserializedValue.getGuestId());
+        assertEquals(FIRST_NAME, deserializedValue.getFirstName());
+        assertEquals(LAST_NAME, deserializedValue.getLastName());
+        assertEquals(AVATAR_URL, deserializedValue.getAvatarUrl());
+        assertEquals(3, deserializedValue.getAdditionalFields().size());
+        assertEquals("bar", deserializedValue.getAdditionalFields().get("foo").getAsString());
+        assertEquals(numbers.toString(), deserializedValue.getAdditionalFields().get("numbers").toString());
+    }
     // End Accounts
 
     // RefreshTokens
@@ -444,6 +542,85 @@ public class AccountCredentialCacheKeyValueDelegateTest {
         assertEquals(CredentialType.RefreshToken.name().toLowerCase(Locale.US), jsonObject.getString("credential_type"));
         assertEquals(CLIENT_ID, jsonObject.getString("client_id"));
         assertEquals(TARGET, jsonObject.getString("target"));
+    }
+
+    @Test
+    public void refreshTokenExtraValueSerialization() throws Exception {
+        final RefreshToken refreshToken = new RefreshToken();
+        refreshToken.setUniqueId(UNIQUE_ID);
+        refreshToken.setEnvironment(ENVIRONMENT);
+        refreshToken.setCredentialType(CredentialType.RefreshToken.name().toLowerCase(Locale.US));
+        refreshToken.setClientId(CLIENT_ID);
+        refreshToken.setTarget(TARGET);
+
+        final Map<String, JsonElement> additionalFields = new HashMap<>();
+
+        // Add some random Json to this object
+        JsonElement jsonStr = new JsonPrimitive("bar");
+        JsonArray jsonNumberArr = new JsonArray();
+        jsonNumberArr.add(1);
+        jsonNumberArr.add(2);
+        jsonNumberArr.add(3);
+        JsonObject jsonObject = new JsonObject();
+        jsonObject.add("object_key", new JsonPrimitive("object_value"));
+
+        additionalFields.put("foo", jsonStr);
+        additionalFields.put("numbers", jsonNumberArr);
+        additionalFields.put("object", jsonObject);
+
+        refreshToken.setAdditionalFields(additionalFields);
+
+        String serializedValue = mDelegate.generateCacheValue(refreshToken);
+        JSONObject derivedCacheValueJsonObject = new JSONObject(serializedValue);
+        assertEquals(UNIQUE_ID, derivedCacheValueJsonObject.getString("unique_id"));
+        assertEquals(ENVIRONMENT, derivedCacheValueJsonObject.getString("environment"));
+        assertEquals(CredentialType.RefreshToken.name().toLowerCase(Locale.US), derivedCacheValueJsonObject.getString("credential_type"));
+        assertEquals(CLIENT_ID, derivedCacheValueJsonObject.getString("client_id"));
+        assertEquals(TARGET, derivedCacheValueJsonObject.getString("target"));
+        assertEquals("bar", derivedCacheValueJsonObject.getString("foo"));
+
+        final JSONArray jsonArr = derivedCacheValueJsonObject.getJSONArray("numbers");
+        assertEquals(3, jsonArr.length());
+
+        final JSONObject jsonObj = derivedCacheValueJsonObject.getJSONObject("object");
+        assertEquals("object_value", jsonObj.getString("object_key"));
+    }
+
+    @Test
+    public void refreshTokenExtraValueDeserialization() throws Exception {
+        final RefreshToken refreshToken = new RefreshToken();
+        refreshToken.setUniqueId(UNIQUE_ID);
+        refreshToken.setEnvironment(ENVIRONMENT);
+        refreshToken.setCredentialType(CredentialType.AccessToken.name().toLowerCase(Locale.US));
+        refreshToken.setClientId(CLIENT_ID);
+        refreshToken.setTarget(TARGET);
+
+        String serializedValue = mDelegate.generateCacheValue(refreshToken);
+
+        // Turn the serialized value into a JSONObject and start testing field equality.
+        final JSONObject jsonObject = new JSONObject(serializedValue);
+
+        // Add more non-standard data to this object...
+        final JSONArray numbers = new JSONArray("[1, 2, 3]");
+        final JSONArray objects = new JSONArray("[{\"hello\" : \"hallo\"}, {\"goodbye\" : \"auf wiedersehen\"}]");
+
+        jsonObject.put("foo", "bar");
+        jsonObject.put("numbers", numbers);
+        jsonObject.put("objects", objects);
+
+        serializedValue = jsonObject.toString();
+
+        final AccessToken deserializedValue = mDelegate.fromCacheValue(serializedValue, AccessToken.class);
+        assertNotNull(deserializedValue);
+        assertNull(deserializedValue.getAdditionalFields().get("environment"));
+        assertEquals(UNIQUE_ID, deserializedValue.getUniqueId());
+        assertEquals(ENVIRONMENT, deserializedValue.getEnvironment());
+        assertEquals(CredentialType.AccessToken.name().toLowerCase(Locale.US), deserializedValue.getCredentialType());
+        assertEquals(CLIENT_ID, deserializedValue.getClientId());
+        assertEquals(TARGET, deserializedValue.getTarget());
+        assertEquals(3, deserializedValue.getAdditionalFields().size());
+        assertEquals("bar", deserializedValue.getAdditionalFields().get("foo").getAsString());
+        assertEquals(numbers.toString(), deserializedValue.getAdditionalFields().get("numbers").toString());
     }
     // End RefreshTokens
 }
