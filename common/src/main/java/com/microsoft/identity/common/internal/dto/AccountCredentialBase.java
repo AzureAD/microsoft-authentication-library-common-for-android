15972a5f890ceed50c21615ea3770f7aff2e51c7
Brian Melton <brianmel@microsoft.com>
2018-03-28 14:19:28 -0400

    Creating new base class to support extra field [de]/serialization

---
+++ common/src/main/java/com/microsoft/identity/common/internal/dto/AccountCredentialBase.java
@@ -0,0 +1,43 @@
+package com.microsoft.identity.common.internal.dto;
+
+import org.json.JSONObject;
+
+import java.util.Map;
+
+/**
+ * Base class for Objects to support the [de]/serialization of extra fields.
+ */
+public abstract class AccountCredentialBase {
+
+    private transient Map<String, JSONObject> mAdditionalFields;
+
+    public Map<String, JSONObject> getAdditionalFields() {
+        return mAdditionalFields;
+    }
+
+    public void setAdditionalFields(Map<String, JSONObject> additionalFields) {
+        this.mAdditionalFields = additionalFields;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+
+        AccountCredentialBase that = (AccountCredentialBase) o;
+
+        return mAdditionalFields != null ? mAdditionalFields.equals(that.mAdditionalFields) : that.mAdditionalFields == null;
+    }
+
+    @Override
+    public int hashCode() {
+        return mAdditionalFields != null ? mAdditionalFields.hashCode() : 0;
+    }
+
+    @Override
+    public String toString() {
+        return "AccountCredentialBase{" +
+                "mAdditionalFields=" + mAdditionalFields +
+                '}';
+    }
+}
