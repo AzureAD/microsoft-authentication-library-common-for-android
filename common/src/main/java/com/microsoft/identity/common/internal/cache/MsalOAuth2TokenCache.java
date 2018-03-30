1a9c2919ce95a68ac06da6d91c44a88e5e3bb122
Brian Melton <brianmel@microsoft.com>
2018-03-30 17:46:00 -0400

    First pass. Delete existing tokens by scope intersection

--- common/src/main/java/com/microsoft/identity/common/internal/cache/MsalOAuth2TokenCache.java
+++ common/src/main/java/com/microsoft/identity/common/internal/cache/MsalOAuth2TokenCache.java
@@ -4,13 +4,18 @@ import android.content.Context;
 
 import com.microsoft.identity.common.internal.dto.AccessToken;
 import com.microsoft.identity.common.internal.dto.Account;
+import com.microsoft.identity.common.internal.dto.Credential;
+import com.microsoft.identity.common.internal.dto.CredentialType;
 import com.microsoft.identity.common.internal.providers.oauth2.AuthorizationRequest;
 import com.microsoft.identity.common.internal.providers.oauth2.OAuth2Strategy;
 import com.microsoft.identity.common.internal.providers.oauth2.OAuth2TokenCache;
 import com.microsoft.identity.common.internal.providers.oauth2.RefreshToken;
 import com.microsoft.identity.common.internal.providers.oauth2.TokenResponse;
 
+import java.util.Arrays;
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
 
 public class MsalOAuth2TokenCache
         extends OAuth2TokenCache
@@ -60,7 +65,46 @@ public class MsalOAuth2TokenCache
             final AuthorizationRequest request,
             final TokenResponse response) {
         final AccessToken accessToken = mAccountCredentialAdapter.createAccessToken(oAuth2Strategy, request, response);
+        deleteAccessTokensWithIntersectingScopes(accessToken);
         mAccountCredentialCache.saveCredential(accessToken);
+    }
+
+    private void deleteAccessTokensWithIntersectingScopes(final AccessToken referenceToken) {
+        final List<Credential> accessTokens = mAccountCredentialCache.getCredentials(
+                referenceToken.getUniqueId(),
+                referenceToken.getEnvironment(),
+                CredentialType.AccessToken,
+                referenceToken.getClientId(),
+                referenceToken.getRealm(),
+                null // Wildcard - delete anything that matches...
+        );
+
+        for (final Credential accessToken : accessTokens) {
+            if (scopesIntersect(referenceToken, (AccessToken) accessToken)) {
+                mAccountCredentialCache.removeCredential(accessToken);
+            }
+        }
+    }
+
+    private boolean scopesIntersect(final AccessToken token1, final AccessToken token2) {
+        final Set<String> token1Scopes = scopesAsSet(token1);
+        final Set<String> token2Scopes = scopesAsSet(token2);
+
+        for (final String scope : token2Scopes) {
+            if (token1Scopes.contains(scope)) {
+                return true;
+            }
+        }
+
+        return false;
+    }
+
+    private Set<String> scopesAsSet(final AccessToken token) {
+        final Set<String> scopeSet = new HashSet<>();
+        final String scopeString = token.getTarget();
+        final String[] scopeArray = scopeString.split("\\s+");
+        scopeSet.addAll(Arrays.asList(scopeArray));
+        return scopeSet;
     }
 
     private void saveAccount(
