a17ae730bcf29b20c05e2b786f933b9eb37f4789
Jinxu Jia <jinjia@microsoft.com>
2018-03-30 15:38:23 -0700

    Add the logging class (#53)
    1. Implement Logger class for CommonCore
    2. Add the external logger callback interface

---
+++ common/src/main/java/com/microsoft/identity/common/internal/logging/ILoggerCallback.java
@@ -0,0 +1,16 @@
+package com.microsoft.identity.common.internal.logging;
+
+/**
+ * Interface for apps to configure the external logging and implement the callback to designate the
+ * output of the log messages.
+ */
+public interface ILoggerCallback {
+    /**
+     * Interface method for apps to hand off each log message as it's generated.
+     * @param tag The TAG for the log message.
+     * @param logLevel The {@link Logger.LogLevel} for the generated message.
+     * @param message The detailed message.
+     * @param containsPII True if the log message contains PII, false otherwise.
+     */
+    void log(String tag, Logger.LogLevel logLevel, String message, boolean containsPII);
+}
