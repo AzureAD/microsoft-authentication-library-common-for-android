0c7f6a903de6419c0d304d92309ec2c27f868dd6
Brian Melton <brianmel@microsoft.com>
2018-04-16 19:48:52 -0400

    Adding method entry/exit logging

--- common/src/main/java/com/microsoft/identity/common/internal/cache/MsalOAuth2TokenCache.java
+++ common/src/main/java/com/microsoft/identity/common/internal/cache/MsalOAuth2TokenCache.java
@@ -34,7 +34,10 @@ public class MsalOAuth2TokenCache
                                 final IAccountCredentialAdapter accountCredentialAdapter,
                                 final List<IShareSingleSignOnState> sharedSsoCaches) {
         super(context);
-        Logger.info(TAG, "Init: " + TAG);
+        Logger.getInstance().setLogLevel(Logger.LogLevel.VERBOSE);
+        Logger.setAllowLogcat(true);
+        Logger.setAllowPii(true);
+        Logger.verbose(TAG, "Init: " + TAG);
         mAccountCredentialCache = accountCredentialCache;
         mSharedSsoCaches = sharedSsoCaches;
         mAccountCredentialAdapter = accountCredentialAdapter;
@@ -45,36 +48,51 @@ public class MsalOAuth2TokenCache
             final OAuth2Strategy oAuth2Strategy,
             final AuthorizationRequest request,
             final TokenResponse response) {
+        final String methodName = "saveTokens";
+        Logger.entering(TAG, methodName, oAuth2Strategy, request, response);
         saveAccount(oAuth2Strategy, request, response);
         saveCredentials(oAuth2Strategy, request, response);
+        Logger.exiting(TAG, methodName);
     }
 
     private void saveCredentials(
             final OAuth2Strategy oAuth2Strategy,
             final AuthorizationRequest request,
             final TokenResponse response) {
+        final String methodName = "saveCredentials";
+        Logger.entering(TAG, methodName, oAuth2Strategy, request, response);
         saveAccessToken(oAuth2Strategy, request, response);
         saveRefreshToken(oAuth2Strategy, request, response);
+        Logger.exiting(TAG, methodName);
     }
 
     private void saveRefreshToken(
             final OAuth2Strategy oAuth2Strategy,
             final AuthorizationRequest request,
             final TokenResponse response) {
+        final String methodName = "saveRefreshToken";
+        Logger.entering(TAG, methodName, oAuth2Strategy, request, response);
         final com.microsoft.identity.common.internal.dto.RefreshToken refreshToken = mAccountCredentialAdapter.createRefreshToken(oAuth2Strategy, request, response);
         mAccountCredentialCache.saveCredential(refreshToken);
+        Logger.exiting(TAG, methodName);
     }
 
     private void saveAccessToken(
             final OAuth2Strategy oAuth2Strategy,
             final AuthorizationRequest request,
             final TokenResponse response) {
+        final String methodName = "saveAccessToken";
+        Logger.entering(TAG, methodName, oAuth2Strategy, request, response);
         final AccessToken accessToken = mAccountCredentialAdapter.createAccessToken(oAuth2Strategy, request, response);
         deleteAccessTokensWithIntersectingScopes(accessToken);
         mAccountCredentialCache.saveCredential(accessToken);
+        Logger.exiting(TAG, methodName);
     }
 
     private void deleteAccessTokensWithIntersectingScopes(final AccessToken referenceToken) {
+        final String methodName = "deleteAccessTokensWithIntersectingScopes";
+        Logger.entering(TAG, methodName, referenceToken);
+
         final List<Credential> accessTokens = mAccountCredentialCache.getCredentials(
                 referenceToken.getUniqueId(),
                 referenceToken.getEnvironment(),
@@ -89,22 +107,33 @@ public class MsalOAuth2TokenCache
                 mAccountCredentialCache.removeCredential(accessToken);
             }
         }
+
+        Logger.exiting(TAG, methodName);
     }
 
     private boolean scopesIntersect(final AccessToken token1, final AccessToken token2) {
+        final String methodName = "scopesIntersect";
+        Logger.entering(TAG, methodName, token1, token2);
         final Set<String> token1Scopes = scopesAsSet(token1);
         final Set<String> token2Scopes = scopesAsSet(token2);
 
+        boolean result = false;
         for (final String scope : token2Scopes) {
             if (token1Scopes.contains(scope)) {
-                return true;
+                result = true;
+                break;
             }
         }
 
-        return false;
+        Logger.exiting(TAG, methodName, result);
+
+        return result;
     }
 
     private Set<String> scopesAsSet(final AccessToken token) {
+        final String methodName = "scopesAsSet";
+        Logger.entering(TAG, methodName, token);
+
         final Set<String> scopeSet = new HashSet<>();
         final String scopeString = token.getTarget();
 
@@ -113,6 +142,8 @@ public class MsalOAuth2TokenCache
             scopeSet.addAll(Arrays.asList(scopeArray));
         }
 
+        Logger.exiting(TAG, methodName, scopeSet);
+
         return scopeSet;
     }
 
@@ -120,19 +151,29 @@ public class MsalOAuth2TokenCache
             final OAuth2Strategy oAuth2Strategy,
             final AuthorizationRequest request,
             final TokenResponse response) {
+        final String methodName = "saveAccount";
+        Logger.entering(TAG, methodName, oAuth2Strategy, request, response);
         final Account accountToSave = mAccountCredentialAdapter.createAccount(oAuth2Strategy, request, response);
         mAccountCredentialCache.saveAccount(accountToSave);
+        Logger.exiting(TAG, methodName);
     }
 
     @Override
     public void setSingleSignOnState(final com.microsoft.identity.common.Account account,
                                      final RefreshToken refreshToken) {
+        final String methodName = "setSingleSignOnState";
+        Logger.entering(TAG, methodName, account, refreshToken);
         // TODO
+        Logger.exiting(TAG, methodName);
     }
 
     @Override
     public RefreshToken getSingleSignOnState(final com.microsoft.identity.common.Account account) {
+        final String methodName = "getSingleSignOnState";
+        Logger.entering(TAG, methodName, account);
+        final RefreshToken result = null;
         // TODO
-        return null;
+        Logger.exiting(TAG, methodName, result);
+        return result;
     }
 }
