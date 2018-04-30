143e3367026571ea683579de74f0cff6ab9e04a6
Brian Melton <brianmel@microsoft.com>
2018-04-30 16:44:16 -0400

    Adding thread_id's to logging

--- common/src/main/java/com/microsoft/identity/common/internal/logging/DiagnosticContext.java
+++ common/src/main/java/com/microsoft/identity/common/internal/logging/DiagnosticContext.java
@@ -4,6 +4,8 @@ import com.microsoft.identity.common.adal.internal.AuthenticationConstants;
 
 public class DiagnosticContext {
 
+    private static final String THREAD_ID = "thread_id";
+
     private static final ThreadLocal<IRequestContext> sREQUEST_CONTEXT_THREAD_LOCAL =
             new ThreadLocal<IRequestContext>() {
                 @Override // This is the default value for the RequestContext if it's unset
@@ -20,11 +22,27 @@ public class DiagnosticContext {
             return;
         }
 
+        requestContext.put(THREAD_ID, String.valueOf(Thread.currentThread().getId()));
         sREQUEST_CONTEXT_THREAD_LOCAL.set(requestContext);
     }
 
     public static IRequestContext getRequestContext() {
+        if (!hasThreadId()) {
+            setThreadId();
+        }
+
         return sREQUEST_CONTEXT_THREAD_LOCAL.get();
+    }
+
+    private static void setThreadId() {
+        sREQUEST_CONTEXT_THREAD_LOCAL.get().put(
+                THREAD_ID,
+                String.valueOf(Thread.currentThread().getId())
+        );
+    }
+
+    private static boolean hasThreadId() {
+        return sREQUEST_CONTEXT_THREAD_LOCAL.get().containsKey(THREAD_ID);
     }
 
     public static void clear() {
