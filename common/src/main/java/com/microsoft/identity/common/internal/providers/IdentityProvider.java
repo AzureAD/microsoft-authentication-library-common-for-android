d1b5e06d66ff005a785e682138baced54f3cef4d
Brian Melton <brianmel@microsoft.com>
2018-05-10 17:20:41 -0400

    Closes #84

--- common/src/main/java/com/microsoft/identity/common/internal/providers/IdentityProvider.java
+++ common/src/main/java/com/microsoft/identity/common/internal/providers/IdentityProvider.java
@@ -3,6 +3,8 @@ package com.microsoft.identity.common.internal.providers;
 import com.microsoft.identity.common.internal.providers.oauth2.OAuth2Configuration;
 import com.microsoft.identity.common.internal.providers.oauth2.OAuth2Strategy;
 
-public abstract class IdentityProvider {
-    public abstract OAuth2Strategy createOAuth2Strategy(OAuth2Configuration config);
+public abstract class IdentityProvider<T extends OAuth2Strategy, U extends OAuth2Configuration> {
+
+    public abstract T createOAuth2Strategy(U config);
+
 }
--- common/src/main/java/com/microsoft/identity/common/internal/providers/microsoft/microsoftsts/MicrosoftSts.java
+++ common/src/main/java/com/microsoft/identity/common/internal/providers/microsoft/microsoftsts/MicrosoftSts.java
@@ -1,16 +1,13 @@
 package com.microsoft.identity.common.internal.providers.microsoft.microsoftsts;
 
 import com.microsoft.identity.common.internal.providers.IdentityProvider;
-import com.microsoft.identity.common.internal.providers.oauth2.OAuth2Configuration;
-import com.microsoft.identity.common.internal.providers.oauth2.OAuth2Strategy;
 
-public class MicrosoftSts extends IdentityProvider {
+public class MicrosoftSts
+        extends IdentityProvider<MicrosoftStsOAuth2Strategy, MicrosoftStsOAuth2Configuration> {
 
     @Override
-    public OAuth2Strategy createOAuth2Strategy(OAuth2Configuration config) {
-        if (config instanceof MicrosoftStsOAuth2Configuration) {
-            return new MicrosoftStsOAuth2Strategy((MicrosoftStsOAuth2Configuration) config);
-        }
-        throw new IllegalArgumentException("Expected instance of AzureActiveDirectoryOAuth2Configuration in AzureActiveDirectory.CreateOAuth2Strategy");
+    public MicrosoftStsOAuth2Strategy createOAuth2Strategy(MicrosoftStsOAuth2Configuration config) {
+        return new MicrosoftStsOAuth2Strategy(config);
     }
+
 }
