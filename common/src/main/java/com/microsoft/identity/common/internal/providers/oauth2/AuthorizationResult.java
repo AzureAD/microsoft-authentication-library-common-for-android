e993306be47a65fb03106c8ece88d6822955b0c3
Jinxu Jia <jinjia@microsoft.com>
2018-06-06 14:29:29 -0700

    Initial the Authorization result APIs (#116)
    * Initial the Authorization result APIs.
    * Suppress the PMD and add child classes of Authorization result for v1 and v2

--- common/src/main/java/com/microsoft/identity/common/internal/providers/oauth2/AuthorizationResult.java
+++ common/src/main/java/com/microsoft/identity/common/internal/providers/oauth2/AuthorizationResult.java
@@ -22,6 +22,10 @@
 // THE SOFTWARE.
 package com.microsoft.identity.common.internal.providers.oauth2;
 
+import android.content.Intent;
+
+import com.microsoft.identity.common.adal.internal.AuthenticationConstants;
+
 /**
  * A class to return the result of the authorization request to the calling code (ADAL or MSAL Controller classes)
  * This class should have a generic status in terms of : Cancelled, TimedOut, Error,  etc...
@@ -29,4 +33,57 @@ package com.microsoft.identity.common.internal.providers.oauth2;
  * In the case of an error/exception this class should return the associated exception
  */
 public abstract class AuthorizationResult {
+    AuthorizationStatus mAuthorizationStatus;
+    AuthorizationResponse mAuthorizationResponse;
+    AuthorizationErrorResponse mAuthorizationErrorResponse;
+
+    public static AuthorizationResult create(int resultCode, final Intent data) {
+        //Step 1: validate data
+        // return the authorization exception/result with AUTHORIZATION_FAILED error.
+
+        if(resultCode == AuthenticationConstants.UIResponse.BROWSER_CODE_CANCEL) {
+            // User cancel the request in webview.
+            // return the Authentication result with user cancel error.
+        } else if (resultCode == AuthenticationConstants.UIResponse.BROWSER_CODE_COMPLETE) {
+            // extract the url from data parse url
+            // and encapsulate the auth code into the AuthorizationResponse
+            // thus AuthorizationResponse should be part of Authentication Result
+        } else if (resultCode == AuthenticationConstants.UIResponse.BROWSER_CODE_ERROR) {
+            // webview returns AuthorizationErrorResponse with error and error description
+        } else if (resultCode == AuthenticationConstants.UIResponse.BROWSER_CODE_AUTHENTICATION_EXCEPTION) {
+            // Serializable exception returned, need to deserialize it and throw it.
+        } else if (resultCode == AuthenticationConstants.UIResponse.BROKER_REQUEST_RESUME) {
+            //Device needs to have broker installed, we expect the apps to call us back when the broker is installed
+        } else {
+            // throw exception for unknown result code.
+        }
+
+        return null;
+    }
+
+    /**
+     * Enum for representing different authorization status.
+     */
+    enum AuthorizationStatus {
+        /**
+         * Code is successfully returned.
+         */
+        SUCCESS,
+
+        /**
+         * User press device back button.
+         */
+        USER_CANCEL,
+
+        /**
+         * Returned URI contains error.
+         */
+        FAIL,
+
+        /**
+         * AuthenticationActivity detects the invalid request.
+         */
+        INVALID_REQUEST
+        //TODO:  Investigate how chrome tab returns http timeout error
+    }
 }
