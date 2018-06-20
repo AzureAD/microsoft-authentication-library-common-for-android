16106b4e12e1e3326b3972e274f369722fcf6869
Brian Melton <brianmel@microsoft.com>
2018-06-20 16:11:06 -0400

    Resolve checkstyle

--- common/src/main/java/com/microsoft/identity/common/adal/internal/AuthenticationConstants.java
+++ common/src/main/java/com/microsoft/identity/common/adal/internal/AuthenticationConstants.java
@@ -744,7 +744,7 @@ public final class AuthenticationConstants {
          * Signature info for Azure authenticator app that installs authenticator
          * component.
          */
-        public static final String AZURE_AUTHENTICATOR_APP_SIGNATURE = "ho040S3ffZkmxqtQrSwpTVOn9r0=";//
+        public static final String AZURE_AUTHENTICATOR_APP_SIGNATURE = "ho040S3ffZkmxqtQrSwpTVOn9r0=";
 
         /**
          * Azure Authenticator app signature hash.
