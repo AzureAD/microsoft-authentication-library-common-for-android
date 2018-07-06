6c952abc7302315910db5f15dd9e4d703b0ad711
heidijinxujia <jinjia@microsoft.com>
2018-07-05 17:11:10 -0700

    Implement the AzureActiveDiretoryWebViewClient, ClientCertAuthChallengeHandler and PKeyAuthChallengeHandler.

--- common/src/main/java/com/microsoft/identity/common/internal/ui/embeddedwebview/challengehandlers/ClientCertAuthChallengeHandler.java
+++ common/src/main/java/com/microsoft/identity/common/internal/ui/embeddedwebview/challengehandlers/ClientCertAuthChallengeHandler.java
@@ -22,5 +22,79 @@
 // THE SOFTWARE.
 package com.microsoft.identity.common.internal.ui.embeddedwebview.challengehandlers;
 
-final class ClientCertAuthChallengeHandler {
+import android.annotation.TargetApi;
+import android.app.Activity;
+import android.os.Build;
+import android.security.KeyChain;
+import android.security.KeyChainAliasCallback;
+import android.security.KeyChainException;
+import android.support.annotation.NonNull;
+import android.webkit.ClientCertRequest;
+
+import com.microsoft.identity.common.internal.logging.Logger;
+
+import java.security.Principal;
+import java.security.PrivateKey;
+import java.security.cert.X509Certificate;
+
+public final class ClientCertAuthChallengeHandler implements IChallengeHandler {
+    private static final String TAG = ClientCertAuthChallengeHandler.class.getSimpleName();
+    private ClientCertRequest mClientCertRequest;
+    private Activity mActivity;
+
+    public ClientCertAuthChallengeHandler(@NonNull final ClientCertRequest request,
+                                          @NonNull final Activity activity) {
+        mClientCertRequest = request;
+        mActivity = activity;
+    }
+
+    @TargetApi(Build.VERSION_CODES.LOLLIPOP)
+    public void process() {
+        final Principal[] acceptableCertIssuers = mClientCertRequest.getPrincipals();
+
+        // When ADFS server sends null or empty issuers, we'll continue with cert prompt.
+        if (acceptableCertIssuers != null) {
+            for (Principal issuer : acceptableCertIssuers) {
+                if (issuer.getName().contains("CN=MS-Organization-Access")) {
+                    //Checking if received acceptable issuers contain "CN=MS-Organization-Access"
+                    Logger.verbose(TAG, "Cancelling the TLS request, not respond to TLS challenge triggered by device authentication.");
+                    mClientCertRequest.cancel();
+                    return;
+                }
+            }
+        }
+
+        KeyChain.choosePrivateKeyAlias(mActivity, new KeyChainAliasCallback() {
+                    @Override
+                    public void alias(String alias) {
+                        if (alias == null) {
+                            Logger.verbose(TAG, "No certificate chosen by user, cancelling the TLS request.");
+                            mClientCertRequest.cancel();
+                            return;
+                        }
+
+                        try {
+                            final X509Certificate[] certChain = KeyChain.getCertificateChain(
+                                    mActivity.getApplicationContext(), alias);
+                            final PrivateKey privateKey = KeyChain.getPrivateKey(
+                                    mActivity, alias);
+
+                            Logger.verbose(TAG, "Certificate is chosen by user, proceed with TLS request.");
+                            mClientCertRequest.proceed(privateKey, certChain);
+                            return;
+                        } catch (final KeyChainException e) {
+                            Logger.errorPII(TAG, "KeyChain exception", e);
+                        } catch (final InterruptedException e) {
+                            Logger.errorPII(TAG, "InterruptedException exception", e);
+                        }
+
+                        mClientCertRequest.cancel();
+                    }
+                },
+                mClientCertRequest.getKeyTypes(),
+                mClientCertRequest.getPrincipals(),
+                mClientCertRequest.getHost(),
+                mClientCertRequest.getPort(),
+                null);
+    }
 }
