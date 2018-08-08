5876504e40c143daff9944e3f53e80c87dd2b227
kreedula <kreedula@microsoft.com>
2018-08-08 14:47:24 -0700

    Commented out Redirect URI validation check and added a TODO to refactor

--- common/src/main/java/com/microsoft/identity/common/internal/providers/microsoft/MicrosoftAuthorizationRequest.java
+++ common/src/main/java/com/microsoft/identity/common/internal/providers/microsoft/MicrosoftAuthorizationRequest.java
@@ -117,9 +117,11 @@ public abstract class MicrosoftAuthorizationRequest extends AuthorizationRequest
                                          final String libraryVersion) {
         super(responseType, clientId, redirectUri, state, scope);
 
+        //TODO : Refactor to move the validation logic of all NonNull values before the request is constructed.
+/*      Commenting out redirect URI validation to resolve IllegalArgumentException for ADAL acquireTokenSync calls with empty redirect url
         if (StringUtil.isEmpty(redirectUri)) {
             throw new IllegalArgumentException("redirect Uri is empty");
-        }
+        }*/
 
         if (StringUtil.isEmpty(authorizationEndpoint)) {
             throw new IllegalArgumentException("Authorization endpoint is empty");
