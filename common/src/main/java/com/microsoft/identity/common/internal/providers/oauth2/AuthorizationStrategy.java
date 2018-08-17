4db00a7575d141ee8bd58e1b08c98414b77608ac
heidijinxujia <jinjia@microsoft.com>
2018-08-16 17:59:30 -0700

    Move authorization constants into AuthorizationStrategy

--- common/src/main/java/com/microsoft/identity/common/internal/providers/oauth2/AuthorizationStrategy.java
+++ common/src/main/java/com/microsoft/identity/common/internal/providers/oauth2/AuthorizationStrategy.java
@@ -23,10 +23,10 @@
 package com.microsoft.identity.common.internal.providers.oauth2;
 
 import android.content.Intent;
+import android.net.Uri;
 
 import com.microsoft.identity.common.exception.ClientException;
 
-import java.io.UnsupportedEncodingException;
 import java.util.concurrent.Future;
 
 /**
@@ -35,18 +35,34 @@ import java.util.concurrent.Future;
  * Possible implementations include: EmbeddedWebViewAuthorizationStrategy, SystemWebViewAuthorizationStrategy, Device Code, etc...
  */
 public abstract class AuthorizationStrategy {
+    public static final int BROWSER_FLOW = 1001;
+
+    public static final String REQUEST_URL_KEY = "com.microsoft.identity.request.url.key";
+
+    public static final String REQUEST_ID = "com.microsoft.identity.request.id";
+
+    public static final String CUSTOM_TAB_REDIRECT = "com.microsoft.identity.customtab.redirect";
+
+    public static final String AUTHORIZATION_FINAL_URL = "com.microsoft.identity.client.final.url";
+
+    static final class UIResponse {
+        static final int CANCEL = 2001;
+
+        static final int AUTH_CODE_ERROR = 2002;
+
+        static final int AUTH_CODE_COMPLETE = 2003;
+
+        static final String ERROR_CODE = "error_code";
+
+        static final String ERROR_DESCRIPTION = "error_description";
+    }
 
     /**
      * Perform the authorization request.
      *
      * @param requestUrl authorization request url
      */
-    public abstract void requestAuthorization(final String requestUrl) throws ClientException;
+    public abstract Future<AuthorizationResult> requestAuthorization(final Uri requestUrl) throws ClientException;
 
-    //public abstract void completeAuthorization(int requestCode, int resultCode, final Intent data);
-
-    public void dispose() {
-        //TODO
-    }
-
+    public abstract void completeAuthorization(int requestCode, int resultCode, final Intent data);
 }
