ef440f00ba7ce9a0e2c2ac1930281743fb83e6f4
Krishna Eedula <38264932+kreedula@users.noreply.github.com>
2018-10-16 11:38:37 -0700

    Constants to accomodate LLT changes (#280) (#286)
    (cherry picked from commit 4bf7cc73e43c10a3ae5b001e2cd9528b59991473)

--- common/src/main/java/com/microsoft/identity/common/adal/internal/AuthenticationConstants.java
+++ common/src/main/java/com/microsoft/identity/common/adal/internal/AuthenticationConstants.java
@@ -378,6 +378,10 @@ public final class AuthenticationConstants {
          */
         public static final String CLAIMS = "claims";
 
+        public static final String CLIENT_CAPABILITIES_CLAIMS_LIST = "xms_cc";
+
+        public static final String CLIENT_CAPABILITY_ACCESS_TOKEN = "access_token";
+
         /**
          * String for cloud instance host name.
          */
@@ -497,6 +501,16 @@ public final class AuthenticationConstants {
          * String for the broker version.
          */
         public static final String ADAL_BROKER_VERSION = "x-client-brkrver";
+
+        /**
+         * String for the host app name
+         */
+        public static final String APP_PACKAGE_NAME = "x-app-name";
+
+        /**
+         * String for the host app version
+         */
+        public static final String APP_VERSION = "x-app-ver";
     }
 
     /**
