28335e3f8ba7f8e5fe4d4e79c0d16b4957de37f4
Shane Oatman <shoatman@microsoft.com>
2018-11-29 15:35:26 -0800

    Adding constants

--- common/src/main/java/com/microsoft/identity/common/internal/broker/BrokerErrorConstants.java
+++ common/src/main/java/com/microsoft/identity/common/internal/broker/BrokerErrorConstants.java
@@ -3,5 +3,7 @@ package com.microsoft.identity.common.internal.broker;
 public class BrokerErrorConstants {
 
     public final static String NO_TOKENS_FOUND = "no_tokens_found";
+    public final static String INVALID_GRANT = "invalid_grant";
+    public static final String IO_ERROR = "io_error";
 
 }
