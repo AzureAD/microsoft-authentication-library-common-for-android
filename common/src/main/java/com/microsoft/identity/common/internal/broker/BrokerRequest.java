ffbd088802b544f6fe17ba4a594b04d85447c403
kreedula <kreedula@microsoft.com>
2019-03-20 22:56:11 -0700

    Broker Result changes

--- common/src/main/java/com/microsoft/identity/common/internal/broker/BrokerRequest.java
+++ common/src/main/java/com/microsoft/identity/common/internal/broker/BrokerRequest.java
@@ -22,8 +22,8 @@
 //  THE SOFTWARE.
 package com.microsoft.identity.common.internal.broker;
 
-import android.os.Parcel;
-import android.os.Parcelable;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
 
 import com.google.gson.annotations.SerializedName;
 
@@ -34,88 +34,133 @@ import java.io.Serializable;
  */
 public class BrokerRequest implements Serializable {
 
+
     private class SerializedNames {
-        public final static String AUTHORITY = "account.authority";
-        public final static String SCOPE = "account.scope";
-        public final static String REDIRECT = "account.redirect";
-        public final static String CLIENT_ID = "account.clientid";
-        public final static String ENVIRONMENT = "account.environment";
-        public final static String REALM = "account.realm";
-        public final static String HOME_ACCOUNT_ID = "account.homeAccountId";
-        public final static String LOCAL_ACCOUNT_ID = "account.localAccountId";
-        public final static String USERNAME = "account.username";
-        public final static String CLIENT_INFO = "account.clientInfo";
-        public final static String AUTHORITY_TYPE = "account.authorityType";
-        public final static String EXTRA_QUERY_STRING_PARAMETER = "account.extra.query.param";
-        public final static String CORRELATION_ID = "account.correlationid";
-        public final static String PROMPT = "account.prompt";
-        public final static String CLAIMS = "account.claims";
-        public final static String FORCE_REFRESH = "force.refresh";
-        public final static String APPLICATION_NAME = "application.name";
-        public final static String APPLICATION_VERSION = "application.version";
-        public final static String MSAL_VERSION_KEY = "msal.version";
-        public final static String EXPIRATION_BUFFER = "expiration.buffer";
+        final static String AUTHORITY = "account.authority";
+        final static String SCOPE = "account.scope";
+        final static String REDIRECT = "account.redirect";
+        final static String CLIENT_ID = "account.clientid";
+        final static String HOME_ACCOUNT_ID = "account.homeAccountId";
+        final static String LOCAL_ACCOUNT_ID = "account.localAccountId";
+        final static String USERNAME = "account.username";
+        final static String EXTRA_QUERY_STRING_PARAMETER = "account.extra.query.param";
+        final static String CORRELATION_ID = "account.correlationid";
+        final static String PROMPT = "account.prompt";
+        final static String CLAIMS = "account.claims";
+        final static String FORCE_REFRESH = "force.refresh";
+        final static String APPLICATION_NAME = "application.name";
+        final static String APPLICATION_VERSION = "application.version";
+        final static String MSAL_VERSION = "msal.version";
     }
 
+    /**
+     * Authority for the request
+     */
     @SerializedName(SerializedNames.AUTHORITY)
+    @NonNull
     private String mAuthority;
 
+    /**
+     * Scopes for the request. This is expected to be of the format
+     * "scope 1 scope2 scope3" with space as a delimiter
+     */
+    @NonNull
     @SerializedName(SerializedNames.SCOPE)
     private String mScope;
 
+    /**
+     * The redirect uri for the request.
+     * // TODO : See if this is needed.
+     */
+    @NonNull
     @SerializedName(SerializedNames.REDIRECT)
     private String mRedirect;
 
+    /**
+     * The client id of the application.
+     */
+    @NonNull
     @SerializedName(SerializedNames.CLIENT_ID)
     private String mClientId;
 
-    @SerializedName(SerializedNames.ENVIRONMENT)
-    private String mEnvironment;
-
-    @SerializedName(SerializedNames.REALM)
-    private String mRealm;
-
-    @SerializedName(SerializedNames.HOME_ACCOUNT_ID)
-    private String mHomeAccountId;
-
-    @SerializedName(SerializedNames.LOCAL_ACCOUNT_ID)
-    private String mLocalAccountId;
-
+    /**
+     * The username for the request.
+     */
+    @NonNull
     @SerializedName(SerializedNames.USERNAME)
     private String mUserName;
 
-    @SerializedName(SerializedNames.CLIENT_INFO)
-    private String mClientInfo;
+    /**
+     * Home account id of the user. Needs to be set for silent request
+     */
+    @Nullable
+    @SerializedName(SerializedNames.HOME_ACCOUNT_ID)
+    private String mHomeAccountId;
 
-    @SerializedName(SerializedNames.AUTHORITY_TYPE)
-    private String mAuthorityType;
+    /**
+     * Local account id of the user. Needs to be set for silent request
+     */
+    @SerializedName(SerializedNames.LOCAL_ACCOUNT_ID)
+    private String mLocalAccountId;
 
+    /**
+     * Extra query parameters set for the request.
+     */
+    @Nullable
     @SerializedName(SerializedNames.EXTRA_QUERY_STRING_PARAMETER)
     private String mExtraQueryStringParameter;
 
+    /**
+     * Correlation id for the request, it should ba unique GUID.
+     */
+    @NonNull
     @SerializedName(SerializedNames.CORRELATION_ID)
     private String mCorrelationId;
 
+    /**
+     * Prompt for the request.
+     * {@link com.microsoft.identity.common.internal.providers.oauth2.OpenIdConnectPromptParameter}
+     * <p>
+     * Default value : {@link com.microsoft.identity.common.internal.providers.oauth2.OpenIdConnectPromptParameter#SELECT_ACCOUNT}
+     */
+    @Nullable
     @SerializedName(SerializedNames.PROMPT)
     private String mPrompt;
 
+    /**
+     * Claims for the request. This needs to be a valid json string.
+     */
+    @Nullable
     @SerializedName(SerializedNames.CLAIMS)
     private String mClaims;
 
+    /**
+     * Boolean if set, will try to refresh the token instead of using it from cache.
+     */
+    @Nullable
     @SerializedName(SerializedNames.FORCE_REFRESH)
     private boolean mForceRefresh;
 
+    /**
+     * Application package name.
+     */
+    @NonNull
     @SerializedName(SerializedNames.APPLICATION_NAME)
     private String mApplicationName;
 
+    /**
+     * Application version.
+     */
+    @NonNull
     @SerializedName((SerializedNames.APPLICATION_VERSION))
     private String mApplicationVersion;
 
-    @SerializedName(SerializedNames.MSAL_VERSION_KEY)
+    /**
+     * Msal version.
+     */
+    @NonNull
+    @SerializedName(SerializedNames.MSAL_VERSION)
     private String mMsalVersion;
-
-    @SerializedName(SerializedNames.EXPIRATION_BUFFER)
-    private int mExpirationBuffer;
 
 
     public BrokerRequest() {
@@ -154,23 +199,6 @@ public class BrokerRequest implements Serializable {
         this.mClientId = clientId;
     }
 
-
-    public String getEnvironment() {
-        return mEnvironment;
-    }
-
-    public void setEnvironment(String environment) {
-        this.mEnvironment = environment;
-    }
-
-    public String getRealm() {
-        return mRealm;
-    }
-
-    public void setRealm(String realm) {
-        this.mRealm = realm;
-    }
-
     public String getHomeAccountId() {
         return mHomeAccountId;
     }
@@ -193,22 +221,6 @@ public class BrokerRequest implements Serializable {
 
     public void setUserName(String userName) {
         this.mUserName = userName;
-    }
-
-    public String getClientInfo() {
-        return mClientInfo;
-    }
-
-    public void setClientInfo(String clientInfo) {
-        this.mClientInfo = clientInfo;
-    }
-
-    public String getAuthorityType() {
-        return mAuthorityType;
-    }
-
-    public void setAuthorityType(String authorityType) {
-        this.mAuthorityType = authorityType;
     }
 
     public String getExtraQueryStringParameter() {
@@ -275,11 +287,4 @@ public class BrokerRequest implements Serializable {
         this.mMsalVersion = version;
     }
 
-    public int getExpirationBuffer() {
-        return mExpirationBuffer;
-    }
-
-    public void setExpirationBuffer(int expirationBuffer) {
-        this.mExpirationBuffer = expirationBuffer;
-    }
 }
--- common/src/main/java/com/microsoft/identity/common/internal/broker/BrokerResult.java
+++ common/src/main/java/com/microsoft/identity/common/internal/broker/BrokerResult.java
@@ -22,11 +22,180 @@
 //  THE SOFTWARE.
 package com.microsoft.identity.common.internal.broker;
 
+import android.support.annotation.Nullable;
+
 import java.io.Serializable;
 
 /**
  * Encapsulates the possible responses from the broker.  Both successful response and error response.
  */
 public class BrokerResult implements Serializable {
+
+    private class SerializedNames {
+        static final String ACCeSS_TOKEN = "broker.accessToken";
+        static final String ID_TOKEN = "broker.idToken";
+        static final String HOMEACCOUNT_ID = "broker.homeAccountId";
+        static final String LCCAL_ACCOUNT_ID = "broker.localAccountId";
+        static final String USERNAME = "broker.username";
+        static final String TOKENTYPE = "broker.tokenType";
+        static final String CLIENINFO = "broker.clientInfo";
+        static final String AUTHORITY = "broker.authority";
+        static final String ENVIRONMENT = "broker.environment";
+        static final String EXPIRES_ON = "broker.expiresOn";
+        static final String EXTENDED_EXPIRES_ON = "broker.extendedExpiresOn";
+        static final String CACHED_AT = "broker.cachedAt";
+        static final String SPE_RING = "broker.speRing";
+        static final String REFRESH_TOKEN_AGE = "broker.refreshTokenAge";
+        static final String ERROR_CODE = "broker.errorCode";
+        static final String ERROR_MESSAGE = "broker.errorMessage";
+        static final String CORRELATION_ID = "broker.correlationId";
+        static final String SUB_ERROR_CODE = "broker.subErrorCode";
+        static final String HTTP_STATUS_CODE = "broker.httpStatusCode";
+        static final String HTTP_RESPONSE_HEADERS = "broker.httpResponseHeaders";
+        static final String HTTP_RESPONSE_BODY = "broker.httpResponseBody";
+        static final String CLI_TELEM_ERRORCODE = "broker.cliTelemErrorCode";
+        static final String CLI_TELEM_SUB_ERROR_CODe = "broker.cliTelemSubErrorCode";
+    }
+
+    // Success parameters
+    /**
+     * Access token from the response
+     */
+    @Nullable
+    private String mAccessToken;
+
+    /**
+     * ID token from the response
+     */
+    @Nullable
+    private String mIdToken;
+
+    /**
+     * Home account id of the user.
+     */
+    @Nullable
+    private String mHomeAccountId;
+
+    /**
+     * Local account id or user id of the User
+     */
+    @Nullable
+    private String mLocalAccountId;
+
+    /**
+     * Username of the User.
+     */
+    @Nullable
+    private String mUserName;
+
+    /**
+     * Token type from the response
+     */
+    @Nullable
+    private String mTokenType;
+
+    /**
+     * Client Info from the response
+     */
+    @Nullable
+    private String mClientInfo;
+
+    /**
+     * Authority from the response.
+     */
+    @Nullable
+    private String mAuthority;
+
+    /**
+     * Environment used to cache token.
+     */
+    @Nullable
+    private String mEnvironment;
+
+    /**
+     * Expires on value for the token.
+     */
+    @Nullable
+    private long mExpiresOn;
+
+    /**
+     * Extended expires on value for the token
+     */
+    @Nullable
+    private long mExtendedExpiresOn;
+
+    /**
+     * Access token cache at time in millis
+     */
+    @Nullable
+    private long mCachedAt;
+
+    /**
+     * Client telemetry SPE ring
+     */
+    @Nullable
+    private String mSpeRing;
+
+    /**
+     * Refresh token age from client telemetry
+     */
+    @Nullable
+    private String mRefreshTokenAge;
+
+    // Exception parameters
+
+    /**
+     * Error code corresponding to any of the {@link com.microsoft.identity.common.exception.ErrorStrings}
+     */
+    @Nullable
+    private String mErrorCode;
+
+    /**
+     * Error message
+     */
+    @Nullable
+    private String mErrorMessage;
+
+    /**
+     * Correlation id of the request
+     */
+    @Nullable
+    private String mCorrelationId;
+
+    /**
+     * Sub error code from the error response
+     */
+    @Nullable
+    private String mSubErrorCode;
+
+    /**
+     * Http Status code of the error response
+     */
+    @Nullable
+    private String mHttpStatusCode;
+
+    /**
+     * Response headers or the error response in json format
+     */
+    @Nullable
+    private String mHttpResponseHeaders;
+
+    /**
+     * Response body of the error response
+     */
+    @Nullable
+    private String mHttpResponseBody;
+
+    /**
+     * Client telemetry error code
+     */
+    @Nullable
+    private String mCliTelemErrorCode;
+
+    /**
+     * Client telemetry sub error code
+     */
+    @Nullable
+    private String mCliTelemSubErrorCode;
 
 }
