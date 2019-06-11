fe54c3792a7d70b93b0ae3ded56c5998d7c29442
Dome Pongmongkol <rapong@microsoft.com>
2019-06-11 15:21:00 -0700

    Get BrokerResult from bundle

--- common/src/main/java/com/microsoft/identity/common/internal/result/MsalBrokerResultAdapter.java
+++ common/src/main/java/com/microsoft/identity/common/internal/result/MsalBrokerResultAdapter.java
@@ -149,14 +149,7 @@ public class MsalBrokerResultAdapter implements IBrokerResultAdapter {
 
     @Override
     public ILocalAuthenticationResult authenticationResultFromBundle(@NonNull final Bundle resultBundle) {
-        final BrokerResult brokerResult =
-                new GsonBuilder()
-                        .registerTypeAdapter(ICacheRecord.class, new ICacheRecordGsonAdapter())
-                        .create()
-                        .fromJson(
-                                resultBundle.getString(AuthenticationConstants.Broker.BROKER_RESULT_V2),
-                                BrokerResult.class
-                        );
+        final BrokerResult brokerResult = brokerResultFromBundle(resultBundle);
 
         if (brokerResult == null) {
             Logger.error(TAG, "Broker Result not returned from Broker, ", null);
@@ -176,14 +169,21 @@ public class MsalBrokerResultAdapter implements IBrokerResultAdapter {
 
     }
 
+    public static BrokerResult brokerResultFromBundle(@NonNull final Bundle resultBundle) {
+        return new GsonBuilder()
+                .registerTypeAdapter(ICacheRecord.class, new ICacheRecordGsonAdapter())
+                .create()
+                .fromJson(
+                        resultBundle.getString(AuthenticationConstants.Broker.BROKER_RESULT_V2),
+                        BrokerResult.class
+                );
+    }
+
     @Override
     public BaseException baseExceptionFromBundle(@NonNull final Bundle resultBundle) {
         Logger.verbose(TAG, "Constructing exception from result bundle");
 
-        final BrokerResult brokerResult = new Gson().fromJson(
-                resultBundle.getString(AuthenticationConstants.Broker.BROKER_RESULT_V2),
-                BrokerResult.class
-        );
+        final BrokerResult brokerResult = brokerResultFromBundle(resultBundle);
 
         if (brokerResult == null) {
             Logger.error(TAG, "Broker Result not returned from Broker, ", null);
@@ -381,7 +381,7 @@ public class MsalBrokerResultAdapter implements IBrokerResultAdapter {
 
     /**
      * Get a bundle from current account's List<ICacheRecord>.
-     * 
+     *
      * @param cacheRecords current account's List<ICacheRecord>.
      * @return Bundle
      */
@@ -389,7 +389,8 @@ public class MsalBrokerResultAdapter implements IBrokerResultAdapter {
         final Bundle resultBundle = new Bundle();
 
         if (cacheRecords != null) {
-            final Type listOfCacheRecords = new TypeToken<List<ICacheRecord>>(){}.getType();
+            final Type listOfCacheRecords = new TypeToken<List<ICacheRecord>>() {
+            }.getType();
             final String recordInGson = new Gson().toJson(cacheRecords, listOfCacheRecords);
             resultBundle.putString(BROKER_CURRENT_ACCOUNT, recordInGson);
         }
@@ -414,7 +415,8 @@ public class MsalBrokerResultAdapter implements IBrokerResultAdapter {
         GsonBuilder builder = new GsonBuilder();
         builder.registerTypeAdapter(ICacheRecord.class, new ICacheRecordGsonAdapter());
 
-        final Type listOfCacheRecords = new TypeToken<List<ICacheRecord>>() {}.getType();
+        final Type listOfCacheRecords = new TypeToken<List<ICacheRecord>>() {
+        }.getType();
         return builder.create().fromJson(accountJson, listOfCacheRecords);
     }
 }
