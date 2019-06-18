3ec7e8b550e80c6d524fe3a661e0d137636ef5bf
Brian Melton-Grace <brianmel@microsoft.com>
2019-06-17 17:13:23 -0700

    Adding design questions + in-util adapter

--- common/src/main/java/com/microsoft/identity/common/adal/internal/tokensharing/TokenShareUtility.java
+++ common/src/main/java/com/microsoft/identity/common/adal/internal/tokensharing/TokenShareUtility.java
@@ -26,6 +26,8 @@ import android.support.annotation.NonNull;
 
 import com.microsoft.identity.common.exception.BaseException;
 import com.microsoft.identity.common.internal.cache.MsalOAuth2TokenCache;
+import com.microsoft.identity.common.internal.dto.IdTokenRecord;
+import com.microsoft.identity.common.internal.dto.RefreshTokenRecord;
 
 public class TokenShareUtility implements ITokenShareInternal {
 
@@ -34,6 +36,40 @@ public class TokenShareUtility implements ITokenShareInternal {
     public TokenShareUtility(@NonNull final MsalOAuth2TokenCache cache) {
         mTokenCache = cache;
     }
+
+    /*
+    Design questions:
+        1. When saving these tokens, do I persist them for my own clientId it so that this app can
+           use it? Or should I try to parse the clientId out of the input and save it under that?
+           Should I add a FoCI lookup that is agnostic of clientIds completely?
+
+           Brian's answer:
+               Add a clientId agnostic lookup and return that.
+
+        2. If I already have an IdToken/RT for the current user and TSL tries to save a new one,
+           do I delete the one I have so that I maintain the 1 RT per user rule?
+
+           Brian's answer:
+               If you already have tokens for this user and they're not expired, do nothing.
+               If you already have tokens for this user and they're expired, save the new tokens (see next question).
+               If you don't have tokens for this user, save the incoming (see next question).
+
+        3. When saving, what do I do about information I don't have such as homeAccountId? (This is part of the
+           cache-key).
+
+           Brian's answer:
+               ...? (I would say, "make a request" but that's a lot to go wrong, slow, etc.)
+
+        4. For back-compat, it would seem that MSAL needs to both receive v1 idtokens AND supply them.
+           Is that true? What about when ADAL goes away? Will we add new API surface or version the
+           payloads so we stop trying to convert everything between v1/v2 and back again?
+
+           Brian's answer:
+               I think we should beef-up this implementation to return v1 and v2 artifacts BUT the
+               version check inside of "SSOStateSerializer" just throws exceptions if the version isn't
+               "1" so that's ridiculous -- seemingly can't use this. Maybe we keep "version = 1" and
+               just stuff more data in there and hope it doesn't break? ;-)
+     */
 
     @Override
     public String getFamilyRefreshToken(String oid) throws BaseException {
@@ -47,5 +83,17 @@ public class TokenShareUtility implements ITokenShareInternal {
         final TokenCacheItem cacheItemToSave = SSOStateSerializer.deserialize(tokenCacheItemJson);
 
         // The supplied TokenCacheItem will be in the v1 format, convert it to v2 and save it...
+        // TODO How does this work for information we don't have? Such as homeTenantId?
+    }
+
+    private static TokenCacheItem adapt(@NonNull final IdTokenRecord idTokenRecord,
+                                        @NonNull final RefreshTokenRecord refreshTokenRecord) {
+        final TokenCacheItem tokenCacheItem = new TokenCacheItem();
+        tokenCacheItem.setAuthority(idTokenRecord.getAuthority());
+        tokenCacheItem.setRefreshToken(refreshTokenRecord.getSecret());
+        tokenCacheItem.setRawIdToken(idTokenRecord.getSecret());
+        tokenCacheItem.setFamilyClientId(refreshTokenRecord.getFamilyId());
+
+        return tokenCacheItem;
     }
 }
