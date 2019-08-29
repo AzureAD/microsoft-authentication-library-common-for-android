611e27c087d26ada1f4ba9be4836d9b7635a0040
Brian Melton-Grace <brianmel@microsoft.com>
2019-08-29 15:52:48 -0700

    Testing fix for FoCI issues with TenantProfiles (#616)

--- common/src/main/java/com/microsoft/identity/common/internal/cache/BrokerOAuth2TokenCache.java
+++ common/src/main/java/com/microsoft/identity/common/internal/cache/BrokerOAuth2TokenCache.java
@@ -461,6 +461,11 @@ public class BrokerOAuth2TokenCache
 
         if (null != appMetadata) {
             isKnownFoci = null != appMetadata.getFoci();
+
+            Logger.info(
+                    TAG + methodName,
+                    "App is known foci? " + isKnownFoci
+            );
         }
 
         final OAuth2TokenCache targetCache = getTokenCacheForClient(
@@ -530,16 +535,35 @@ public class BrokerOAuth2TokenCache
                                                             @Nullable final String target,
                                                             @NonNull final AccountRecord account) {
         final String methodName = ":loadWithAggregatedAccountData";
+
+        final BrokerApplicationMetadata appMetadata = mApplicationMetadataCache.getMetadata(
+                clientId,
+                account.getEnvironment(),
+                mCallingProcessUid
+        );
+
+        boolean isKnownFoci = false;
+
+        if (null != appMetadata) {
+            isKnownFoci = null != appMetadata.getFoci();
+
+            Logger.info(
+                    TAG + methodName,
+                    "App is known foci? " + isKnownFoci
+            );
+        }
+
         final OAuth2TokenCache targetCache = getTokenCacheForClient(
                 clientId,
                 account.getEnvironment(),
                 mCallingProcessUid
         );
 
-        final boolean shouldUseFociCache = null == targetCache;
+        final boolean appIsUnknownUseFociAsFallback = null == targetCache;
+
         final List<ICacheRecord> resultRecords;
 
-        if (shouldUseFociCache) {
+        if (appIsUnknownUseFociAsFallback) {
             // We do not have a cache for this app or it is not yet known to be a member of the family
             // use the foci cache....
 
@@ -553,6 +577,13 @@ public class BrokerOAuth2TokenCache
                             account
                     )
             );
+        } else if (isKnownFoci) {
+            resultRecords =
+                    mFociCache.loadByFamilyIdWithAggregatedAccountData(
+                            clientId,
+                            target,
+                            account
+                    );
         } else {
             resultRecords = targetCache.loadWithAggregatedAccountData(
                     clientId,
