d0a1d515d0b961e504f5d212a30d6c25b8b4a6ab
Shahzaib <shahzaib.jameel@microsoft.com>
2019-10-11 16:20:23 -0700

    Add public api ids to capture telemetry for overload methods

--- common/src/main/java/com/microsoft/identity/common/internal/eststelemetry/PublicApiId.java
+++ common/src/main/java/com/microsoft/identity/common/internal/eststelemetry/PublicApiId.java
@@ -24,18 +24,83 @@ package com.microsoft.identity.common.internal.eststelemetry;
 
 public final class PublicApiId {
 
-    // Silent Apis
-    public static final String BROKER_ACQUIRE_TOKEN_SILENT = "21";
-    public static final String LOCAL_ACQUIRE_TOKEN_SILENT = "22";
+    //region Silent APIs
 
-    // Interactive APIs
-    public static final String BROKER_ACQUIRE_TOKEN_INTERACTIVE = "121";
-    public static final String LOCAL_ACQUIRE_TOKEN_INTERACTIVE = "122";
+    // PublicClientApplication
+    //==============================================================================================
+    public static final String PCA_ACQUIRE_TOKEN_SILENT_WITH_PARAMETERS = "21";
+    public static final String PCA_ACQUIRE_TOKEN_SILENT_ASYNC_WITH_PARAMETERS = "22";
 
-    // Get/Remove accounts
-    public static final String GET_ACCOUNTS = "921";
-    public static final String GET_ACCOUNT = "922";
-    public static final String GET_CURRENT_ACCOUNT_ASYNC = "923";
-    public static final String REMOVE_ACCOUNT = "924";
-    public static final String SIGN_OUT = "925";
+    // SingleAccountPublicClientApplication
+    //==============================================================================================
+    public static final String SINGLE_ACCOUNT_PCA_ACQUIRE_TOKEN_SILENT_WITH_PARAMETERS = "23";
+    public static final String SINGLE_ACCOUNT_PCA_ACQUIRE_TOKEN_SILENT_ASYNC_WITH_PARAMETERS = "24";
+    public static final String SINGLE_ACCOUNT_PCA_ACQUIRE_TOKEN_SILENT_WITH_SCOPES_AUTHORITY = "25";
+    public static final String SINGLE_ACCOUNT_PCA_ACQUIRE_TOKEN_SILENT_ASYNC_WITH_SCOPES_AUTHORITY_CALLBACK = "26";
+
+    // MultipleAccountPublicClientApplication
+    //==============================================================================================
+    public static final String MULTIPLE_ACCOUNT_PCA_ACQUIRE_TOKEN_SILENT_WITH_SCOPES_ACCOUNT_AUTHORITY = "27";
+    public static final String MULTIPLE_ACCOUNT_PCA_ACQUIRE_TOKEN_SILENT_ASYNC_WITH_SCOPES_ACCOUNT_AUTHORITY_CALLBACK = "28";
+
+    // BrokerClientApplication
+    //==============================================================================================
+    public static final String BROKER_ACQUIRE_TOKEN_SILENT_WITH_PARAMETERS_CALLBACK = "29";
+
+    //endregion
+
+    //region Interactive APIs
+
+    // PublicClientApplication
+    //==============================================================================================
+    public static final String PCA_ACQUIRE_TOKEN_WITH_PARAMETERS = "121";
+    public static final String PCA_ACQUIRE_TOKEN_WITH_ACTIVITY_SCOPES_CALLBACK = "122";
+
+    // SingleAccountPublicClientApplication;
+    //==============================================================================================
+    public static final String SINGLE_ACCOUNT_PCA_SIGN_IN = "123";
+    public static final String SINGLE_ACCOUNT_PCA_ACQUIRE_TOKEN_WITH_PARAMETERS = "124";
+    public static final String SINGLE_ACCOUNT_PCA_ACQUIRE_TOKEN_WITH_ACTIVITY_SCOPES_CALLBACK = "125";
+
+    // MultipleAccountPublicClientApplication
+    //==============================================================================================
+    public static final String MULTIPLE_ACCOUNT_PCA_ACQUIRE_TOKEN_WITH_ACTIVITY_SCOPES_LOGINHINT_CALLBACK = "126";
+
+    // BrokerClientApplication
+    //==============================================================================================
+    public static final String BROKER_ACQUIRE_TOKEN_WITH_PARAMETERS_CALLBACK = "127";
+    public static final String BROKER_ADD_ACCOUNT_WITH_ACTIVITY = "128";
+    public static final String BROKER_CHOOSE_ACCOUNT_WITH_ACTIVITY_ACCOUNTNAME = "129";
+
+    //endregion
+
+    // region GET Accounts
+
+    // SingleAccountPublicClientApplication
+    //==============================================================================================
+    public static final String SINGLE_ACCOUNT_PCA_GET_CURRENT_ACCOUNT = "921";
+    public static final String SINGLE_ACCOUNT_PCA_GET_CURRENT_ACCOUNT_ASYNC = "922";
+
+    // MultipleAccountPublicClientApplication
+    //==============================================================================================
+    public static final String MULTIPLE_ACCOUNT_PCA_GET_ACCOUNTS = "923";
+    public static final String MULTIPLE_ACCOUNT_PCA_GET_ACCOUNTS_WITH_CALLBACK = "924";
+    public static final String MULTIPLE_ACCOUNT_PCA_GET_ACCOUNT_WITH_IDENTIFIER = "925";
+    public static final String MULTIPLE_ACCOUNT_PCA_GET_ACCOUNT_WITH_IDENTIFIER_CALLBACK = "926";
+
+    //endregion
+
+    // region REMOVE account
+
+    // SingleAccountPublicClientApplication
+    //==============================================================================================
+    public static final String SINGLE_ACCOUNT_PCA_SIGN_OUT = "927"; //REMOVE
+    public static final String SINGLE_ACCOUNT_PCA_SIGN_OUT_WITH_CALLBACK = "928"; //REMOVE
+
+    // MultipleAccountPublicClientApplication
+    //==============================================================================================
+    public static final String MULTIPLE_ACCOUNT_PCA_REMOVE_ACCOUNT_WITH_ACCOUNT = "929";
+    public static final String MULTIPLE_ACCOUNT_PCA_REMOVE_ACCOUNT_WITH_ACCOUNT_CALLBACK = "930";
+
+    //endregion
 }
