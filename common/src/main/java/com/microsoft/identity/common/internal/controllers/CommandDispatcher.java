031d91f8871c97bd1c5164deed7197d261c53c1f
kreedula <kreedula@microsoft.com>
2019-10-15 12:27:13 -0700

    Add nullable annotation import

--- common/src/main/java/com/microsoft/identity/common/internal/controllers/CommandDispatcher.java
+++ common/src/main/java/com/microsoft/identity/common/internal/controllers/CommandDispatcher.java
@@ -26,6 +26,7 @@ import android.content.Intent;
 import android.os.Handler;
 import android.os.Looper;
 import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
 import android.text.TextUtils;
 import android.util.Pair;
 
