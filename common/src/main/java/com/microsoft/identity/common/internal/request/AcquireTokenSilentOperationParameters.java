2190ff385cfc2388dbcf4fb7f1e51a91ef9db6d5
Krishna Eedula <38264932+kreedula@users.noreply.github.com>
2019-10-17 18:24:51 -0700

    Revert the ArgumentException removal change (#695)
    * Revert the ArgumentException removal change
    
    * Adding log

--- common/src/main/java/com/microsoft/identity/common/internal/request/AcquireTokenSilentOperationParameters.java
+++ common/src/main/java/com/microsoft/identity/common/internal/request/AcquireTokenSilentOperationParameters.java
@@ -22,12 +22,11 @@
 //  THE SOFTWARE.
 package com.microsoft.identity.common.internal.request;
 
-import androidx.annotation.Nullable;
-
-import com.google.gson.annotations.Expose;
 import com.microsoft.identity.common.exception.ArgumentException;
 import com.microsoft.identity.common.internal.dto.RefreshTokenRecord;
 import com.microsoft.identity.common.internal.logging.Logger;
+
+import androidx.annotation.Nullable;
 
 public class AcquireTokenSilentOperationParameters extends OperationParameters {
 
@@ -50,6 +49,11 @@ public class AcquireTokenSilentOperationParameters extends OperationParameters {
 
         if (mAccount == null) {
             Logger.warn(TAG, "The account set on silent operation parameters is NULL.");
+            throw new ArgumentException(
+                    ArgumentException.ACQUIRE_TOKEN_SILENT_OPERATION_NAME,
+                    ArgumentException.IACCOUNT_ARGUMENT_NAME,
+                    "account is null"
+            );
         }
 
     }
