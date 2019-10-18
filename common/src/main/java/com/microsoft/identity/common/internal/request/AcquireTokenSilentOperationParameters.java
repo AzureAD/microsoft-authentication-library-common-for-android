3ae984734edb31864f9a4d2b4808eaf087f9ce01
kreedula <kreedula@microsoft.com>
2019-10-17 17:59:16 -0700

    Adding log

--- common/src/main/java/com/microsoft/identity/common/internal/request/AcquireTokenSilentOperationParameters.java
+++ common/src/main/java/com/microsoft/identity/common/internal/request/AcquireTokenSilentOperationParameters.java
@@ -24,6 +24,7 @@ package com.microsoft.identity.common.internal.request;
 
 import com.microsoft.identity.common.exception.ArgumentException;
 import com.microsoft.identity.common.internal.dto.RefreshTokenRecord;
+import com.microsoft.identity.common.internal.logging.Logger;
 
 import androidx.annotation.Nullable;
 
@@ -47,6 +48,7 @@ public class AcquireTokenSilentOperationParameters extends OperationParameters {
         super.validate();
 
         if (mAccount == null) {
+            Logger.warn(TAG, "The account set on silent operation parameters is NULL.");
             throw new ArgumentException(
                     ArgumentException.ACQUIRE_TOKEN_SILENT_OPERATION_NAME,
                     ArgumentException.IACCOUNT_ARGUMENT_NAME,
