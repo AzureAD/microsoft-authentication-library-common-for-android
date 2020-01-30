c86a7fdd03e43fd1e7cf90eb8872606f40966834
Dome Pongmongkol <rapong@microsoft.com>
2020-01-30 10:42:19 -0800

    Add spinner on first page load (#795)

--- common/src/main/java/com/microsoft/identity/common/internal/providers/oauth2/WebViewAuthorizationFragment.java
+++ common/src/main/java/com/microsoft/identity/common/internal/providers/oauth2/WebViewAuthorizationFragment.java
@@ -9,6 +9,7 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.webkit.WebSettings;
 import android.webkit.WebView;
+import android.widget.ProgressBar;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
@@ -18,6 +19,7 @@ import com.microsoft.identity.common.R;
 import com.microsoft.identity.common.adal.internal.AuthenticationConstants;
 import com.microsoft.identity.common.internal.logging.Logger;
 import com.microsoft.identity.common.internal.ui.webview.AzureActiveDirectoryWebViewClient;
+import com.microsoft.identity.common.internal.ui.webview.OnPageLoadedCallback;
 import com.microsoft.identity.common.internal.ui.webview.WebViewUtil;
 import com.microsoft.identity.common.internal.ui.webview.challengehandlers.IAuthorizationCompletionCallback;
 
@@ -39,6 +41,8 @@ public class WebViewAuthorizationFragment extends AuthorizationFragment {
     private static final String PKEYAUTH_STATUS = "pkeyAuthStatus";
 
     private WebView mWebView;
+
+    private ProgressBar mProgressBar;
 
     private Intent mAuthIntent;
 
@@ -80,10 +84,18 @@ public class WebViewAuthorizationFragment extends AuthorizationFragment {
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         final View view = inflater.inflate(R.layout.common_activity_authentication, container, false);
+        mProgressBar = view.findViewById(R.id.common_auth_webview_progressbar);
 
         AzureActiveDirectoryWebViewClient webViewClient = new AzureActiveDirectoryWebViewClient(
                 getActivity(),
-                new AuthorizationCompletionCallback(), mRedirectUri);
+                new AuthorizationCompletionCallback(),
+                new OnPageLoadedCallback() {
+                    @Override
+                    public void onPageLoaded() {
+                        mProgressBar.setVisibility(View.INVISIBLE);
+                    }
+                },
+                mRedirectUri);
         setUpWebView(view, webViewClient);
 
         return view;
@@ -102,6 +114,11 @@ public class WebViewAuthorizationFragment extends AuthorizationFragment {
                 Logger.info(TAG + methodName, "Launching embedded WebView for acquiring auth code.");
                 Logger.infoPII(TAG + methodName, "The start url is " + mAuthorizationRequestUrl);
                 mWebView.loadUrl(mAuthorizationRequestUrl, mRequestHeaders);
+
+                // The first page load could take time, and we do not want to just show a blank page.
+                // Therefore, we'll show a spinner here, and hides it when mAuthorizationRequestUrl is successfully loaded.
+                // After that, progress bar will be displayed by MSA/AAD.
+                mProgressBar.setVisibility(View.VISIBLE);
             }
         });
     }
