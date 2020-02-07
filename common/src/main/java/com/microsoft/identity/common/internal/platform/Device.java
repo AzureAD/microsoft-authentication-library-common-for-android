48c5feee9efc05525feeb1d10af04c49a2d32528
Brian Melton-Grace <brianmel@microsoft.com>
2020-02-07 14:54:20 -0800

    Update version string for 1.3.0 MSAL version

--- common/src/main/java/com/microsoft/identity/common/internal/platform/Device.java
+++ common/src/main/java/com/microsoft/identity/common/internal/platform/Device.java
@@ -73,7 +73,7 @@ public final class Device {
         /**
          * The String representing the sdk platform version.
          */
-        public static final String PRODUCT_VERSION = "1.2.1";
+        public static final String PRODUCT_VERSION = "1.3.0";
 
         /**
          * The String representing the sdk version.
