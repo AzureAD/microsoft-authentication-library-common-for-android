aa0df3864ebb2f68bc4182d92ca90ed31d918099
shahzaibj <shahzaib.jameel@microsoft.com>
2020-03-06 12:09:51 -0800

    Add comment

--- common/src/main/java/com/microsoft/identity/common/internal/providers/oauth2/BrowserAuthorizationFragment.java
+++ common/src/main/java/com/microsoft/identity/common/internal/providers/oauth2/BrowserAuthorizationFragment.java
@@ -73,7 +73,12 @@ public class BrowserAuthorizationFragment extends AuthorizationFragment {
     public static Intent createCustomTabResponseIntent(final Context context,
                                                        final String responseUri) {
         if (sCallingActivityClass == null) {
-            // can't create intent for response if no activity available
+            // can't create intent for response if no activity available, this can happen if the app
+            // was closed either by the user or the OS.
+            // An example would be in the case of using browser without custom tabs, and closing
+            // the app after the interactive request started in the browser. After closing, the user
+            // returns to the browser and completes authorization and when the OS redirects here,
+            // the calling activity class is NULL as the app was closed and memory was wiped.
             Logger.warn(TAG, "Calling activity class is NULL. Unable to create intent for response.");
             return null;
         }
