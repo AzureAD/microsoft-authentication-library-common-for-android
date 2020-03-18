74d15936073f0246da9eef7ed29d4e940a5ed652
Dome Pongmongkol <rapong@microsoft.com>
2020-03-18 14:04:11 -0700

    Add dual screen support in common (#854)

--- common/src/main/java/com/microsoft/identity/common/internal/controllers/BaseController.java
+++ common/src/main/java/com/microsoft/identity/common/internal/controllers/BaseController.java
@@ -62,6 +62,7 @@ import com.microsoft.identity.common.internal.providers.oauth2.TokenResponse;
 import com.microsoft.identity.common.internal.providers.oauth2.TokenResult;
 import com.microsoft.identity.common.internal.request.AcquireTokenOperationParameters;
 import com.microsoft.identity.common.internal.request.AcquireTokenSilentOperationParameters;
+import com.microsoft.identity.common.internal.request.BrokerAcquireTokenOperationParameters;
 import com.microsoft.identity.common.internal.request.BrokerAcquireTokenSilentOperationParameters;
 import com.microsoft.identity.common.internal.request.OperationParameters;
 import com.microsoft.identity.common.internal.request.SdkType;
@@ -190,8 +191,6 @@ public abstract class BaseController {
                     acquireTokenOperationParameters.getOpenIdConnectPromptParameter() == OpenIdConnectPromptParameter.SELECT_ACCOUNT) {
                 builder.setPrompt(null);
             }
-
-
         }
 
         builder.setScope(TextUtils.join(" ", parameters.getScopes()));
