2e4bc4b8ba880b9c2fe501a9571531dbf384fec9
kreedula <kreedula@microsoft.com>
2020-06-12 13:01:51 -0700

    Removed RC tag

--- common/src/main/java/com/microsoft/identity/common/internal/platform/Device.java
+++ common/src/main/java/com/microsoft/identity/common/internal/platform/Device.java
@@ -85,7 +85,7 @@ public final class Device {
         /**
          * The String representing the sdk platform version.
          */
-        public static final String PRODUCT_VERSION = "1.5.1";
+        public static final String PRODUCT_VERSION = "1.5.2";
 
         /**
          * The String representing the sdk version.
