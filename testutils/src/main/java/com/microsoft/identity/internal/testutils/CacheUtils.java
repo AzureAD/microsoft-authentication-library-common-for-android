c8a8de2b534cee95b91b926a4a8d4ec79bf5a931
Himanshu Jaiswal <67945020+jaiswalh@users.noreply.github.com>
2020-08-13 01:51:33 +0530

    added comments and modify the editTokenSignature method.

--- testutils/src/main/java/com/microsoft/identity/internal/testutils/CacheUtils.java
+++ testutils/src/main/java/com/microsoft/identity/internal/testutils/CacheUtils.java
@@ -1,7 +1,30 @@
+// Copyright (c) Microsoft Corporation.
+// All rights reserved.
+//
+// This code is licensed under the MIT License.
+//
+// Permission is hereby granted, free of charge, to any person obtaining a copy
+// of this software and associated documentation files(the "Software"), to deal
+// in the Software without restriction, including without limitation the rights
+// to use, copy, modify, merge, publish, distribute, sublicense, and / or sell
+// copies of the Software, and to permit persons to whom the Software is
+// furnished to do so, subject to the following conditions :
+//
+// The above copyright notice and this permission notice shall be included in
+// all copies or substantial portions of the Software.
+//
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+// THE SOFTWARE.
 package com.microsoft.identity.internal.testutils;
 
 import android.content.Context;
 import android.content.SharedPreferences;
+import android.text.TextUtils;
 
 import androidx.annotation.NonNull;
 import androidx.arch.core.util.Function;
@@ -30,38 +53,50 @@ public class CacheUtils {
         boolean test(T t);
     }
 
-    private boolean isAccessToken(@NonNull String cacheKey) {
+    private static boolean isAccessToken(@NonNull final String cacheKey) {
         return SharedPreferencesAccountCredentialCache.getCredentialTypeForCredentialCacheKey(cacheKey) == CredentialType.AccessToken;
     }
 
-    private boolean isRefreshToken(@NonNull String cacheKey) {
+    private static boolean isRefreshToken(@NonNull final String cacheKey) {
         return SharedPreferencesAccountCredentialCache.getCredentialTypeForCredentialCacheKey(cacheKey) == CredentialType.RefreshToken;
     }
 
-    private void editAllTokenInCache(String SharedPrefName, Predicate<String> predicate, Function<String, String> editor) {
+    /**
+     * This method will edit all the token specified by the predicate using the  editor
+     * in the shared preference.
+     *
+     * @param SharedPrefName Name of the shared preference where token has been stored.
+     * @param predicate      Generic functional interface representing function that returns true
+     *                       or false depending on taken type.
+     * @param editor         Functional interface to have any number of token editing method.
+     */
+    public void editAllTokenInCache(@NonNull final String SharedPrefName, Predicate<String> predicate, Function<String, String> editor) {
         SharedPreferences sharedPref = getSharedPreferences(SharedPrefName);
         SharedPreferences.Editor prefEditor = sharedPref.edit();
         Map<String, ?> cacheEntries = sharedPref.getAll();
 
-        //get all the key from the cache entry, verify and corrupt it.
+        //get all the key from the cache entry, verify and edit it.
         for (Map.Entry<String, ?> cacheEntry : cacheEntries.entrySet()) {
-            String keyToCorrupt = cacheEntry.getKey();
-            if (predicate.test(keyToCorrupt)) {
-                if (keyToCorrupt != null) {
-                    Object cacheValue = cacheEntries.get(keyToCorrupt);
-                    if (cacheValue instanceof String) {
-                        Credential credential = CACHE_KEY_VALUE_DELEGATE.fromCacheValue((String) cacheValue, Credential.class);
-                        credential.setSecret(editor.apply(credential.getSecret()));
-                        prefEditor.putString(keyToCorrupt, CACHE_KEY_VALUE_DELEGATE.generateCacheValue(credential));
-                        prefEditor.apply();
-                    }
-
+            String keyToEdit = cacheEntry.getKey();
+            if (predicate.test(keyToEdit)) {
+                Object cacheValue = cacheEntries.get(keyToEdit);
+                if (cacheValue instanceof String) {
+                    Credential credential = CACHE_KEY_VALUE_DELEGATE.fromCacheValue((String) cacheValue, Credential.class);
+                    credential.setSecret(editor.apply(credential.getSecret()));
+                    prefEditor.putString(keyToEdit, CACHE_KEY_VALUE_DELEGATE.generateCacheValue(credential));
+                    prefEditor.apply();
                 }
             }
         }
     }
 
-    public void editAllAccessTokenInCache(@NonNull String SharedPrefName, Function<String, String> editor) {
+    /**
+     * utility function to edit Access Token.
+     *
+     * @param SharedPrefName Name of the shared preference.
+     * @param editor         Functional interface for token editing method.
+     */
+    public void editAllAccessTokenInCache(@NonNull final String SharedPrefName, Function<String, String> editor) {
         Predicate<String> predicate = new Predicate<String>() {
             @Override
             public boolean test(String cacheKey) {
@@ -72,7 +107,13 @@ public class CacheUtils {
         editAllTokenInCache(SharedPrefName, predicate, editor);
     }
 
-    public void editAllRefreshTokenInCache(@NonNull String SharedPrefName, Function<String, String> editor) {
+    /**
+     * utility function to edit Refresh Token.
+     *
+     * @param SharedPrefName Name of the shared preference.
+     * @param editor         Functional interface for token editing method.
+     */
+    public void editAllRefreshTokenInCache(@NonNull final String SharedPrefName, Function<String, String> editor) {
         Predicate<String> predicate = new Predicate<String>() {
             @Override
             public boolean test(String cacheKey) {
@@ -83,28 +124,36 @@ public class CacheUtils {
         editAllTokenInCache(SharedPrefName, predicate, editor);
     }
 
-    public static SharedPreferences getSharedPreferences(final String sharedPrefName) {
+    private static SharedPreferences getSharedPreferences(final String sharedPrefName) {
         final Context context = ApplicationProvider.getApplicationContext();
         return context.getSharedPreferences(sharedPrefName, Context.MODE_PRIVATE);
     }
 
-    /*split the string into 3 segments separated by period "." and
-        set the random character at the random position in the string.*/
+    /**
+     * Split the string into 3 segment separated by period "." and set the
+     * random character at the random position in the 3rd segment.
+     *
+     * @param string string to be edited.
+     * @return edited string.
+     */
     private static String editTokenSignature(String string) {
         String[] segments = string.split(".");
         String signature = segments[segments.length - 1];
 
         StringBuilder sb = new StringBuilder(signature);
         Random rnd = new Random();
-        char charAtRndPosition = sb.charAt(rnd.nextInt());
+        int rndIndex = rnd.nextInt(sb.length());
+        char charAtRndPosition = sb.charAt(rndIndex);
         char rndChar = (char) ('a' + rnd.nextInt(26));
 
         while (charAtRndPosition == rndChar) {
-            charAtRndPosition = sb.charAt(rnd.nextInt());
+            rndIndex = rnd.nextInt(sb.length());
+            charAtRndPosition = sb.charAt(rndIndex);
             rndChar = (char) ('a' + rnd.nextInt(26));
         }
 
-        sb.setCharAt(charAtRndPosition, rndChar);
-        return sb.toString();
+        sb.setCharAt(rndIndex, rndChar);
+        segments[segments.length - 1] = sb.toString();
+        return TextUtils.join(".", segments);
     }
 }
