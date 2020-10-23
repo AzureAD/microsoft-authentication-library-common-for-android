d8afd9e4392ec5ae3b00181a8346cc5fad980264
shahzaibj <shahzaib.jameel@microsoft.com>
2020-10-23 09:57:32 -0700

    Invoke copy method for BrokerHost apk

--- uiautomationutilities/src/main/java/com/microsoft/identity/client/ui/automation/rules/FirebaseRule.java
+++ uiautomationutilities/src/main/java/com/microsoft/identity/client/ui/automation/rules/FirebaseRule.java
@@ -2,6 +2,7 @@ package com.microsoft.identity.client.ui.automation.rules;
 
 import com.microsoft.identity.client.ui.automation.app.AzureSampleApp;
 import com.microsoft.identity.client.ui.automation.broker.BrokerCompanyPortal;
+import com.microsoft.identity.client.ui.automation.broker.BrokerHost;
 import com.microsoft.identity.client.ui.automation.broker.BrokerMicrosoftAuthenticator;
 
 import org.junit.rules.TestRule;
@@ -17,6 +18,7 @@ public class FirebaseRule implements TestRule {
             public void evaluate() throws Throwable {
                 new BrokerMicrosoftAuthenticator().copyApk();
                 new BrokerCompanyPortal().copyApk();
+                new BrokerHost().copyApk();
                 new AzureSampleApp().copyApk();
                 base.evaluate();
             }
