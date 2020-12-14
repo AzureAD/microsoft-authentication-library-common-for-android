b08670d1c3abbf9c12d02ef9ff15ddbfb86f2e14
ayushmeghwani <aayushmeghwani@gmail.com>
2020-12-14 15:23:17 +0530

    remove adal specific files and make wrapper generic

--- uiautomationutilities/src/main/java/com/microsoft/identity/client/ui/automation/sdk/AuthResult.java
+++ uiautomationutilities/src/main/java/com/microsoft/identity/client/ui/automation/sdk/AuthResult.java
@@ -1,36 +1,53 @@
+//  Copyright (c) Microsoft Corporation.
+//  All rights reserved.
+//
+//  This code is licensed under the MIT License.
+//
+//  Permission is hereby granted, free of charge, to any person obtaining a copy
+//  of this software and associated documentation files(the "Software"), to deal
+//  in the Software without restriction, including without limitation the rights
+//  to use, copy, modify, merge, publish, distribute, sublicense, and / or sell
+//  copies of the Software, and to permit persons to whom the Software is
+//  furnished to do so, subject to the following conditions :
+//
+//  The above copyright notice and this permission notice shall be included in
+//  all copies or substantial portions of the Software.
+//
+//  THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+//  IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+//  FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+//  AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+//  LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+//  OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+//  THE SOFTWARE.
 package com.microsoft.identity.client.ui.automation.sdk;
 
-import com.microsoft.aad.adal.AuthenticationResult;
+//import com.microsoft.aad.adal.AuthenticationResult;
 import android.text.TextUtils;
 import org.junit.Assert;
-import java.util.Map;
 import androidx.annotation.NonNull;
 import lombok.Getter;
 
 /**
- * A wrapper class for the result obtained from acquire token interactively
+ * An abstract wrapper class for the result obtained from acquire token interactively
  * or silently, it wraps all the parameters obtained from token and also
  * the exception, with methods to assert success or failures.
  */
 @Getter
-public class AuthResult {
+public abstract class AuthResult {
 
-    private String accessToken;
-    private String idToken;
-    private String userId;
-    private String username;
-    private String authority;
-    private Exception exception;
+    protected static String accessToken;
+    protected static String idToken;
+    protected static String userId;
+    protected static String username;
+    protected static String authority;
+    protected static Exception exception;
 
-    public AuthResult(@NonNull final AuthenticationResult authenticationResult) {
-        this.accessToken = authenticationResult.getAccessToken();
-        this.idToken = authenticationResult.getIdToken();
-        this.userId = authenticationResult.getUserInfo().getUserId();
-        this.username = authenticationResult.getUserInfo().getDisplayableId();
-        this.authority = authenticationResult.getAuthority();
+    // defaul no-arguments constructor to let parent class create their own constructor
+    protected AuthResult() {
     }
 
-    public AuthResult(@NonNull final Exception exception) {
+    protected AuthResult(@NonNull final Exception exception) {
         this.exception = exception;
     }
 
--- uiautomationutilities/src/main/java/com/microsoft/identity/client/ui/automation/sdk/AuthTestParams.java
+++ uiautomationutilities/src/main/java/com/microsoft/identity/client/ui/automation/sdk/AuthTestParams.java
@@ -1,27 +1,19 @@
 package com.microsoft.identity.client.ui.automation.sdk;
 
 import android.app.Activity;
-
-import com.microsoft.aad.adal.PromptBehavior;
-
-import lombok.Builder;
 import lombok.Getter;
 
 /**
  * A wrapper class for all the parameters that are required to acquire token
  * either interactively or silently.
  */
-@Builder
 @Getter
 public class AuthTestParams {
 
-    private final String loginHint;
-    private final String resource;
-    private final String clientId;
-    private final String redirectUri;
-    private final String authority;
-    private final PromptBehavior promptParameter;
-    private final Activity activity;
-    private final String extraQueryParameters;
-
+    protected String loginHint;
+    protected String resource;
+    protected String clientId;
+    protected String redirectUri;
+    protected String authority;
+    protected Activity activity;
 }
