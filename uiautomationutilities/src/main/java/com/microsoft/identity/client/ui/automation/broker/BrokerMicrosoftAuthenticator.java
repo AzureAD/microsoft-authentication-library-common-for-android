7e2f5d2b3c1b380a9223edc20b407467665019d8
krishna reddy kopparthy <68246256+kkrishnareddy@users.noreply.github.com>
2020-12-17 22:27:24 +0530

    Update uiautomationutilities/src/main/java/com/microsoft/identity/client/ui/automation/broker/BrokerMicrosoftAuthenticator.java
    Co-authored-by: Shahzaib <37125644+shahzaibj@users.noreply.github.com>

--- uiautomationutilities/src/main/java/com/microsoft/identity/client/ui/automation/broker/BrokerMicrosoftAuthenticator.java
+++ uiautomationutilities/src/main/java/com/microsoft/identity/client/ui/automation/broker/BrokerMicrosoftAuthenticator.java
@@ -71,7 +71,7 @@ public class BrokerMicrosoftAuthenticator extends AbstractTestBroker implements 
     /**
      * allows you to choose which appInstaller should we use to install Microsoft authenticator.The app can be installed from playstore or from localApkinstaller.
      *
-     * @param appInstaller appInstaller can be either play store or localapkinstaller.
+     * @param appInstaller appInstaller can be either play store or localapkinstaller
      */
     public BrokerMicrosoftAuthenticator(IAppInstaller appInstaller) {
         super(AUTHENTICATOR_APP_PACKAGE_NAME, AUTHENTICATOR_APK, appInstaller);
