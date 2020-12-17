140b00ae8a3905bdcfcd135efeaae9468166eb4e
Shane Oatman <shoatman@microsoft.com>
2020-12-17 14:43:05 -0800

    corrected script

--- scripts/run-instrumented-tests.sh
+++ scripts/run-instrumented-tests.sh
@@ -2,4 +2,4 @@
 emulator @test -no-window -no-audio -wipe-data -no-snapshot
 adb wait-for-device
 sleep 10
-docker run --privileged -v "$PWD":/home/gradle/ -w /home/gradle/ authclient.azurecr.io/samples/dbi-instrumented-api30 gradle common:connectedDebugAndroidTest
+gradle common:connectedDebugAndroidTest
