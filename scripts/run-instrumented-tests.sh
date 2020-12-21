ffe9e7b254fdfd79bc6ebf5f0202d0166dcaa1c9
Shane Oatman <shoatman@microsoft.com>
2020-12-21 15:29:07 -0800

    Updating scripts

--- scripts/run-instrumented-tests.sh
+++ scripts/run-instrumented-tests.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 adb start-server
-emulator @test -no-window -no-audio -wipe-data -no-snapshot &
+emulator @test -no-window -no-audio -wipe-data &
 sleep 30
 gradle -version
 gradle common:connectedDebugAndroidTest
