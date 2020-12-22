bd4a5a93119b522f93777b794abd9dd492d0f384
Shane Oatman <shoatman@microsoft.com>
2020-12-21 21:28:06 -0800

    combine commands

--- scripts/run-instrumented-tests.sh
+++ scripts/run-instrumented-tests.sh
@@ -7,18 +7,7 @@ emulator @test -no-window -no-audio -wipe-data &
 sleep 30
 gradle -version
 echo =============================================
-echo Running unit tests
+echo Running unit and instrumented tests
 echo =============================================
-gradle common:testDebugUnitTest -i
-echo =============================================
-echo Running instrumented tests -i
-echo =============================================
-gradle common:connectedDebugAndroidTest -i
-echo =============================================================
-echo Running gradle clean to remove files now owned by docker user
-echo =============================================================
-gradle clean -i
-echo =============================================================
-echo Dumping environment variables of docker container
-echo =============================================================
-env
+gradle common:testDebugUnitTest common:connectedDebugAndroidTest -i
+
