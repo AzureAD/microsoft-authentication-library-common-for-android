04293c3b74b055f190acae3adb129d4cd09858d9
Shane Oatman <shoatman@microsoft.com>
2020-12-21 20:34:46 -0800

    Formatting and adding debug flag

--- scripts/run-instrumented-tests.sh
+++ scripts/run-instrumented-tests.sh
@@ -1,13 +1,24 @@
 #!/bin/bash
+echo =============================================
+echo Starting ADB Daemon
+echo =============================================
 adb start-server
 emulator @test -no-window -no-audio -wipe-data &
 sleep 30
 gradle -version
+echo =============================================
 echo Running unit tests
-gradle common:testDebugUnitTest
-echo Running instrumented tests
-gradle common:connectedDebugAndroidTest
+echo =============================================
+gradle common:testDebugUnitTest -debug
+echo =============================================
+echo Running instrumented tests -debug
+echo =============================================
+gradle common:connectedDebugAndroidTest -debug
+echo =============================================================
 echo Running gradle clean to remove files now owned by docker user
-gradle clean
+echo =============================================================
+gradle clean -debug
+echo =============================================================
 echo Dumping environment variables of docker container
+echo =============================================================
 env
