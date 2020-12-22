e28fdae220617f10788a1323ff1c951e2f5a602f
Shane Oatman <shoatman@microsoft.com>
2020-12-21 19:50:51 -0800

    different approach

--- scripts/run-instrumented-tests.sh
+++ scripts/run-instrumented-tests.sh
@@ -4,3 +4,5 @@ emulator @test -no-window -no-audio -wipe-data &
 sleep 30
 gradle -version
 gradle common:connectedDebugAndroidTest
+echo Running gradle clean to remove files now owned by docker user
+gradle clean
