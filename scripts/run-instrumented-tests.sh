2d204805ea1e547d30b6f5e63e48289e2971b1f6
Shane Oatman <shoatman@microsoft.com>
2020-12-21 20:20:55 -0800

    Cleanup and add unit test execution

--- scripts/run-instrumented-tests.sh
+++ scripts/run-instrumented-tests.sh
@@ -3,6 +3,11 @@ adb start-server
 emulator @test -no-window -no-audio -wipe-data &
 sleep 30
 gradle -version
+echo Running unit tests
+gradle common:testDebugUnitTest
+echo Running instrumented tests
 gradle common:connectedDebugAndroidTest
 echo Running gradle clean to remove files now owned by docker user
 gradle clean
+echo Dumping environment variables of docker container
+env
