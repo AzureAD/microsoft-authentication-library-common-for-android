3b1263e6bb47f94724f9b61b0a5e43fd1b0df3fb
Moses Gitau <gitaumoses4@gmail.com>
2021-01-29 16:28:04 +0300

    Add mock http request interceptor for testing and intercepting httpclient requests

--- testutils/src/main/java/com/microsoft/identity/internal/testutils/MockHttpResponse.java
+++ testutils/src/main/java/com/microsoft/identity/internal/testutils/MockHttpResponse.java
@@ -22,21 +22,73 @@
 // THE SOFTWARE.
 package com.microsoft.identity.internal.testutils;
 
+import android.util.Pair;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+
+import com.microsoft.identity.common.internal.net.HttpClient.HttpMethod;
 import com.microsoft.identity.common.internal.net.HttpResponse;
 
+import java.net.URL;
+import java.util.HashMap;
+import java.util.Map;
+
 /**
- * Class to set a mock http response at runtime.
+ * Class to  set a mock request interceptor at runtime.
  */
 public class MockHttpResponse {
 
-    private static HttpResponse sHttpResponse;
+    private static final HashMap<Pair<HttpMethod, String>, MockHttpRequestInterceptor> interceptors = new HashMap<>();
 
-
-    public static HttpResponse getHttpResponse() {
-        return sHttpResponse;
+    public static void setInterceptor(@NonNull final MockHttpRequestInterceptor interceptor) {
+        MockHttpResponse.setInterceptor(interceptor, null, ".*");
     }
 
-    public static void setHttpResponse(final HttpResponse httpResponse) {
-        sHttpResponse = httpResponse;
+    public static void setInterceptor(@NonNull final MockHttpRequestInterceptor interceptor,
+                                      @NonNull String urlRegex) {
+        MockHttpResponse.setInterceptor(interceptor, null, urlRegex);
+    }
+
+    public static void setInterceptor(@NonNull final MockHttpRequestInterceptor interceptor,
+                                      @NonNull HttpMethod method) {
+        MockHttpResponse.setInterceptor(interceptor, method, ".*");
+    }
+
+
+    public static void setInterceptor(@NonNull final MockHttpRequestInterceptor interceptor,
+                                      @Nullable final HttpMethod method,
+                                      @NonNull final String urlRegex) {
+        interceptors.put(new Pair<>(method, urlRegex), interceptor);
+    }
+
+    public static MockHttpRequestInterceptor intercept(@NonNull final HttpMethod method, @NonNull final URL url) {
+        for (Pair<HttpMethod, String> pair : interceptors.keySet()) {
+            if ((pair.first == null || pair.first.compareTo(method) == 0) && url.toString().matches(pair.second)) {
+                return interceptors.get(pair);
+            }
+        }
+        return null;
+    }
+
+    public static void setHttpResponse(@NonNull final HttpResponse httpResponse) {
+        MockHttpResponse.setHttpResponse(httpResponse, null, ".*");
+    }
+
+    public static void setHttpResponse(@NonNull final HttpResponse httpResponse, @NonNull HttpMethod method) {
+        MockHttpResponse.setHttpResponse(httpResponse, method, ".*");
+    }
+
+    public static void setHttpResponse(@NonNull final HttpResponse httpResponse, @NonNull final String urlRegex) {
+        MockHttpResponse.setHttpResponse(httpResponse, null, urlRegex);
+    }
+
+    public static void setHttpResponse(@NonNull final HttpResponse httpResponse, @Nullable final HttpMethod method, @NonNull final String urlRegex) {
+        MockHttpResponse.setInterceptor(new MockHttpRequestInterceptor() {
+            @Override
+            public HttpResponse method(@NonNull HttpMethod httpMethod, @NonNull URL requestUrl, @NonNull Map<String, String> requestHeaders, @Nullable byte[] requestContent) {
+                return httpResponse;
+            }
+        }, method, urlRegex);
     }
 }
