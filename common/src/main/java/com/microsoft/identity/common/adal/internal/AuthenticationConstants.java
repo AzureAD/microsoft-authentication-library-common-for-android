6aa6a4f093a6a01f8489ccb1b7c523ec02bef516
Adam Johnson <adjoh@microsoft.com>
2021-04-05 12:05:44 -0700

    Update version to 3 for most MSAL apis

--- common/src/main/java/com/microsoft/identity/common/adal/internal/AuthenticationConstants.java
+++ common/src/main/java/com/microsoft/identity/common/adal/internal/AuthenticationConstants.java
@@ -84,8 +84,6 @@ public final class AuthenticationConstants {
      */
     public static final String ONE_POINT_ZERO = "1.0";
     public static final String TWO_POINT_ZERO = "2.0";
-    public static final String FIVE_POINT_ZERO = "5.0";
-    public static final String SEVEN_POINT_ZERO = "7.0";
 
 
     /**
@@ -1452,9 +1450,9 @@ public final class AuthenticationConstants {
         public static final String AUTHORITY = "microsoft.identity.broker";
 
         private static final String VERSION_1 = ONE_POINT_ZERO;
-        private static final String VERSION_5 = FIVE_POINT_ZERO;
+        private static final String VERSION_3 = "3.0";
         private static final String VERSION_6 = "6.0";
-        private static final String VERSION_7 = SEVEN_POINT_ZERO;
+        private static final String VERSION_7 = "7.0";
         private static final String BROKER_VERSION_1 = ONE_POINT_ZERO;
         private static final String BROKER_VERSION_2 = TWO_POINT_ZERO;
 
@@ -1470,14 +1468,14 @@ public final class AuthenticationConstants {
         @Accessors(prefix = "m")
         @AllArgsConstructor
         public enum API {
-            MSAL_HELLO(MSAL_HELLO_PATH, null, VERSION_5),
-            ACQUIRE_TOKEN_INTERACTIVE(MSAL_ACQUIRE_TOKEN_INTERACTIVE_PATH, null, VERSION_5),
-            ACQUIRE_TOKEN_SILENT(MSAL_ACQUIRE_TOKEN_SILENT_PATH, null, VERSION_5),
-            GET_ACCOUNTS(MSAL_GET_ACCOUNTS_PATH, null, VERSION_5),
-            REMOVE_ACCOUNTS(MSAL_REMOVE_ACCOUNTS_PATH, null, VERSION_5),
-            GET_CURRENT_ACCOUNT_SHARED_DEVICE(MSAL_GET_CURRENT_ACCOUNT_SHARED_DEVICE_PATH, null, VERSION_5),
-            GET_DEVICE_MODE(MSAL_GET_DEVICE_MODE_PATH, null, VERSION_5),
-            SIGN_OUT_FROM_SHARED_DEVICE(MSAL_SIGN_OUT_FROM_SHARED_DEVICE_PATH, null, VERSION_5),
+            MSAL_HELLO(MSAL_HELLO_PATH, null, VERSION_3),
+            ACQUIRE_TOKEN_INTERACTIVE(MSAL_ACQUIRE_TOKEN_INTERACTIVE_PATH, null, VERSION_3),
+            ACQUIRE_TOKEN_SILENT(MSAL_ACQUIRE_TOKEN_SILENT_PATH, null, VERSION_3),
+            GET_ACCOUNTS(MSAL_GET_ACCOUNTS_PATH, null, VERSION_3),
+            REMOVE_ACCOUNTS(MSAL_REMOVE_ACCOUNTS_PATH, null, VERSION_3),
+            GET_CURRENT_ACCOUNT_SHARED_DEVICE(MSAL_GET_CURRENT_ACCOUNT_SHARED_DEVICE_PATH, null, VERSION_3),
+            GET_DEVICE_MODE(MSAL_GET_DEVICE_MODE_PATH, null, VERSION_3),
+            SIGN_OUT_FROM_SHARED_DEVICE(MSAL_SIGN_OUT_FROM_SHARED_DEVICE_PATH, null, VERSION_3),
             GENERATE_SHR(GENERATE_SHR_PATH, null, VERSION_6),
             BROKER_HELLO(BROKER_API_HELLO_PATH, BROKER_VERSION_1, null),
             BROKER_GET_ACCOUNTS(BROKER_API_GET_BROKER_ACCOUNTS_PATH, BROKER_VERSION_1, null),
