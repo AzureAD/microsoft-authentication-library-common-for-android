020969f7db9b6082fcfbf6a36b77616a2d41d0fd
Adam Johnson <adjoh@microsoft.com>
2021-04-27 10:28:58 -0700

    Just rolling back the edge-triggered connection changes, in case.

--- common/src/main/java/com/microsoft/identity/common/adal/internal/net/DefaultConnectionService.java
+++ common/src/main/java/com/microsoft/identity/common/adal/internal/net/DefaultConnectionService.java
@@ -49,12 +49,6 @@ import com.microsoft.identity.common.internal.telemetry.events.BaseEvent;
 public class DefaultConnectionService implements IConnectionService {
 
     private final Context mConnectionContext;
-    private static boolean connectionAvailable = false;
-    // We need a single instance of the NetworkCallback to prevent multiple callbacks from being
-    // registered for each instance of DefaultConnectionService.
-    private static ConnectivityManager.NetworkCallback networkCallback;
-
-    private static final Object lock = new Object();
 
     /**
      * Constructor of DefaultConnectionService.
@@ -63,65 +57,6 @@ public class DefaultConnectionService implements IConnectionService {
      */
     public DefaultConnectionService(Context ctx) {
         mConnectionContext = ctx;
-        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
-            registerNetworkCallback();
-        }
-    }
-
-    /**
-     * Registers a new network callback
-     */
-    @RequiresApi(api = Build.VERSION_CODES.LOLLIPOP)
-    public void registerNetworkCallback() {
-        synchronized (lock) {
-            if (null == networkCallback) {
-                try {
-                    final ConnectivityManager connectivityManager = (ConnectivityManager) mConnectionContext
-                            .getSystemService(Context.CONNECTIVITY_SERVICE);
-                    final NetworkRequest.Builder builder = new NetworkRequest.Builder()
-                            .addCapability(NetworkCapabilities.NET_CAPABILITY_INTERNET);
-
-                    NetworkInfo activeNetwork = connectivityManager.getActiveNetworkInfo();
-                    // Initialize the connectionAvailable to the active network info, before the callback is registered.
-                    DefaultConnectionService.connectionAvailable =
-                            null != activeNetwork &&
-                                    activeNetwork.isConnectedOrConnecting();
-
-                    connectivityManager.registerNetworkCallback(
-                            builder.build(),
-                            networkCallback = new ConnectivityManager.NetworkCallback() {
-                                @Override
-                                public void onAvailable(@NonNull Network network) {
-                                    DefaultConnectionService.connectionAvailable = true;
-                                }
-
-                                @Override
-                                public void onLost(@NonNull Network network) {
-                                    DefaultConnectionService.connectionAvailable = false;
-                                }
-                            });
-                } catch (Exception e) {
-                    // The connection callback registration failed
-                    DefaultConnectionService.connectionAvailable = false;
-                }
-            }
-        }
-    }
-
-    /**
-     * De-registers the existing network callback.
-     */
-    @RequiresApi(api = Build.VERSION_CODES.LOLLIPOP)
-    public void unregisterNetworkCallback() {
-        synchronized (lock) {
-            if (networkCallback != null) {
-                final ConnectivityManager connectivityManager = (ConnectivityManager) mConnectionContext
-                        .getSystemService(Context.CONNECTIVITY_SERVICE);
-
-                connectivityManager.unregisterNetworkCallback(networkCallback);
-                networkCallback = null;
-            }
-        }
     }
 
     /**
@@ -130,22 +65,12 @@ public class DefaultConnectionService implements IConnectionService {
      * @return True if network connection available, false otherwise.
      */
     public boolean isConnectionAvailable() {
-        if (Build.VERSION.SDK_INT < Build.VERSION_CODES.LOLLIPOP) {
-            ConnectivityManager connectivityManager = (ConnectivityManager) mConnectionContext
-                    .getSystemService(Context.CONNECTIVITY_SERVICE);
+        ConnectivityManager connectivityManager = (ConnectivityManager) mConnectionContext
+                .getSystemService(Context.CONNECTIVITY_SERVICE);
+        NetworkInfo activeNetwork = connectivityManager.getActiveNetworkInfo();
 
-            NetworkInfo activeNetwork = connectivityManager.getActiveNetworkInfo();
-
-            DefaultConnectionService.connectionAvailable =
-                    activeNetwork != null &&
-                            activeNetwork.isConnectedOrConnecting();
-        }
-        Telemetry.emit((BaseEvent) new BaseEvent().put(
-                TelemetryEventStrings.Key.NETWORK_CONNECTION,
-                String.valueOf(DefaultConnectionService.connectionAvailable)
-        ));
-
-        return DefaultConnectionService.connectionAvailable;
+        return activeNetwork != null &&
+                        activeNetwork.isConnectedOrConnecting();
     }
 
     /**
