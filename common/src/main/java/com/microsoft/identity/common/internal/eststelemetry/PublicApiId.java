2dd78bcc67321d48c30f67cf33f5b7a7c09827ee
Ahmet Erciyas <65786706+aherciya@users.noreply.github.com>
2021-05-10 10:55:54 -0500

    Adding PublicApiId's for Msal & Broker.

--- common/src/main/java/com/microsoft/identity/common/internal/eststelemetry/PublicApiId.java
+++ common/src/main/java/com/microsoft/identity/common/internal/eststelemetry/PublicApiId.java
@@ -119,4 +119,13 @@ public final class PublicApiId {
     public static final String PCA_GENERATE_SIGNED_HTTP_REQUEST = "1100";
     public static final String PCA_GENERATE_SIGNED_HTTP_REQUEST_ASYNC = "1101";
     //endregion
+
+    // region RefreshOn API
+    //==============================================================================================
+    public static final String MSAL_REFRESH_ON= "1200";
+    public static final String BROKER_SINGLE_ACCOUNT_REFRESH_ON = "1201";
+    public static final String BROKER_JOINT_ACCOUNT_REFRESH_ON = "1202";
+    //endregion
+
+
 }
