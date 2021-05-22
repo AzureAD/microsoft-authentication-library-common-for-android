47aa10dd953b31954598a2074ae02a13d7bf39ab
Shahzaib <shahzaib.jameel@microsoft.com>
2021-05-22 14:20:47 -0700

    Add lab utilities to use msal4j to obtain token for confidential client

---
+++ LabApiUtilities/src/main/java/com/microsoft/identity/labapi/utilities/authentication/msal4j/Msal4jAuthenticationResult.java
@@ -0,0 +1,54 @@
+// Copyright (c) Microsoft Corporation.
+// All rights reserved.
+//
+// This code is licensed under the MIT License.
+//
+// Permission is hereby granted, free of charge, to any person obtaining a copy
+// of this software and associated documentation files(the "Software"), to deal
+// in the Software without restriction, including without limitation the rights
+// to use, copy, modify, merge, publish, distribute, sublicense, and / or sell
+// copies of the Software, and to permit persons to whom the Software is
+// furnished to do so, subject to the following conditions :
+//
+// The above copyright notice and this permission notice shall be included in
+// all copies or substantial portions of the Software.
+//
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+// THE SOFTWARE.
+package java.com.microsoft.identity.labapi.utilities.authentication.msal4j;
+
+import java.com.microsoft.identity.labapi.utilities.authentication.IAuthenticationResult;
+import java.util.Date;
+
+import lombok.AllArgsConstructor;
+
+@AllArgsConstructor
+public class Msal4jAuthenticationResult implements IAuthenticationResult {
+
+    private final com.microsoft.aad.msal4j.IAuthenticationResult mAuthenticationResult;
+
+    @Override
+    public String getAccessToken() {
+        return mAuthenticationResult.accessToken();
+    }
+
+    @Override
+    public String getIdToken() {
+        return mAuthenticationResult.idToken();
+    }
+
+    @Override
+    public String getScopes() {
+        return mAuthenticationResult.scopes();
+    }
+
+    @Override
+    public Date getExpiresOnDate() {
+        return mAuthenticationResult.expiresOnDate();
+    }
+}
