71781c0a705cfbdf4f7b308e8a5f1f73d5cb4efa
Shahzaib <shahzaib.jameel@microsoft.com>
2021-05-27 16:39:47 -0700

    Add a MapRule to generate fields with Map type

---
+++ JsonRules/src/main/java/com/microsoft/identity/json/rules/MapRule.java
@@ -0,0 +1,45 @@
+// Copyright (c) Microsoft Corporation.
+// All rights reserved.
+//
+// This code is licensed under the MIT License.
+//
+// Permission is hereby granted, free of charge, to any person obtaining a copy
+// of this software and associated documentation files(the "Software"), to deal
+// in the Software without restriction, including without limitation the rights
+// to use, copy, modify, merge, publish, distribute, sublicense, and / or sell
+// copies of the Software, and to permit persons to whom the Software is
+// furnished to do so, subject to the following conditions :
+//
+// The above copyright notice and this permission notice shall be included in
+// all copies or substantial portions of the Software.
+//
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+// THE SOFTWARE.
+package com.microsoft.identity.json.rules;
+
+import com.fasterxml.jackson.databind.JsonNode;
+import com.sun.codemodel.JClass;
+import com.sun.codemodel.JClassContainer;
+
+import org.jsonschema2pojo.Schema;
+import org.jsonschema2pojo.rules.Rule;
+
+import java.util.Map;
+
+public class MapRule implements Rule<JClassContainer, JClass> {
+    @Override
+    public JClass apply(String nodeName, JsonNode node, JsonNode parent, JClassContainer jClassContainer, Schema schema) {
+        return addPropertyAsMap(jClassContainer);
+    }
+
+    private JClass addPropertyAsMap(final JClassContainer jclass) {
+        return jclass.owner().ref(Map.class).narrow(
+                jclass.owner().ref(String.class), jclass.owner().ref(String.class)
+        );
+    }
+}
