df8adb499d5bef41ecd4a5859da9fca38a136800
Shahzaib <shahzaib.jameel@microsoft.com>
2021-06-05 16:32:02 -0700

    Add constants for Lab Api

---
+++ LabApiUtilities/src/main/com/microsoft/identity/labapi/utilities/constants/ProtectionPolicy.java
@@ -0,0 +1,46 @@
+// Copyright (c) Microsoft Corporation.
+// All rights reserved.
+//
+// This code is licensed under the MIT License.
+//
+// Permission is hereby granted, free of charge, to any person obtaining a copy
+// of this software and associated documentation files(the "Software"), to deal
+// in the Software without restriction, including without limitation the rights
+// to use, copy, modify, merge, publish, distribute, sublicense, and / or sell
+// copies of the Software, and to permit persons to whom the Software is
+// furnished to do so, subject to the following conditions :
+//
+// The above copyright notice and this permission notice shall be included in
+// all copies or substantial portions of the Software.
+//
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+// THE SOFTWARE.
+package com.microsoft.identity.labapi.utilities.constants;
+
+public enum ProtectionPolicy {
+    NONE(LabConstants.ProtectionPolicy.NONE),
+    CA(LabConstants.ProtectionPolicy.CA),
+    CADJ(LabConstants.ProtectionPolicy.CADJ),
+    MAM(LabConstants.ProtectionPolicy.MAM),
+    MDM(LabConstants.ProtectionPolicy.MDM),
+    MAM_CA(LabConstants.ProtectionPolicy.MAM_CA),
+    MDM_CA(LabConstants.ProtectionPolicy.MDM_CA),
+    TRUE_MAM_CA(LabConstants.ProtectionPolicy.TRUE_MAM_CA),
+    MAM_SPO(LabConstants.ProtectionPolicy.MAM_SPO),
+    BLOCKED(LabConstants.ProtectionPolicy.BLOCKED);
+
+    final String value;
+
+    ProtectionPolicy(final String value) {
+        this.value = value;
+    }
+
+    public String getValue() {
+        return value;
+    }
+}
