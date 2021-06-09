8b6f836e64889c9828b0a127b963d4905a976da1
Dome Pongmongkol <rapong@microsoft.com>
2021-06-09 08:52:25 -0700

    Update shadowHttpClient

--- testutils/src/main/java/com/microsoft/identity/internal/testutils/shadows/ShadowHttpClient.java
+++ testutils/src/main/java/com/microsoft/identity/internal/testutils/shadows/ShadowHttpClient.java
@@ -25,10 +25,10 @@ package com.microsoft.identity.internal.testutils.shadows;
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 
+import com.microsoft.identity.common.java.net.AbstractHttpClient;
 import com.microsoft.identity.common.java.net.HttpClient;
 import com.microsoft.identity.common.java.net.HttpResponse;
 import com.microsoft.identity.common.java.net.UrlConnectionHttpClient;
-import com.microsoft.identity.common.java.net.AbstractHttpClient;
 import com.microsoft.identity.internal.testutils.HttpRequestInterceptor;
 import com.microsoft.identity.internal.testutils.MockHttpClient;
 
@@ -39,13 +39,15 @@ import java.io.IOException;
 import java.net.URL;
 import java.util.Map;
 
+import javax.net.ssl.SSLContext;
+
 /**
  * Allows us to mock http request responses by shadowing the {@link HttpClient}.
  * <p>
  * <p>
  * We need to shadow the {@link AbstractHttpClient} because we are using an instance of the
  * {@link UrlConnectionHttpClient} in the method here
- * {@link ShadowHttpClient#method(HttpClient.HttpMethod, URL, Map, byte[])} therefore using the
+ * {@link ShadowHttpClient#intercept(HttpClient.HttpMethod, URL, Map, byte[], SSLContext)} therefore using the
  * {@link UrlConnectionHttpClient} as the shadow would prevent us from making an actual http request
  * when there are no interceptors defined for the request.
  *
@@ -54,163 +56,76 @@ import java.util.Map;
  */
 @Implements(AbstractHttpClient.class)
 public class ShadowHttpClient {
-
-    /**
-     * Execute the http method using either the interceptor or the actual http client.
-     *
-     * @param httpMethod     the http method
-     * @param requestUrl     the request url
-     * @param requestHeaders the request headers
-     * @param requestContent the request body
-     * @return the mocked response or the actual response
-     * @throws IOException throw an IOException when an error occurred
-     */
     public HttpResponse method(@NonNull HttpClient.HttpMethod httpMethod,
-                               @NonNull URL requestUrl,
-                               @NonNull Map<String, String> requestHeaders,
-                               @Nullable byte[] requestContent) throws IOException {
-        HttpRequestInterceptor interceptor = MockHttpClient.intercept(httpMethod, requestUrl, requestHeaders, requestContent);
+                                  @NonNull URL requestUrl,
+                                  @NonNull Map<String, String> requestHeaders,
+                                  @Nullable byte[] requestContent,
+                                  @Nullable final SSLContext sslContext) throws IOException {
+        final HttpRequestInterceptor interceptor = MockHttpClient.intercept(httpMethod, requestUrl, requestHeaders, requestContent);
         if (interceptor == null) {
-            return UrlConnectionHttpClient.getDefaultInstance().method(httpMethod, requestUrl, requestHeaders, requestContent, null);
+            return UrlConnectionHttpClient.getDefaultInstance().method(httpMethod, requestUrl, requestHeaders, requestContent, sslContext);
         } else {
             return interceptor.intercept(httpMethod, requestUrl, requestHeaders, requestContent);
         }
     }
 
-    /**
-     * This will shadow the PUT method in the http client
-     *
-     * @param requestUrl     the request url
-     * @param requestHeaders the request headers
-     * @param requestContent the request body
-     * @return the mocked response or the actual response
-     * @throws IOException throw an IOException when an error occurred
-     * @see ShadowHttpClient#method(HttpClient.HttpMethod, URL, Map, byte[])
-     * @see HttpClient#put(URL, Map, byte[])
-     */
     @Implementation
     public HttpResponse put(@NonNull URL requestUrl,
                             @NonNull Map<String, String> requestHeaders,
-                            @Nullable byte[] requestContent) throws IOException {
-        return method(HttpClient.HttpMethod.PUT, requestUrl, requestHeaders, requestContent);
+                            @Nullable byte[] requestContent,
+                            @Nullable final SSLContext sslContext) throws IOException {
+        return method(HttpClient.HttpMethod.PUT, requestUrl, requestHeaders, requestContent, sslContext);
     }
 
-    /**
-     * This will shadow the PATCH method in the http client
-     *
-     * @param requestUrl     the request url
-     * @param requestHeaders the request headers
-     * @param requestContent the request body
-     * @return the mocked response or the actual response
-     * @throws IOException throw an IOException when an error occurred
-     * @see ShadowHttpClient#method(HttpClient.HttpMethod, URL, Map, byte[])
-     * @see HttpClient#patch(URL, Map, byte[])
-     */
     @Implementation
     public HttpResponse patch(@NonNull URL requestUrl,
                               @NonNull Map<String, String> requestHeaders,
-                              @Nullable byte[] requestContent) throws IOException {
-        return method(HttpClient.HttpMethod.PATCH, requestUrl, requestHeaders, requestContent);
+                              @Nullable byte[] requestContent,
+                              @Nullable final SSLContext sslContext) throws IOException {
+        return method(HttpClient.HttpMethod.PATCH, requestUrl, requestHeaders, requestContent, sslContext);
     }
 
-    /**
-     * This will shadow the OPTIONS method in the http client
-     *
-     * @param requestUrl     the request url
-     * @param requestHeaders the request headers
-     * @return the mocked response or the actual response
-     * @throws IOException throw an IOException when an error occurred
-     * @see ShadowHttpClient#method(HttpClient.HttpMethod, URL, Map, byte[])
-     * @see HttpClient#options(URL, Map)
-     */
     @Implementation
     public HttpResponse options(@NonNull URL requestUrl,
-                                @NonNull Map<String, String> requestHeaders) throws IOException {
-        return method(HttpClient.HttpMethod.OPTIONS, requestUrl, requestHeaders, null);
+                                @NonNull Map<String, String> requestHeaders,
+                                @Nullable final SSLContext sslContext) throws IOException {
+        return method(HttpClient.HttpMethod.OPTIONS, requestUrl, requestHeaders, null, sslContext);
     }
 
-    /**
-     * This will shadow the POST method in the http client
-     *
-     * @param requestUrl     the request url
-     * @param requestHeaders the request headers
-     * @param requestContent the request body content
-     * @return the mocked response or the actual response
-     * @throws IOException throw an IOException when an error occurred
-     * @see ShadowHttpClient#method(HttpClient.HttpMethod, URL, Map, byte[])
-     * @see HttpClient#post(URL, Map, byte[])
-     */
     @Implementation
     protected HttpResponse post(@NonNull URL requestUrl,
                                 @NonNull Map<String, String> requestHeaders,
-                                @Nullable byte[] requestContent) throws IOException {
-        return method(HttpClient.HttpMethod.POST, requestUrl, requestHeaders, requestContent);
+                                @Nullable byte[] requestContent,
+                                @Nullable final SSLContext sslContext) throws IOException {
+        return method(HttpClient.HttpMethod.POST, requestUrl, requestHeaders, requestContent, sslContext);
     }
 
-    /**
-     * This will shadow the DELETE method in the http client
-     *
-     * @param requestUrl     the request url
-     * @param requestHeaders the request headers
-     * @param requestContent the request body content
-     * @return the mocked response or the actual response
-     * @throws IOException throw an IOException when an error occurred
-     * @see ShadowHttpClient#method(HttpClient.HttpMethod, URL, Map, byte[])
-     * @see HttpClient#delete(URL, Map, byte[])
-     */
     @Implementation
     public HttpResponse delete(@NonNull URL requestUrl,
                                @NonNull Map<String, String> requestHeaders,
-                               @Nullable byte[] requestContent) throws IOException {
-        return method(HttpClient.HttpMethod.DELETE, requestUrl, requestHeaders, requestContent);
+                               @Nullable byte[] requestContent,
+                               @Nullable final SSLContext sslContext) throws IOException {
+        return method(HttpClient.HttpMethod.DELETE, requestUrl, requestHeaders, requestContent, sslContext);
     }
 
-    /**
-     * This will shadow the GET method in the http client
-     *
-     * @param requestUrl     the request url
-     * @param requestHeaders the request headers
-     * @return the mocked response or the actual response
-     * @throws IOException throw an IOException when an error occurred
-     * @see ShadowHttpClient#method(HttpClient.HttpMethod, URL, Map, byte[])
-     * @see HttpClient#get(URL, Map)
-     */
     @Implementation
     public HttpResponse get(@NonNull URL requestUrl,
-                            @NonNull Map<String, String> requestHeaders) throws IOException {
-        return method(HttpClient.HttpMethod.GET, requestUrl, requestHeaders, null);
+                            @NonNull Map<String, String> requestHeaders,
+                            @Nullable final SSLContext sslContext) throws IOException {
+        return method(HttpClient.HttpMethod.GET, requestUrl, requestHeaders, null, sslContext);
     }
 
-    /**
-     * This will shadow the HEAD method in the http client
-     *
-     * @param requestUrl     the request url
-     * @param requestHeaders the request headers
-     * @return the mocked response or the actual response
-     * @throws IOException throw an IOException when an error occurred
-     * @see ShadowHttpClient#method(HttpClient.HttpMethod, URL, Map, byte[])
-     * @see HttpClient#head(URL, Map)
-     */
     @Implementation
     public HttpResponse head(@NonNull URL requestUrl,
-                             @NonNull Map<String, String> requestHeaders) throws IOException {
-        return method(HttpClient.HttpMethod.HEAD, requestUrl, requestHeaders, null);
+                             @NonNull Map<String, String> requestHeaders,
+                             @Nullable final SSLContext sslContext) throws IOException {
+        return method(HttpClient.HttpMethod.HEAD, requestUrl, requestHeaders, null, sslContext);
     }
 
-    /**
-     * This will shadow the TRACE method in the http client
-     *
-     * @param requestUrl     the request url
-     * @param requestHeaders the request headers
-     * @return the mocked response or the actual response
-     * @throws IOException throw an IOException when an error occurred
-     * @see ShadowHttpClient#method(HttpClient.HttpMethod, URL, Map, byte[])
-     * @see HttpClient#trace(URL, Map)
-     */
     @Implementation
     public HttpResponse trace(@NonNull URL requestUrl,
-                              @NonNull Map<String, String> requestHeaders) throws IOException {
-        return method(HttpClient.HttpMethod.TRACE, requestUrl, requestHeaders, null);
+                              @NonNull Map<String, String> requestHeaders,
+                              @Nullable final SSLContext sslContext) throws IOException {
+        return method(HttpClient.HttpMethod.TRACE, requestUrl, requestHeaders, null, sslContext);
     }
-
 }
