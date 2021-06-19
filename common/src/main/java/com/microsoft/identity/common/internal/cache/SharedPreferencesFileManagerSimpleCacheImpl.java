5984fd599215e67275620b6d7fa1ad29872f0a28
Brian Melton-Grace <brianmel@microsoft.com>
2021-06-18 19:18:15 -0700

    Using singleton-based SharedPrefsFileManager for BAM cache + timing logs

---
+++ common/src/main/java/com/microsoft/identity/common/internal/cache/SharedPreferencesFileManagerSimpleCacheImpl.java
@@ -0,0 +1,168 @@
+//  Copyright (c) Microsoft Corporation.
+//  All rights reserved.
+//
+//  This code is licensed under the MIT License.
+//
+//  Permission is hereby granted, free of charge, to any person obtaining a copy
+//  of this software and associated documentation files(the "Software"), to deal
+//  in the Software without restriction, including without limitation the rights
+//  to use, copy, modify, merge, publish, distribute, sublicense, and / or sell
+//  copies of the Software, and to permit persons to whom the Software is
+//  furnished to do so, subject to the following conditions :
+//
+//  The above copyright notice and this permission notice shall be included in
+//  all copies or substantial portions of the Software.
+//
+//  THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+//  IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+//  FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+//  AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+//  LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+//  OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+//  THE SOFTWARE.
+package com.microsoft.identity.common.internal.cache;
+
+import android.content.Context;
+import android.os.SystemClock;
+
+import androidx.annotation.NonNull;
+
+import com.google.gson.Gson;
+import com.microsoft.identity.common.internal.util.StringUtil;
+import com.microsoft.identity.common.logging.Logger;
+
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+import java.util.concurrent.Callable;
+
+import lombok.SneakyThrows;
+
+/**
+ * A simple metadata store definition that uses SharedPreferencesFileManager to persist, read,
+ * update, and delete data. Please note that all CRUD actions return success, as the underlying
+ * store's API does not surface a success indicator. If you need stronger guarantees that an
+ * operation was successful, use {@link SharedPreferencesSimpleCacheImpl} which is less performance
+ * oriented.
+ * <p>
+ * Data serializes as JSON.
+ *
+ * @param <T> The type of metadata that will be persisted.
+ * @see SharedPreferencesSimpleCacheImpl
+ */
+public abstract class SharedPreferencesFileManagerSimpleCacheImpl<T> implements ISimpleCache<T>, IListTypeToken {
+
+    private static final String TAG = SharedPreferencesFileManagerSimpleCacheImpl.class.getSimpleName();
+    private static final String EMTPY_ARRAY = "[]";
+    private static final String TIMING_TAG = "execWithTiming";
+
+    private ISharedPreferencesFileManager mSharedPrefsFileManager;
+    private final String mKeySingleEntry;
+    private final Gson mGson = new Gson();
+
+    public SharedPreferencesFileManagerSimpleCacheImpl(@NonNull final Context context,
+                                                       @NonNull final String prefsName,
+                                                       @NonNull final String singleKey) {
+        Logger.verbose(
+                TAG + "::ctor",
+                "Init"
+        );
+        mSharedPrefsFileManager = SharedPreferencesFileManager.getSharedPreferences(
+                context,
+                prefsName,
+                Context.MODE_PRIVATE,
+                null // File is not encrypted
+        );
+        mKeySingleEntry = singleKey;
+    }
+
+    private interface NamedRunnable<V> extends Callable<V> {
+        String getName();
+    }
+
+    @SneakyThrows
+    private <V> V execWithTiming(@NonNull NamedRunnable<V> runnable) {
+        final long startTimeMillis = SystemClock.elapsedRealtime();
+        final V v = runnable.call();
+        final long execTime = SystemClock.elapsedRealtime() - startTimeMillis;
+        Logger.verbose(TAG + TIMING_TAG, runnable.getName() + "::" + execTime);
+        return v;
+    }
+
+    @Override
+    public boolean insert(final T t) {
+        return execWithTiming(new NamedRunnable<Boolean>() {
+            @Override
+            public String getName() {
+                return "insert";
+            }
+
+            @Override
+            public Boolean call() {
+                final Set<T> allMetadata = new HashSet<>(getAll());
+                allMetadata.add(t);
+                final String json = mGson.toJson(allMetadata);
+                mSharedPrefsFileManager.putString(mKeySingleEntry, json);
+                return true;
+            }
+        });
+    }
+
+    @Override
+    public boolean remove(final T t) {
+        return execWithTiming(new NamedRunnable<Boolean>() {
+            @Override
+            public String getName() {
+                return "remove";
+            }
+
+            @Override
+            public Boolean call() {
+                final Set<T> allMetadata = new HashSet<>(getAll());
+                allMetadata.remove(t);
+                final String json = mGson.toJson(allMetadata);
+                mSharedPrefsFileManager.putString(mKeySingleEntry, json);
+                return true;
+            }
+        });
+    }
+
+    @Override
+    public List<T> getAll() {
+        return execWithTiming(new NamedRunnable<List<T>>() {
+            @Override
+            public String getName() {
+                return "getAll";
+            }
+
+            @Override
+            public List<T> call() {
+                String jsonList = mSharedPrefsFileManager.getString(mKeySingleEntry);
+
+                if (StringUtil.isEmpty(jsonList)) {
+                    jsonList = EMTPY_ARRAY;
+                }
+
+                final List<T> result = mGson.fromJson(jsonList, getListTypeToken());
+
+                return result;
+            }
+        });
+    }
+
+    @Override
+    public boolean clear() {
+        return execWithTiming(new NamedRunnable<Boolean>() {
+            @Override
+            public String getName() {
+                return "clear";
+            }
+
+            @Override
+            public Boolean call() {
+                mSharedPrefsFileManager.clear();
+                return true;
+            }
+        });
+    }
+}
