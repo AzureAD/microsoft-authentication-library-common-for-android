96e3ce4b6a997970d1ec69b8b3d88d234bae7f06
tongyuze <yuto@microsoft.com>
2021-06-23 15:57:01 -0700

    update to test

--- common/src/test/java/com/microsoft/identity/common/MsalCppOAuth2TokenCacheTest.java
+++ common/src/test/java/com/microsoft/identity/common/MsalCppOAuth2TokenCacheTest.java
@@ -244,7 +244,7 @@ public class MsalCppOAuth2TokenCacheTest {
     }
 
     @Test
-    public void removeTwoAccountsWithDifferentRealmsTest() throws ClientException {
+    public void removeTwoAccountsWithDifferentRealmsAndEnvironmentsTest() throws ClientException {
         // Get the generated account
         final AccountRecord generatedAccount = mTestBundle.mGeneratedAccount;
 
@@ -252,7 +252,8 @@ public class MsalCppOAuth2TokenCacheTest {
         mCppCache.saveAccountRecord(generatedAccount);
         mCppCache.saveCredentials(null, mTestBundle.mGeneratedRefreshToken);
 
-        // Save the second account with a different realm
+        // Save the second account with a different realm and environment
+        generatedAccount.setEnvironment("login.chinacloudapi.cn");
         generatedAccount.setRealm(REALM2);
         mCppCache.saveAccountRecord(generatedAccount);
 
@@ -275,7 +276,7 @@ public class MsalCppOAuth2TokenCacheTest {
 
         final AccountRecord restoredAccount2 = mCppCache.getAccount(
                 generatedAccount.getHomeAccountId(),
-                generatedAccount.getEnvironment(),
+                "login.chinacloudapi.cn",
                 REALM2
         );
 
