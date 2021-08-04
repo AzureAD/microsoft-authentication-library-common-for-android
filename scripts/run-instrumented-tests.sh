eff97e48f3390e2315f775c1360d93890ea4c5ba
p3dr0rv <76129899+p3dr0rv@users.noreply.github.com>
2021-08-04 13:05:21 -0700

    list avds

--- scripts/run-instrumented-tests.sh
+++ scripts/run-instrumented-tests.sh
@@ -3,6 +3,9 @@ echo =============================================
 echo Starting ADB Daemon
 echo =============================================
 adb start-server
+echo =============================================
+emulator -list-avds
+echo =============================================
 emulator @test -no-window -no-audio -wipe-data &
 sleep 30
 gradle -version
