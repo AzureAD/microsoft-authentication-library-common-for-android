d77d29a6587c1a1f7c7ee4adadf715af072af248
p3dr0rv <76129899+p3dr0rv@users.noreply.github.com>
2021-08-04 18:36:40 -0700

    Update run-instrumented-tests.sh

--- scripts/run-instrumented-tests.sh
+++ scripts/run-instrumented-tests.sh
@@ -4,13 +4,11 @@ echo Starting ADB Daemon
 echo =============================================
 adb start-server
 echo =============================================
-android list avd
-echo =============================================
 emulator @test -no-window -no-audio -wipe-data &
-sleep 120
+sleep 100
 gradle -version
 echo =============================================
 echo Running instrumented tests
 echo =============================================
-#gradle common:connectedLocalDebugAndroidTest -i -Psugar=true
+gradle common:connectedLocalDebugAndroidTest -i -Psugar=true
 
