b160e907127203e14372d1f0eae80eb7a0804d8b
p3dr0rv <76129899+p3dr0rv@users.noreply.github.com>
2021-08-04 19:06:32 -0700

    looking for adv

--- scripts/run-instrumented-tests.sh
+++ scripts/run-instrumented-tests.sh
@@ -4,8 +4,10 @@ echo Starting ADB Daemon
 echo =============================================
 adb start-server
 echo =============================================
+echo "$ANDROID_AVD_HOME, $ANDROID_SDK_HOME/avd and $HOME/.android/avd"
+echo =============================================
 emulator @test -no-window -no-audio -wipe-data &
-sleep 100
+sleep 60
 gradle -version
 echo =============================================
 echo Running instrumented tests
