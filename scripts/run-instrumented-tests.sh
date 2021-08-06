a01b5c475e2c56bb66be58126918da3eebcbe7f1
p3dr0rv <76129899+p3dr0rv@users.noreply.github.com>
2021-08-06 13:02:12 -0700

    restore

--- scripts/run-instrumented-tests.sh
+++ scripts/run-instrumented-tests.sh
@@ -9,5 +9,4 @@ gradle -version
 echo =============================================
 echo Running instrumented tests
 echo =============================================
-gradle common:connectedLocalDebugAndroidTest -Psugar=true --stacktrace --debug
-
+gradle common:connectedLocalDebugAndroidTest -i -Psugar=true
