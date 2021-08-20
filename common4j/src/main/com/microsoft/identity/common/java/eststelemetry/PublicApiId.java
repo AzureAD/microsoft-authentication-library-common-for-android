09e81995ec5e304ac7a5212c8090042b7db1694b
Dome Pongmongkol <rapong@microsoft.com>
2021-08-19 20:49:05 -0700

    Make changes to accomodate BrokerClientApplication Change

--- common4j/src/main/com/microsoft/identity/common/java/eststelemetry/PublicApiId.java
+++ common4j/src/main/com/microsoft/identity/common/java/eststelemetry/PublicApiId.java
@@ -20,7 +20,7 @@
 // LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
 // OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
 // THE SOFTWARE.
-package com.microsoft.identity.common.internal.eststelemetry;
+package com.microsoft.identity.common.java.eststelemetry;
 
 public final class PublicApiId {
 
