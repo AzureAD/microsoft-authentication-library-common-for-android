55c1210df12e21b138e13d4b9348d7795d9694b6
Shane Oatman <shoatman@microsoft.com>
2021-08-25 08:52:48 -0700

    Fix authorization activity selection to address available user agent on device

--- common/src/main/java/com/microsoft/identity/common/internal/providers/oauth2/AuthorizationActivityFactory.java
+++ common/src/main/java/com/microsoft/identity/common/internal/providers/oauth2/AuthorizationActivityFactory.java
@@ -77,7 +77,7 @@ public class AuthorizationActivityFactory {
         final LibraryConfiguration libraryConfig = LibraryConfiguration.getInstance();
         if (ProcessUtil.isBrokerProcess(context)) {
             intent = new Intent(context, BrokerAuthorizationActivity.class);
-        } else if (libraryConfig.isAuthorizationInCurrentTask()) {
+        } else if (libraryConfig.isAuthorizationInCurrentTask() && !authorizationAgent.equals(AuthorizationAgent.WEBVIEW)) {
                 intent = new Intent(context, CurrentTaskAuthorizationActivity.class);
         } else {
                 intent = new Intent(context, AuthorizationActivity.class);
