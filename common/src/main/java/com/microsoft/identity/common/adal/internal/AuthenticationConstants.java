a7a0ed66b0e2ab878970a2253ed01d66774daa1d
Tanmay Manolkar <tamanolk@microsoft.com>
2022-03-01 09:51:14 -0800

    Update AuthenticationConstants.java

--- common/src/main/java/com/microsoft/identity/common/adal/internal/AuthenticationConstants.java
+++ common/src/main/java/com/microsoft/identity/common/adal/internal/AuthenticationConstants.java
@@ -996,7 +996,7 @@ public final class AuthenticationConstants {
          */
         public static final String INTUNE_APP_PACKAGE_NAME = "com.microsoft.intune";
 
-        /**g
+        /**
          * Azure Authenticator app package name.
          */
         public static final String AZURE_AUTHENTICATOR_APP_PACKAGE_NAME = "com.azure.authenticator";
