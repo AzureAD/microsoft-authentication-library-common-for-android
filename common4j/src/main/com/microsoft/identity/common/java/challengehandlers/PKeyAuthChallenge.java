aab0aa1ff8ab513e0c1e021056ec2a67e70b1f18
Dome Pongmongkol <rapong@microsoft.com>
2022-03-03 17:40:24 -0600

    Fix silent flow pkeyauth, add build param to disable silent flow timeout during debugging

--- common4j/src/main/com/microsoft/identity/common/java/challengehandlers/PKeyAuthChallenge.java
+++ common4j/src/main/com/microsoft/identity/common/java/challengehandlers/PKeyAuthChallenge.java
@@ -45,127 +45,81 @@ import java.util.List;
 import java.util.Map;
 
 import edu.umd.cs.findbugs.annotations.NonNull;
+import edu.umd.cs.findbugs.annotations.Nullable;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.experimental.Accessors;
+import sun.rmi.runtime.Log;
 
 /**
  * A class/builder that represents PKeyAuth challenge.
  * see {@link PKeyAuthChallengeFactory}.
+ *
+ * Spec: https://microsoft.sharepoint.com/teams/aad/devex/Shared%20Documents/Core%20Design/Device%20authentication%20using%20PKeyAuth.docx?web=1
  * */
+@Builder
+@Getter
+@Accessors(prefix = "m")
 public class PKeyAuthChallenge implements Serializable {
     private static final String TAG = PKeyAuthChallenge.class.getSimpleName();
 
     enum RequestField {
-        Nonce, CertAuthorities, Version, SubmitUrl, Context, CertThumbprint
+        Nonce, CertAuthorities, Version, SubmitUrl, Context, CertThumbprint, TenantId
     }
 
     /**
-     * Serial version id.
+     * A challenge code issued to the client. The client is expected to return this nonce value in its response.
      */
-
     private final String mNonce;
 
+    /**
+     * An encrypted blob that is opaque to the client and contains information about the challenge
+     * such as the nonce and the timestamp at which the challenge was issued.
+     *
+     * This is expected to be replayed by the client to the server and provides
+     * a way for the server to validate the response
+     */
     private final String mContext;
 
     /**
-     * Authorization endpoint will return accepted authorities.
+     * An array of strings consisting of all the trusted certificate issuers for the device certificate –
+     * typically this is the DRS.
+     * This is used by the client in order to determine the right certificate to be presented
+     * by the client in order to perform device authentication.
+     *
      * The mCertAuthorities could be empty when either no certificate or no permission for ADFS
      * service account for the Device container in AD.
      */
     private final List<String> mCertAuthorities;
 
     /**
-     * Token endpoint will return thumbprint.
+     * The thumbprint of the device certificate that the client MUST present in order to complete proof of possession.
+     * This is used by the client in order to determine the right certificate to be presented by the client in order to perform device authentication.
      */
     private final String mThumbprint;
 
+    /**
+     * The version number of this challenge response protocol.
+     */
     private final String mVersion;
 
     private final String mSubmitUrl;
 
-    protected PKeyAuthChallenge(final Builder builder) {
-        mNonce = builder.mNonce;
-        mContext = builder.mContext;
-        mCertAuthorities = builder.mCertAuthorities;
-        mThumbprint = builder.mThumbprint;
-        mVersion = builder.mVersion;
-        mSubmitUrl = builder.mSubmitUrl;
-    }
-
-    public static class Builder {
-        private String mNonce = "";
-        private String mContext = "";
-        private List<String> mCertAuthorities;
-        private String mThumbprint = "";
-        private String mVersion;
-        private String mSubmitUrl;
-
-        public Builder setNonce(final String nonce) {
-            mNonce = nonce;
-            return self();
-        }
-
-        public Builder setContext(final String context) {
-            mContext = context;
-            return self();
-        }
-
-        public Builder setCertAuthorities(final List<String> certAuthorities) {
-            mCertAuthorities = certAuthorities;
-            return self();
-        }
-
-        public Builder setThumbprint(final String thumbprint) {
-            mThumbprint = thumbprint;
-            return self();
-        }
-
-        public Builder setVersion(final String version) {
-            mVersion = version;
-            return self();
-        }
-
-        public Builder setSubmitUrl(final String submitUrl) {
-            mSubmitUrl = submitUrl;
-            return self();
-        }
-
-        public Builder self() {
-            return this;
-        }
-
-        public PKeyAuthChallenge build() {
-            return new PKeyAuthChallenge(this);
-        }
-    }
-
-    public String getNonce() {
-        return mNonce;
-    }
-
-    public String getContext() {
-        return mContext;
-    }
-
-    public List<String> getCertAuthorities() {
-        return mCertAuthorities;
-    }
-
-    public String getThumbprint() {
-        return mThumbprint;
-    }
-
-    public String getVersion() {
-        return mVersion;
-    }
-
-    public String getSubmitUrl() {
-        return mSubmitUrl;
-    }
-
     public Map<String, String> getChallengeHeader() throws ClientException {
         final String methodName = ":getChallengeHeader";
 
-        String authorizationHeaderValue = String.format("%s Context=\"%s\",Version=\"%s\"",
-                CHALLENGE_RESPONSE_TYPE, mContext, mVersion);
+        // Either cert thumbprint or authorities must present
+        // Otherwise, we won't have enough information to look for the cert.
+        //
+        // In such case, Client should ideally ignore this scenario /
+        // send a response which is equivalent to no certificate present on client.
+        if ((mCertAuthorities == null || mCertAuthorities.size() == 0) &&
+                StringUtil.isNullOrEmpty(mThumbprint)) {
+            Logger.info(TAG + methodName,
+                    "Both cert Authorities and Thumbprint are not provided." +
+                            "Sending a response which is equivalent to no certificate present on client.");
+            return getChallengeHeaderWithoutSignedJwt();
+        }
 
         // If not device cert exists, alias or private key will not exist on the device
         // Suppressing unchecked warnings due to the generic type not provided in the object returned from method getDeviceCertificateProxy
@@ -173,35 +127,62 @@ public class PKeyAuthChallenge implements Serializable {
         final Class<IDeviceCertificate> certClazz =
                 (Class<IDeviceCertificate>) AuthenticationSettings.INSTANCE.getDeviceCertificateProxy();
 
-        if (certClazz != null) {
-            IDeviceCertificate deviceCertProxy = getWPJAPIInstance(certClazz);
-            if (deviceCertProxy.isValidIssuer(mCertAuthorities)
-                    || StringUtil.equalsIgnoreCase(deviceCertProxy.getThumbPrint(), mThumbprint)) {
-                final PrivateKey privateKey = deviceCertProxy.getPrivateKey();
-                if (privateKey == null) {
-                    throw new ClientException(ErrorStrings.KEY_CHAIN_PRIVATE_KEY_EXCEPTION);
-                }
-                final PublicKey publicKey = deviceCertProxy.getPublicKey();
-                final X509Certificate certificate = deviceCertProxy.getCertificate();
-                if (certificate == null) {
-                    throw new ClientException(ErrorStrings.KEY_CHAIN_CERTIFICATE_EXCEPTION);
-                }
-                final String jwt = (new JWSBuilder()).generateSignedJWT(
-                        mNonce,
-                        mSubmitUrl,
-                        privateKey,
-                        publicKey,
-                        certificate);
-                authorizationHeaderValue = String.format(
-                        "%s AuthToken=\"%s\",Context=\"%s\",Version=\"%s\"",
-                        CHALLENGE_RESPONSE_TYPE, jwt, mContext, mVersion);
-                Logger.info(TAG + methodName, "Receive challenge response. ");
-            }
+        if (certClazz == null) {
+            Logger.warn(TAG + methodName, "Device Certificate Proxy is not initialized");
+            return getChallengeHeaderWithoutSignedJwt();
         }
+
+        final IDeviceCertificate deviceCertProxy = getWPJAPIInstance(certClazz);
+        if (!deviceCertProxy.isValidIssuer(mCertAuthorities)
+                && !StringUtil.equalsIgnoreCase(deviceCertProxy.getThumbPrint(), mThumbprint)) {
+            Logger.info(TAG + methodName,
+                    "Cannot find a certificate matching the provided authority.");
+            return getChallengeHeaderWithoutSignedJwt();
+        }
+
+        return getChallengeHeaderWithSignedJwt(deviceCertProxy);
+    }
+
+    /**
+     * Returns a "we can do nothing" response to the server.
+     */
+    private Map<String, String> getChallengeHeaderWithoutSignedJwt() {
+        final Map<String, String> headers = new HashMap<>();
+        headers.put(CHALLENGE_RESPONSE_HEADER,
+                String.format("%s Context=\"%s\",Version=\"%s\"",
+                        CHALLENGE_RESPONSE_TYPE, mContext, mVersion));
+        return headers;
+    }
+
+    /**
+     * Generates a signed jwt and return it as part of the challenge header.
+     */
+    private Map<String, String> getChallengeHeaderWithSignedJwt(@NonNull final IDeviceCertificate deviceCertProxy) throws ClientException {
+        final String methodName = ":generateChallengeResponse";
+
+        final PrivateKey privateKey = deviceCertProxy.getPrivateKey();
+        if (privateKey == null) {
+            throw new ClientException(ErrorStrings.KEY_CHAIN_PRIVATE_KEY_EXCEPTION);
+        }
+        final PublicKey publicKey = deviceCertProxy.getPublicKey();
+        final X509Certificate certificate = deviceCertProxy.getCertificate();
+        if (certificate == null) {
+            throw new ClientException(ErrorStrings.KEY_CHAIN_CERTIFICATE_EXCEPTION);
+        }
+        final String jwt = (new JWSBuilder()).generateSignedJWT(
+                mNonce,
+                mSubmitUrl,
+                privateKey,
+                publicKey,
+                certificate);
+
+        Logger.info(TAG + methodName, "Generated a signed challenge response.");
 
         final Map<String, String> headers = new HashMap<>();
         headers.put(CHALLENGE_RESPONSE_HEADER,
-                authorizationHeaderValue);
+                String.format(
+                        "%s AuthToken=\"%s\",Context=\"%s\",Version=\"%s\"",
+                        CHALLENGE_RESPONSE_TYPE, jwt, mContext, mVersion));
         return headers;
     }
 
