4808fbf115701c98e080c519e7569b4b6ec9f937
Moses Gitau <gitaumoses4@gmail.com>
2022-04-26 19:34:00 +0300

    Rebase onto dev

--- common4j/src/main/com/microsoft/identity/common/java/network/NetworkInterface.java
+++ common4j/src/main/com/microsoft/identity/common/java/network/NetworkInterface.java
@@ -1,3 +1,25 @@
+// Copyright (c) Microsoft Corporation.
+// All rights reserved.
+//
+// This code is licensed under the MIT License.
+//
+// Permission is hereby granted, free of charge, to any person obtaining a copy
+// of this software and associated documentation files(the "Software"), to deal
+// in the Software without restriction, including without limitation the rights
+// to use, copy, modify, merge, publish, distribute, sublicense, and / or sell
+// copies of the Software, and to permit persons to whom the Software is
+// furnished to do so, subject to the following conditions :
+//
+// The above copyright notice and this permission notice shall be included in
+// all copies or substantial portions of the Software.
+//
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+// THE SOFTWARE.
 package com.microsoft.identity.common.java.network;
 
 /**
@@ -53,7 +75,7 @@ public enum NetworkInterface {
 
 
     /**
-     * Set the interfaceType from a string representation of {@link NetworkInterface}
+     * Get the interfaceType from a string representation of {@link NetworkInterface}
      *
      * @param value a string representation of {@link NetworkInterface}
      */
@@ -63,6 +85,6 @@ public enum NetworkInterface {
                 return interfaceType;
             }
         }
-        return null;
+        throw new IllegalArgumentException("NetworkInterface cannot be " + value);
     }
 }
