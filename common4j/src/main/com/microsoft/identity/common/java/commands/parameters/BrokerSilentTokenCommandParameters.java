5af058fe45a571fe2b6c24ae9756bce8170cb404
pedro romero vargas <pedroro@microsoft.com>
2022-08-29 17:19:13 -0700

    add comments

--- common4j/src/main/com/microsoft/identity/common/java/commands/parameters/BrokerSilentTokenCommandParameters.java
+++ common4j/src/main/com/microsoft/identity/common/java/commands/parameters/BrokerSilentTokenCommandParameters.java
@@ -48,6 +48,9 @@ public class BrokerSilentTokenCommandParameters extends SilentTokenCommandParame
     private final int sleepTimeBeforePrtAcquisition;
 
     private final String negotiatedBrokerProtocolVersion;
+
+    // If this flag is true, we will request to send x-ms-PKeyAuth Header to the token endpoint.
+    // This flag is transferred to a MicrosoftTokenRequest in BaseController.
     private final boolean pKeyAuthHeaderAllowed;
 
     private final BrokerRequestType requestType;
