375e2c88b55e7ee59baff3a77c2b4bb635c098e0
Fadi Durah <fadidurah@microsoft.com>
2022-09-01 08:13:14 -0700

    increase wait time for FIND_UI_WAIT_TIMEOUT

--- uiautomationutilities/src/main/java/com/microsoft/identity/client/ui/automation/utils/CommonUtils.java
+++ uiautomationutilities/src/main/java/com/microsoft/identity/client/ui/automation/utils/CommonUtils.java
@@ -47,7 +47,7 @@ import java.util.concurrent.TimeUnit;
 public class CommonUtils {
 
     private final static String TAG = CommonUtils.class.getSimpleName();
-    public final static long FIND_UI_ELEMENT_TIMEOUT = TimeUnit.SECONDS.toMillis(5);
+    public final static long FIND_UI_ELEMENT_TIMEOUT = TimeUnit.SECONDS.toMillis(12);
 
     private final static String SD_CARD = "/sdcard";
 
