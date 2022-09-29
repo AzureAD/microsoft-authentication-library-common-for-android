629848a3a66ef0ab23a7f161bc0796b7b5bf8af4
Shahzaib <shahzaib.jameel@microsoft.com>
2022-09-29 14:08:30 -0700

    Add javadoc

--- common4j/src/main/com/microsoft/identity/common/java/opentelemetry/AttributeName.java
+++ common4j/src/main/com/microsoft/identity/common/java/opentelemetry/AttributeName.java
@@ -26,7 +26,16 @@ package com.microsoft.identity.common.java.opentelemetry;
  * Names of Open Telemetry Span Attributes we want to capture for broker's Spans.
  */
 public enum AttributeName {
+    /**
+     * The length of the response body returned from network request.
+     */
     response_body_length,
+    /**
+     * Indicates if the JWT returned by eSTS is a valid JWT.
+     */
     jwt_valid,
+    /**
+     * Indicates the algorithm for the JWE returned by eSTS.
+     */
     jwt_alg
 }
