7b906bfdd6e59d35e0d98b61d19a80f15d9b7dfd
Fadi Durah <fadidurah@microsoft.com>
2022-10-12 07:04:24 -0700

    annotations kept through runtime

--- uiautomationutilities/src/main/java/com/microsoft/identity/client/ui/automation/annotations/DoNotRunOnPipeline.java
+++ uiautomationutilities/src/main/java/com/microsoft/identity/client/ui/automation/annotations/DoNotRunOnPipeline.java
@@ -1,10 +1,17 @@
 package com.microsoft.identity.client.ui.automation.annotations;
 
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
 /**
  * An annotation indicating that an automated End-to-End test case is not fit to be ran on the UI automation
  * pipeline. A pipeline has to include the argument to ignore tests with this annotation in the test targets option
  * for this to take effect.
  */
+@Retention(RetentionPolicy.RUNTIME)
+@Target({ElementType.METHOD, ElementType.TYPE})
 public @interface DoNotRunOnPipeline {
     String value() default "";
 }
--- uiautomationutilities/src/main/java/com/microsoft/identity/client/ui/automation/annotations/RunOnAPI29Minus.java
+++ uiautomationutilities/src/main/java/com/microsoft/identity/client/ui/automation/annotations/RunOnAPI29Minus.java
@@ -1,5 +1,10 @@
 package com.microsoft.identity.client.ui.automation.annotations;
 
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
 /**
  * An annotation indicating that an automated End-to-End test case should be ran on a test device with API 29 or lower.
  * Tests not marked with this annotation are assumed to be compatible with API 30+
@@ -8,6 +13,8 @@ package com.microsoft.identity.client.ui.automation.annotations;
  *      - Some apps not behaving correctly on API 30+ i.e. Azure Sample app
  *      - WebView seems to be easier to test on API 29-.
  */
+@Retention(RetentionPolicy.RUNTIME)
+@Target({ElementType.METHOD, ElementType.TYPE})
 public @interface RunOnAPI29Minus {
     String value() default "";
 }
