47cf6cf1e9f610c56df03eb278b16c4d245d3ee5
Fadi Durah <fadidurah@microsoft.com>
2022-10-19 18:59:48 -0700

    re-instate powerlift rule

--- uiautomationutilities/src/main/java/com/microsoft/identity/client/ui/automation/rules/PowerLiftIncidentRule.java
+++ uiautomationutilities/src/main/java/com/microsoft/identity/client/ui/automation/rules/PowerLiftIncidentRule.java
@@ -2,7 +2,6 @@ package com.microsoft.identity.client.ui.automation.rules;
 
 import android.text.TextUtils;
 
-import com.microsoft.identity.client.ui.automation.app.App;
 import com.microsoft.identity.client.ui.automation.powerlift.IPowerLiftIntegratedApp;
 import com.microsoft.identity.client.ui.automation.logging.Logger;
 import com.microsoft.identity.client.ui.automation.powerlift.ThrowableWithPowerLiftIncident;
@@ -30,35 +29,35 @@ public class PowerLiftIncidentRule implements TestRule {
             @Override
             public void evaluate() throws Throwable {
                 Logger.i(TAG, "Applying rule....");
-//                try {
+                try {
                     base.evaluate();
-//                } catch (final Throwable originalThrowable) {
-//                    String powerLiftIncidentDetails = null;
-//                    try {
-//                        Logger.e(
-//                                TAG,
-//                                "Encountered error during test....creating PowerLift incident.",
-//                                originalThrowable
-//                        );
-//                        powerLiftIncidentDetails = powerLiftIntegratedApp.createPowerLiftIncident();
-//                    } catch (final Throwable powerLiftError) {
-//                        Logger.e(
-//                                TAG,
-//                                "Oops...something went wrong...unable to create PowerLift incident.",
-//                                powerLiftError
-//                        );
-//                    }
-//                    if (TextUtils.isEmpty(powerLiftIncidentDetails)) {
-//                        throw originalThrowable;
-//                    } else {
-//                        assert powerLiftIncidentDetails != null;
-//                        throw new ThrowableWithPowerLiftIncident(
-//                                powerLiftIntegratedApp,
-//                                powerLiftIncidentDetails,
-//                                originalThrowable
-//                        );
-//                    }
-//                }
+                } catch (final Throwable originalThrowable) {
+                    String powerLiftIncidentDetails = null;
+                    try {
+                        Logger.e(
+                                TAG,
+                                "Encountered error during test....creating PowerLift incident.",
+                                originalThrowable
+                        );
+                        powerLiftIncidentDetails = powerLiftIntegratedApp.createPowerLiftIncident();
+                    } catch (final Throwable powerLiftError) {
+                        Logger.e(
+                                TAG,
+                                "Oops...something went wrong...unable to create PowerLift incident.",
+                                powerLiftError
+                        );
+                    }
+                    if (TextUtils.isEmpty(powerLiftIncidentDetails)) {
+                        throw originalThrowable;
+                    } else {
+                        assert powerLiftIncidentDetails != null;
+                        throw new ThrowableWithPowerLiftIncident(
+                                powerLiftIntegratedApp,
+                                powerLiftIncidentDetails,
+                                originalThrowable
+                        );
+                    }
+                }
             }
         };
     }
