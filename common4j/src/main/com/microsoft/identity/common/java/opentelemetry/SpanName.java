8c3abcc4675c8a72921bd67ce3768a710af739cf
Sowmya Malayanur <somalaya@microsoft.com>
2022-10-25 19:23:29 -0700

    rebased with dev

--- common4j/src/main/com/microsoft/identity/common/java/opentelemetry/SpanName.java
+++ common4j/src/main/com/microsoft/identity/common/java/opentelemetry/SpanName.java
@@ -29,5 +29,7 @@ public enum SpanName {
     AcquireTokenInteractive,
     AcquireTokenSilent,
     CryptoFactoryEvent,
-    SetScopeForDMAgentForFoci
+    SetScopeForDMAgentForFoci,
+    GetAccounts,
+    RemoveAccounts
 }
