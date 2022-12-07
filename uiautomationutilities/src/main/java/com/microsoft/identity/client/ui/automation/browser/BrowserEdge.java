4f6a8a13aedacc46af9398fd15f6bebdb045271f
ellymakuba <ellymakuba@gmail.com>
2022-12-07 11:27:17 +0300

    merge dev

--- uiautomationutilities/src/main/java/com/microsoft/identity/client/ui/automation/browser/BrowserEdge.java
+++ uiautomationutilities/src/main/java/com/microsoft/identity/client/ui/automation/browser/BrowserEdge.java
@@ -53,32 +53,10 @@ public class BrowserEdge extends App implements IBrowser {
 
     @Override
     public void handleFirstRun() {
-<<<<<<< HEAD
-        if (shouldHandleFirstRun) {
-            Logger.i(TAG, "Handle First Run of Browser..");
-            // cancel sync in Edge
-            UiAutomatorUtils.handleButtonClick("com.microsoft.emmx:id/not_now");
-            sleep(); // need to use sleep due to Edge animations
-            // cancel sharing data
-            UiAutomatorUtils.handleButtonClick("com.microsoft.emmx:id/not_now");
-            sleep(); // need to use sleep due to Edge animations
-            // cancel personalization
-            UiAutomatorUtils.handleButtonClick("com.microsoft.emmx:id/fre_share_not_now");
-            sleep();// need to use sleep due to Edge animations
-            // avoid setting default
-            UiAutomatorUtils.handleButtonClick("com.microsoft.emmx:id/no");
-            sleep();// need to use sleep due to Edge animations
-            shouldHandleFirstRun = false;
-        }
-=======
         Logger.i(TAG, "Handle First Run of Browser..");
         // cancel sync in Edge
         UiAutomatorUtils.handleButtonClickForObjectWithText("Not now");
         sleep(); // need to use sleep due to Edge animations
-<<<<<<< HEAD
->>>>>>> 3d9d7aae5 (implementing custom email and password field handlers for edge)
-=======
->>>>>>> 3d9d7aae5c364e89bcbf36ed99120a78e7f2a808
     }
 
     @Override
