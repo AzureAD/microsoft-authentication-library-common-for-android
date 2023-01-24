ef069aedd6d680256430eb44ac83a8547bfd9778
pedro romero vargas <76129899+p3dr0rv@users.noreply.github.com>
2023-01-24 14:10:25 -0800

    Update ClientException.java

--- common4j/src/main/com/microsoft/identity/common/java/exception/ClientException.java
+++ common4j/src/main/com/microsoft/identity/common/java/exception/ClientException.java
@@ -83,7 +83,7 @@ public class ClientException extends BaseException {
     public static final String SCOPE_EMPTY_OR_NULL = "scope_empty_or_null";
 
     /**
-     * Emitted when the device Token is not present in successful response
+     * Emitted when the device Token is not present in successful response.
      */
     public static final String DEVICE_TOKEN_EMPTY_OR_NULL = "device_token_empty_or_null";
 
