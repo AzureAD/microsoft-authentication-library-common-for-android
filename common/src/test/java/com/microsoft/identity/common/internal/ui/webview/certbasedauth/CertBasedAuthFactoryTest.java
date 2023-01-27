00a26cc7afa586794ebbb94f36ef9b4a3c2f0d20
Melissa Ahn <melissaahn@microsoft.com>
2023-01-26 16:57:06 -0800

    finished factory tests

--- common/src/test/java/com/microsoft/identity/common/internal/ui/webview/certbasedauth/CertBasedAuthFactoryTest.java
+++ common/src/test/java/com/microsoft/identity/common/internal/ui/webview/certbasedauth/CertBasedAuthFactoryTest.java
@@ -23,8 +23,10 @@
 package com.microsoft.identity.common.internal.ui.webview.certbasedauth;
 
 import static org.junit.Assert.assertNotNull;
+import static org.junit.Assert.assertNull;
 import static org.junit.Assert.assertTrue;
 
+import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 
 import com.microsoft.identity.common.shadows.ShadowCertBasedAuthTelemetryHelper;
@@ -40,9 +42,16 @@ import java.util.ArrayList;
 @RunWith(RobolectricTestRunner.class)
 @Config(shadows={ShadowCertBasedAuthTelemetryHelper.class})
 public class CertBasedAuthFactoryTest extends AbstractCertBasedAuthTest {
+
     private CertBasedAuthFactory mFactory;
     protected TestUsbSmartcardCertBasedAuthManager mUsbManager;
     protected TestNfcSmartcardCertBasedAuthManager mNfcManager;
+
+    private enum ExpectedChallengeHandler {
+        USB,
+        NFC,
+        NULL
+    }
 
     @Before
     public void factorySetUp() {
@@ -54,22 +63,13 @@ public class CertBasedAuthFactoryTest extends AbstractCertBasedAuthTest {
     @Test
     public void testInitiallyUsbConnected() {
         mUsbManager.mockConnect();
-        mFactory.createCertBasedAuthChallengeHandler(new CertBasedAuthFactory.CertBasedAuthChallengeHandlerCallback() {
-            @Override
-            public void onReceived(@Nullable ICertBasedAuthChallengeHandler challengeHandler) {
-                assertTrue(challengeHandler instanceof UsbSmartcardCertBasedAuthChallengeHandler);
-            }
-        });
+        challengeHandlerHelper(ExpectedChallengeHandler.USB);
     }
 
     @Test
     public void testCancelAtUserChoiceDialog() {
-        mFactory.createCertBasedAuthChallengeHandler(new CertBasedAuthFactory.CertBasedAuthChallengeHandlerCallback() {
-            @Override
-            public void onReceived(@Nullable ICertBasedAuthChallengeHandler challengeHandler) {
-                //nothing needed
-            }
-        });
+        challengeHandlerHelper(ExpectedChallengeHandler.NULL);
+        checkIfCorrectDialogIsShowing(TestDialog.user_choice);
         final UserChoiceDialog.CancelCbaCallback callback = mDialogHolder.getMUserChoiceCancelCbaCallback();
         assertNotNull(callback);
         callback.onCancel();
@@ -77,17 +77,68 @@ public class CertBasedAuthFactoryTest extends AbstractCertBasedAuthTest {
     }
 
     @Test
-    public void testCancelAtPromptDialog() {
+    public void testPlugInAtUserChoiceDialog() {
+        challengeHandlerHelper(ExpectedChallengeHandler.USB);
+        checkIfCorrectDialogIsShowing(TestDialog.user_choice);
+        mUsbManager.mockConnect();
+        final UserChoiceDialog.PositiveButtonListener listener = mDialogHolder.getMUserChoicePositiveButtonListener();
+        assertNotNull(listener);
+        listener.onClick(1);
+    }
 
+    @Test
+    public void testCancelAtPromptDialog() {
+        challengeHandlerHelper(ExpectedChallengeHandler.NULL);
+        checkIfCorrectDialogIsShowing(TestDialog.user_choice);
+        final UserChoiceDialog.PositiveButtonListener listener = mDialogHolder.getMUserChoicePositiveButtonListener();
+        assertNotNull(listener);
+        listener.onClick(1);
+        checkIfCorrectDialogIsShowing(TestDialog.prompt);
+        final SmartcardPromptDialog.CancelCbaCallback callback = mDialogHolder.getMPromptCancelCbaCallback();
+        assertNotNull(callback);
+        callback.onCancel();
+        checkIfCorrectDialogIsShowing(null);
     }
 
     @Test
     public void testChooseSmartcardAndProceedWithUsb() {
-
+        challengeHandlerHelper(ExpectedChallengeHandler.USB);
+        checkIfCorrectDialogIsShowing(TestDialog.user_choice);
+        final UserChoiceDialog.PositiveButtonListener listener = mDialogHolder.getMUserChoicePositiveButtonListener();
+        assertNotNull(listener);
+        listener.onClick(1);
+        checkIfCorrectDialogIsShowing(TestDialog.prompt);
+        mUsbManager.mockConnect();
     }
 
     @Test
     public void testChooseSmartcardAndProceedWithNfc() {
+        challengeHandlerHelper(ExpectedChallengeHandler.NFC);
+        checkIfCorrectDialogIsShowing(TestDialog.user_choice);
+        final UserChoiceDialog.PositiveButtonListener listener = mDialogHolder.getMUserChoicePositiveButtonListener();
+        assertNotNull(listener);
+        listener.onClick(1);
+        checkIfCorrectDialogIsShowing(TestDialog.prompt);
+        mNfcManager.mockConnect(false);
+        checkIfCorrectDialogIsShowing(TestDialog.nfc_loading);
+    }
 
+    private void challengeHandlerHelper(@NonNull final ExpectedChallengeHandler expectedChallengeHandler) {
+        mFactory.createCertBasedAuthChallengeHandler(new CertBasedAuthFactory.CertBasedAuthChallengeHandlerCallback() {
+            @Override
+            public void onReceived(@Nullable ICertBasedAuthChallengeHandler challengeHandler) {
+                switch (expectedChallengeHandler) {
+                    case USB:
+                        assertTrue(challengeHandler instanceof UsbSmartcardCertBasedAuthChallengeHandler);
+                        break;
+                    case NFC:
+                        assertTrue(challengeHandler instanceof NfcSmartcardCertBasedAuthChallengeHandler);
+                        break;
+                    default:
+                        assertNull(challengeHandler);
+                        break;
+                }
+            }
+        });
     }
 }
