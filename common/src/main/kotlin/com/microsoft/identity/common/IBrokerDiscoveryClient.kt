0b16b760f2bd5de73f488e7bbc7a5fdbd2a37215
Dome Pongmongkol <rapong@microsoft.com>
2023-02-21 15:42:39 -0600

    add comment

--- common/src/main/kotlin/com/microsoft/identity/common/IBrokerDiscoveryClient.kt
+++ common/src/main/kotlin/com/microsoft/identity/common/IBrokerDiscoveryClient.kt
@@ -3,5 +3,14 @@ package com.microsoft.identity.common
 import com.microsoft.identity.common.internal.broker.BrokerData
 
 interface IBrokerDiscoveryClient {
+
+    /**
+     * Performs a discovery to figure out which broker app the SDK (MSAL/OneAuth)
+     * has to send its request to.
+     *
+     * @param forceTrigger If true, this will skip cached value (if any)
+     *                     and force triggering the broker election process.
+     * @return BrokerData package name and signature hash of the targeted app.
+     * **/
     fun getActiveBroker(forceTrigger: Boolean) : BrokerData?
 }
