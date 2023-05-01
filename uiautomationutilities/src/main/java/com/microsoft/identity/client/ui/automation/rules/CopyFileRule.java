6f5c2dba2be4dc705a57ef67e7401a6e4ca8841a
Fadi Durah <fadidurah@microsoft.com>
2023-05-01 19:51:13 -0400

    comments

--- uiautomationutilities/src/main/java/com/microsoft/identity/client/ui/automation/rules/CopyFileRule.java
+++ uiautomationutilities/src/main/java/com/microsoft/identity/client/ui/automation/rules/CopyFileRule.java
@@ -1,3 +1,25 @@
+// Copyright (c) Microsoft Corporation.
+// All rights reserved.
+//
+// This code is licensed under the MIT License.
+//
+// Permission is hereby granted, free of charge, to any person obtaining a copy
+// of this software and associated documentation files(the "Software"), to deal
+// in the Software without restriction, including without limitation the rights
+// to use, copy, modify, merge, publish, distribute, sublicense, and / or sell
+// copies of the Software, and to permit persons to whom the Software is
+// furnished to do so, subject to the following conditions :
+//
+// The above copyright notice and this permission notice shall be included in
+// all copies or substantial portions of the Software.
+//
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+// THE SOFTWARE.
 package com.microsoft.identity.client.ui.automation.rules;
 
 import androidx.annotation.NonNull;
@@ -18,11 +40,18 @@ import org.junit.rules.TestRule;
 import org.junit.runner.Description;
 import org.junit.runners.model.Statement;
 
+/**
+ * We use this rule to copy files from a particular directory in Android device to another directory,
+ * in case firebase does not allow us to push to and install apks from the same directory.
+ */
 public class CopyFileRule implements TestRule {
     private final static String TAG = CopyFileRule.class.getSimpleName();
 
-    private String mSourceFolder;
-    private String mDestFolder;
+    private final static String DEFAULT_SOURCE_FOLDER = "/sdcard/";
+    private final static String DEFAULT_DESTINATION_FOLDER = "/data/local/tmp/";
+
+    private final String mSourceFolder;
+    private final String mDestFolder;
     private final String[] mApkFileNames = {
             BrokerHost.BROKER_HOST_APK,
             BrokerHost.OLD_BROKER_HOST_APK,
@@ -41,8 +70,8 @@ public class CopyFileRule implements TestRule {
     };
 
     public CopyFileRule() {
-        mSourceFolder = "/sdcard/";
-        mDestFolder = "/data/local/tmp/";
+        mSourceFolder = DEFAULT_SOURCE_FOLDER;
+        mDestFolder = DEFAULT_DESTINATION_FOLDER;
     }
 
     public CopyFileRule(@NonNull final String sourceFolder, @NonNull final String destFolder) {
