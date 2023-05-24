7b0e2316123b3529a18a855083685c73d601198a
pedro romero vargas <pedroro@microsoft.com>
2023-05-23 17:31:10 -0700

    add removeTrailingSlash

--- common4j/src/main/com/microsoft/identity/common/java/util/UrlUtil.java
+++ common4j/src/main/com/microsoft/identity/common/java/util/UrlUtil.java
@@ -230,4 +230,9 @@ public class UrlUtil {
             throw new RuntimeException(e);
         }
     }
+
+    public static String removeTrailingSlash(@NonNull final String urlString) {
+        final String uri = urlString.replaceFirst("/*$", "");
+        return uri;
+    }
 }
