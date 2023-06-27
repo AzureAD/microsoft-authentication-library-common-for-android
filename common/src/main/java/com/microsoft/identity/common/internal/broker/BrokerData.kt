18d4cfb4e0ff7172f865a126a003b67f30bc4b55
Dean Xu <cikelengfeng@gmail.com>
2023-06-27 16:02:15 +0800

    Hardcode sShouldTrustDebugBrokers

--- common/src/main/java/com/microsoft/identity/common/internal/broker/BrokerData.kt
+++ common/src/main/java/com/microsoft/identity/common/internal/broker/BrokerData.kt
@@ -65,7 +65,7 @@ data class BrokerData(val packageName : String,
          * Determines if the debug brokers should be trusted or not.
          * This should only be set to true only during testing.
          */
-        val sShouldTrustDebugBrokers = BuildConfig.DEBUG
+        val sShouldTrustDebugBrokers = true
 
         @JvmStatic
         val debugMicrosoftAuthenticator = BrokerData(
