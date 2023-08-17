913b8b2bdfe63124855555fa3ed44dc6586895dd
Robert Mccahill <rmccahill@microsoft.com>
2023-08-17 15:54:15 +0000

    Merged PR 9387: Use BuildConfig to set useRealAuthority and DC parameters
    Sets `useRealAuthority` and `DC` parameters through the BuildConfig via gradle.
    
    Note: this will set `useRealAuthority` to `true` by default now. The reasoning being that the 'default' behaviour should be to validate the incoming authority and to hit the authority passed in, and disabling this should only be used for development and testing.
    
    To set either the `useRealAuthority` or `DC` parameters, set those variables in either the project or global `gradle.properties`. I'd recommend using the global properties file as that way it won't be impacted by switching branches and won't be committed to source control. Setting the parameters would look like the following:
    
    ```
    useRealAuthority=true
    dc=ESTS-PUB-SCUS-LZ1-FD000-TEST1
    ```
    
    Note: Numerous test cases rely on `useRealAuthority` to be false in order to use the mock API, so when running local tests, it should be set to false. This is also the case for the CI pipelines, hence the update to the `azure-pipelines.yml`
    
    Related work items: #2629229

--- common4j/src/main/com/microsoft/identity/common/java/providers/nativeauth/NativeAuthOAuth2Configuration.kt
+++ common4j/src/main/com/microsoft/identity/common/java/providers/nativeauth/NativeAuthOAuth2Configuration.kt
@@ -23,6 +23,8 @@
 
 package com.microsoft.identity.common.java.providers.nativeauth
 
+import com.microsoft.identity.common.java.BuildConfig
+import com.microsoft.identity.common.java.BuildValues
 import com.microsoft.identity.common.java.logging.LogSession
 import com.microsoft.identity.common.java.providers.microsoft.microsoftsts.MicrosoftStsOAuth2Configuration
 import com.microsoft.identity.common.java.util.UrlUtil
@@ -36,8 +38,7 @@ class NativeAuthOAuth2Configuration(
     val challengeType: String,
     // Need this to decide whether or not to return mock api authority or actual authority supplied in configuration
     // Turn this on if you plan to use web auth and/or open id configuration
-    // TODO make this configurable: https://identitydivision.visualstudio.com/Engineering/_workitems/edit/2629229
-    val useRealAuthority: Boolean = false
+    val useRealAuthority: Boolean = BuildValues.USE_REAL_AUTHORITY
 ) : MicrosoftStsOAuth2Configuration() {
 
     private val TAG = NativeAuthOAuth2Configuration::class.java.simpleName
@@ -211,9 +212,11 @@ class NativeAuthOAuth2Configuration(
     private fun getEndpointUrlFromRootAndTenantAndSuffix(root: URL, endpointSuffix: String): URL {
         LogSession.logMethodCall(tag = TAG)
         return try {
-            // TODO make this configurable: https://identitydivision.visualstudio.com/Engineering/_workitems/edit/2629229
-//            UrlUtil.appendPathToURL(root, endpointSuffix, "dc=ESTS-PUB-WUS3-AZ1-FD000-TEST1")
-            UrlUtil.appendPathToURL(root, endpointSuffix, null)
+            if (BuildValues.DC.isNotEmpty()) {
+                UrlUtil.appendPathToURL(root, endpointSuffix, "dc=${BuildValues.DC}")
+            } else {
+                UrlUtil.appendPathToURL(root, endpointSuffix, null)
+            }
         } catch (e: URISyntaxException) {
             LogSession.logException(tag = TAG, throwable = e)
             throw e
