b2e79fdbe0b4cf0417c57e5065f10fe87c10e981
Melissa Ahn <melissaahn@microsoft.com>
2023-08-30 14:43:19 -0700

    removed CredMan dependency

--- common/src/main/java/com/microsoft/identity/common/internal/fido/CredManApiFidoManager.kt
+++ common/src/main/java/com/microsoft/identity/common/internal/fido/CredManApiFidoManager.kt
@@ -1,17 +1,12 @@
 package com.microsoft.identity.common.internal.fido
 
 import android.content.Context
-import androidx.credentials.CredentialManager
-import androidx.credentials.GetCredentialRequest
-import androidx.credentials.GetPublicKeyCredentialOption
-import androidx.credentials.PublicKeyCredential
 
 class CredManApiFidoManager(val context: Context) : IFidoManager {
-    val credentialManager = CredentialManager.create(context)
+    //val credentialManager = CredentialManager.create(context)
 
     override suspend fun authenticate(challenge: AuthFidoChallenge): String {
-        val requestJson = WebAuthnJsonUtil.createJsonAuthRequestFromChallengeObject(challenge)
-        val publicKeyCredentialOption = GetPublicKeyCredentialOption(requestJson)
+        /*        val publicKeyCredentialOption = GetPublicKeyCredentialOption(requestJson)
         val getRequest = GetCredentialRequest(listOf(publicKeyCredentialOption))
         //This could throw an exception, which should later be handled in getExceptionMessage.
         val result = credentialManager.getCredential(
@@ -19,7 +14,9 @@ class CredManApiFidoManager(val context: Context) : IFidoManager {
             context = context,
         )
         val credential = result.credential as PublicKeyCredential
-        return credential.authenticationResponseJson
+        return credential.authenticationResponseJson*/
+        //Making sure util method is called so we can appropriately test sizing.
+        return WebAuthnJsonUtil.createJsonAuthRequestFromChallengeObject(challenge)
     }
 
     override fun getExceptionMessage(exception: Exception): String {
