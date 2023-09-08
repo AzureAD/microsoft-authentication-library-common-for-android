6b083182345c063553965ec45e96c9cfe41c49ac
Saurabh Gautam <saugautam@microsoft.com>
2023-09-08 06:54:19 +0100

    Update build config

--- common4j/src/main/com/microsoft/identity/common/java/BuildValues.java
+++ common4j/src/main/com/microsoft/identity/common/java/BuildValues.java
@@ -7,9 +7,17 @@ import androidx.annotation.NonNull;
 public class BuildValues {
     //Appended to the URL constructed in NativeAuthOAuth2Configuration, used for making calls to tenants on test slices
     @NonNull
-    public static final String DC = BuildConfig.DC;
+    public static String DC = BuildConfig.DC;
+    
+    public static void setDC(String dc) {
+        DC = dc;
+    }    
 
     //The mock API authority used for testing will be rejected by validation logic run on instantiation. This flag is used to bypass those checks in various points in the application
     @NonNull
-    public static final Boolean USE_REAL_AUTHORITY = BuildConfig.USE_REAL_AUTHORITY;
+    public static Boolean USE_REAL_AUTHORITY = BuildConfig.USE_REAL_AUTHORITY;
+    
+    public static void setUseRealAuthority(Boolean ura) {
+        USE_REAL_AUTHORITY = ura;
+    }
 }
