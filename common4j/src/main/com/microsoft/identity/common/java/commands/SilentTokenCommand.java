0c1374e8b50a4c8b16dbb21cf600f76a15ef7c0b
Shahzaib <shahzaib.jameel@microsoft.com>
2023-11-09 10:52:01 -0800

    Increase silent token timeout to 90 seconds

--- common4j/src/main/com/microsoft/identity/common/java/commands/SilentTokenCommand.java
+++ common4j/src/main/com/microsoft/identity/common/java/commands/SilentTokenCommand.java
@@ -49,7 +49,7 @@ import lombok.NonNull;
 @EqualsAndHashCode(callSuper = true)
 public class SilentTokenCommand extends TokenCommand {
 
-    public static final int ACQUIRE_TOKEN_SILENT_DEFAULT_TIMEOUT_MILLISECONDS = 60000;
+    public static final int ACQUIRE_TOKEN_SILENT_DEFAULT_TIMEOUT_MILLISECONDS = 90000;
 
     private static final String TAG = SilentTokenCommand.class.getSimpleName();
 
