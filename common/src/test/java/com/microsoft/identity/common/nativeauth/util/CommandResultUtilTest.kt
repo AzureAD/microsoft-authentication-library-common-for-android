1e7bce0e7839d213fbbbce8e92bfe3a9472a1694
Saurabh Gautam <saugautam@microsoft.com>
2023-12-12 17:20:54 +0000

    Move test files

--- common/src/test/java/com/microsoft/identity/common/nativeauth/util/CommandResultUtilTest.kt
+++ common/src/test/java/com/microsoft/identity/common/nativeauth/util/CommandResultUtilTest.kt
@@ -20,7 +20,7 @@
 //  LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
 //  OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
 //  THE SOFTWARE.
-package com.microsoft.identity.common.java.nativeauth.util
+package com.microsoft.identity.common.nativeauth.util
 
 import com.microsoft.identity.common.java.commands.ICommandResult.ResultStatus
 import com.microsoft.identity.common.java.controllers.CommandResult
