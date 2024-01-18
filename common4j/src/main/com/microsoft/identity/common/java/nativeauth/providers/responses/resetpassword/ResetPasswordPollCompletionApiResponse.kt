4900edb7f9db5952474586f83efd37b90995cde8
Robert McCahill <130065853+rmccahill@users.noreply.github.com>
2024-01-18 16:45:24 +0000

    Update Native Auth password reset API response (#2281)
    Update relevant response classes to include the
    continuation token returned from the updated poll completion response,
    and updates constructors in test classes to reflect the change.

--- common4j/src/main/com/microsoft/identity/common/java/nativeauth/providers/responses/resetpassword/ResetPasswordPollCompletionApiResponse.kt
+++ common4j/src/main/com/microsoft/identity/common/java/nativeauth/providers/responses/resetpassword/ResetPasswordPollCompletionApiResponse.kt
@@ -47,6 +47,7 @@ class ResetPasswordPollCompletionApiResponse(
     @Expose override var statusCode: Int,
     @Expose @SerializedName("status") val status: String?,
     @SerializedName("continuation_token") val continuationToken: String?,
+    @SerializedName("expires_in") val expiresIn: Int?,
     @SerializedName("error") val error: String?,
     @SerializedName("error_description") val errorDescription: String?,
     @SerializedName("error_uri") val errorUri: String?,
@@ -115,7 +116,10 @@ class ResetPasswordPollCompletionApiResponse(
                         ResetPasswordPollCompletionApiResult.InProgress
                     }
                     status.isPollSucceeded() -> {
-                        ResetPasswordPollCompletionApiResult.PollingSucceeded
+                        ResetPasswordPollCompletionApiResult.PollingSucceeded(
+                            continuationToken = continuationToken,
+                            expiresIn = expiresIn
+                        )
                     }
                     else -> {
                         ResetPasswordPollCompletionApiResult.PollingFailed(
