6a7adc04646c80b529cbbcf03721528ce47aef93
Melissa Ahn <melissaahn@microsoft.com>
2024-02-29 19:13:17 -0800

    exception string

--- common/src/main/java/com/microsoft/identity/common/internal/fido/AuthFidoChallengeHandler.kt
+++ common/src/main/java/com/microsoft/identity/common/internal/fido/AuthFidoChallengeHandler.kt
@@ -190,7 +190,7 @@ class AuthFidoChallengeHandler (
         if (exception != null) {
             span.recordException(exception)
             span.setStatus(StatusCode.ERROR)
-            respondToChallenge(submitUrl, exception.toString(), context, span)
+            respondToChallenge(submitUrl, exception.javaClass.name + ": " + exception.message, context, span)
         } else {
             span.setStatus(StatusCode.ERROR, errorMessage)
             respondToChallenge(submitUrl, errorMessage, context, span)
