2b61e4ad4db7b7d365c471b5c035a6e217666edf
amgusain <amgusain@microsoft.com>
2024-04-15 12:26:34 -0700

    revert cert related changes

--- LabApiUtilities/src/main/com/microsoft/identity/labapi/utilities/authentication/KeyVaultAuthenticationClient.java
+++ LabApiUtilities/src/main/com/microsoft/identity/labapi/utilities/authentication/KeyVaultAuthenticationClient.java
@@ -37,9 +37,9 @@ import lombok.NonNull;
  */
 public class KeyVaultAuthenticationClient implements IAccessTokenSupplier {
 
-    private final static String CLIENT_ID = "f62c5ae3-bf3a-4af5-afa8-a68b800396e9";
+    private final static String CLIENT_ID = "4bc6e96f-bd23-408f-8ecb-a7a7145463f9";
     private final static String SCOPE = "https://vault.azure.net/.default";
-    private final static String CERTIFICATE_ALIAS = "LabVaultAccessCert";
+    private final static String CERTIFICATE_ALIAS = "AutomationRunner";
     private final static String KEYSTORE_TYPE = "Windows-MY";
     private final static String KEYSTORE_PROVIDER = "SunMSCAPI";
 
