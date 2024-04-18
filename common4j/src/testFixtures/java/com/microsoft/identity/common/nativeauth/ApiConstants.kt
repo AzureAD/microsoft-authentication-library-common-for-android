1436316d1bff74d7da4e9bb0dcd0979ffed66896
yuxin <yuxin@microsoft.com>
2024-04-18 11:30:52 +0100

    extract the mock api url from code to pipeline env

--- common4j/src/testFixtures/java/com/microsoft/identity/common/nativeauth/ApiConstants.kt
+++ common4j/src/testFixtures/java/com/microsoft/identity/common/nativeauth/ApiConstants.kt
@@ -30,8 +30,8 @@ import java.net.URL
  */
 interface ApiConstants {
     companion object {
-        const val BASEPATH = "https://native-auth-mock-api.azurewebsites.net/"
-        private const val BASE_REQUEST_PATH = BASEPATH + "1234/"
+        val BASEPATH = System.getenv("MOCK_API_DOMAIN")
+        private val BASE_REQUEST_PATH = BASEPATH + "1234/"
         val signUpStartRequestUrl = URL(BASE_REQUEST_PATH + "signup/v1.0/start")
         val signUpChallengeRequestUrl = URL(BASE_REQUEST_PATH + "signup/v1.0/challenge")
         val signUpContinueRequestUrl = URL(BASE_REQUEST_PATH + "signup/v1.0/continue")
