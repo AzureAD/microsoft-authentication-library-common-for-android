03d1b4e9c54c24da50e1bb3a8430297a3f81f802
SammyO <1315628+SammyO@users.noreply.github.com>
2024-05-14 09:47:32 +0100

    Make NativeAuthCredentialHelper Kotlin-based

---
+++ testutils/src/main/java/com/microsoft/identity/internal/testutils/nativeauth/NativeAuthCredentialHelper.kt
@@ -0,0 +1,26 @@
+package com.microsoft.identity.internal.testutils.nativeauth
+
+import com.microsoft.identity.common.java.util.StringUtil
+import com.microsoft.identity.internal.testutils.BuildConfig
+
+object NativeAuthCredentialHelper {
+    val nativeAuthSignInUsername: String
+        get() {
+            val username = BuildConfig.NATIVE_AUTH_SIGNIN_TEST_USERNAME
+            return if (StringUtil.isNullOrEmpty(username)) {
+                throw IllegalStateException("env var NATIVE_AUTH_SIGNIN_TEST_USERNAME value not set")
+            } else {
+                username
+            }
+        }
+
+    val nativeAuthSignInPassword: String
+        get() {
+            val password = BuildConfig.NATIVE_AUTH_SIGNIN_TEST_PASSWORD
+            return if (StringUtil.isNullOrEmpty(password)) {
+                throw IllegalStateException("env var NATIVE_AUTH_SIGNIN_TEST_PASSWORD value not set")
+            } else {
+                password
+            }
+        }
+}
