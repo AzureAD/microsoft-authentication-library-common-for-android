fa22ff52a5f226bf748566e8c5532c1c9c29f186
Sowmya Malayanur <somalaya@microsoft.com>
2024-06-06 21:50:56 -0700

    addressed comments

--- common/src/main/java/com/microsoft/identity/common/adal/internal/AuthenticationConstants.java
+++ common/src/main/java/com/microsoft/identity/common/adal/internal/AuthenticationConstants.java
@@ -1377,12 +1377,12 @@ public final class AuthenticationConstants {
         /**
          * The Bundle key name of transfer token.
          */
-        public static final String TRANSFER_TOKEN = "transfer_token_value";
+        public static final String TRANSFER_TOKEN_BUNDLE_KEY = "transfer_token";
 
         /**
          * The Bundle key name of transfer token key (account name).
          */
-        public static final String TRANSFER_TOKEN_ACCOUNT_NAME = "account_name";
+        public static final String TRANSFER_TOKEN_ACCOUNT_NAME_BUNDLE_KEY = "account_name";
 
         /**
          * Boolean to return when a Broker RT is successfully updated.
@@ -1562,7 +1562,7 @@ public final class AuthenticationConstants {
             BROKER_GET_ACCOUNTS(BROKER_API_GET_BROKER_ACCOUNTS_PATH, BROKER_VERSION_1, null),
             BROKER_REMOVE_ACCOUNT(BROKER_API_REMOVE_BROKER_ACCOUNT_PATH, BROKER_VERSION_1, null),
             BROKER_UPDATE_BRT(BROKER_API_UPDATE_BRT_PATH, BROKER_VERSION_1, null),
-            BROKER_SAVE_TRANSFER_TOKEN(BROKER_SAVE_TRANSFER_TOKEN_PATH, null, null),
+            BROKER_BACKUP_TRANSFER_TOKEN(BROKER_BACKUP_TRANSFER_TOKEN_PATH, null, null),
             BROKER_SET_FLIGHTS(BROKER_API_SET_FLIGHTS_PATH, BROKER_VERSION_3, null),
             BROKER_GET_FLIGHTS(BROKER_API_GET_FLIGHTS_PATH, BROKER_VERSION_3, null),
             GET_SSO_TOKEN(GET_SSO_TOKEN_PATH, null, VERSION_7),
@@ -1683,9 +1683,9 @@ public final class AuthenticationConstants {
         public static final String BROKER_API_UPDATE_BRT_PATH = "/brokerApi/updateBrt";
 
         /**
-         * URI Path constant for BrokerApi-to-Broker save transfer token request using ContentProvider.
+         * URI Path constant for BrokerApi-to-Broker backup transfer token request using ContentProvider.
          */
-        public static final String BROKER_SAVE_TRANSFER_TOKEN_PATH = "/brokerApi/saveTransferToken";
+        public static final String BROKER_BACKUP_TRANSFER_TOKEN_PATH = "/brokerApi/backupTransferToken";
 
         /**
          * Broker api path constant for setting flight information.
