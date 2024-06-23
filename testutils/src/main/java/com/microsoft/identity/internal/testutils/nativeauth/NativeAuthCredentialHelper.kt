bf72a2f73e2e9c6d62c59dde34e7e8c537548d69
yuxin <yuxin@microsoft.com>
2024-06-23 19:30:42 +0100

    NativeAuthTestConfig

--- testutils/src/main/java/com/microsoft/identity/internal/testutils/nativeauth/NativeAuthCredentialHelper.kt
+++ testutils/src/main/java/com/microsoft/identity/internal/testutils/nativeauth/NativeAuthCredentialHelper.kt
@@ -1,6 +1,7 @@
 package com.microsoft.identity.internal.testutils.nativeauth
 
 import com.google.gson.Gson
+import com.google.gson.reflect.TypeToken
 import com.microsoft.identity.common.java.util.StringUtil
 import com.microsoft.identity.internal.testutils.BuildConfig
 import com.microsoft.identity.internal.testutils.nativeauth.api.models.NativeAuthTestConfig
@@ -8,11 +9,12 @@ import com.microsoft.identity.internal.testutils.nativeauth.api.models.NativeAut
 object NativeAuthCredentialHelper {
     val nativeAuthTestConfig:  NativeAuthTestConfig
         get() {
-            val config = BuildConfig.NATIVE_AUTH_TEST_CONFIG
-            return if (StringUtil.isNullOrEmpty(config)) {
+            val configs = BuildConfig.NATIVE_AUTH_TEST_CONFIG
+            return if (StringUtil.isNullOrEmpty(configs)) {
                 throw IllegalStateException("env var NATIVE_AUTH_CONFIG value not set")
             } else {
-                Gson().fromJson(config, NativeAuthTestConfig::class.java)
+                val type = object : TypeToken<Map<String, NativeAuthTestConfig.Config>>() {}.type
+                Gson().fromJson(configs, type)
             }
         }
 }
--- testutils/src/main/java/com/microsoft/identity/internal/testutils/nativeauth/api/models/NativeAuthTestConfig.kt
+++ testutils/src/main/java/com/microsoft/identity/internal/testutils/nativeauth/api/models/NativeAuthTestConfig.kt
@@ -2,9 +2,8 @@ package com.microsoft.identity.internal.testutils.nativeauth.api.models
 
 
 data class NativeAuthTestConfig(
-    val SIGN_IN_PASSWORD: Config,
-    val SIGN_UP_PASSWORD: Config,
-    val SSPR: Config) {
+    val configs: Map<String, Config>
+) {
     data class Config(
         val email: String,
         val client_id: String,
