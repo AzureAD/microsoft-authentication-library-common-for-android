774c7ce80a08a39194e078d2fbc7b0fea5397360
yuxin <yuxin@microsoft.com>
2024-06-24 01:22:22 +0100

    Use json file instead of string

--- testutils/src/main/java/com/microsoft/identity/internal/testutils/nativeauth/NativeAuthCredentialHelper.kt
+++ testutils/src/main/java/com/microsoft/identity/internal/testutils/nativeauth/NativeAuthCredentialHelper.kt
@@ -5,11 +5,12 @@ import com.google.gson.reflect.TypeToken
 import com.microsoft.identity.common.java.util.StringUtil
 import com.microsoft.identity.internal.testutils.BuildConfig
 import com.microsoft.identity.internal.testutils.nativeauth.api.models.NativeAuthTestConfig
+import java.io.File
 
 object NativeAuthCredentialHelper {
     val nativeAuthTestConfig:  NativeAuthTestConfig
         get() {
-            val configs = BuildConfig.NATIVE_AUTH_TEST_CONFIG
+            val configs = readJsonFile()
             return if (StringUtil.isNullOrEmpty(configs)) {
                 throw IllegalStateException("env var NATIVE_AUTH_CONFIG value not set")
             } else {
@@ -17,4 +18,8 @@ object NativeAuthCredentialHelper {
                 Gson().fromJson(configs, type)
             }
         }
+
+    private fun readJsonFile(): String {
+        return File(BuildConfig.NATIVE_AUTH_TEST_CONFIG).readText(Charsets.UTF_8)
+    }
 }
