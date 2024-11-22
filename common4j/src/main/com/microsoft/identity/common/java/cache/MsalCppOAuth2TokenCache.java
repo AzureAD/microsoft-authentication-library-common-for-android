ec44661fb67fa0186e7823ff7d401e96205a7bb1
Sandesh Kumar Srivastava <sansrivastav@microsoft.com>
2024-11-22 10:49:49 -0800

    For MSALCPP flows, match exact calims when deleting AT with intersecting scopes

--- common4j/src/main/com/microsoft/identity/common/java/cache/MsalCppOAuth2TokenCache.java
+++ common4j/src/main/com/microsoft/identity/common/java/cache/MsalCppOAuth2TokenCache.java
@@ -148,7 +148,7 @@ public class MsalCppOAuth2TokenCache
             }
         }
 
-        saveCredentialsInternal(credentials);
+        saveCredentialsInternal(true, credentials);
     }
 
     /**
