37acf1f0a2b1d0e36573dcf6f318fb288d6bec91
Melissa Ahn <melissaahn@microsoft.com>
2024-11-22 11:15:31 -0800

    maybe?

--- common/src/main/java/com/microsoft/identity/common/internal/ui/webview/OAuth2WebViewClient.java
+++ common/src/main/java/com/microsoft/identity/common/internal/ui/webview/OAuth2WebViewClient.java
@@ -188,7 +188,8 @@ public abstract class OAuth2WebViewClient extends WebViewClient {
     public void onPageFinished(final WebView view,
                                final String url) {
         super.onPageFinished(view, url);
-        Logger.info(TAG + ":onPageFinished", "WebView finished loading.");
+        final String methodTag = TAG + ":onPageFinished";
+        Logger.info(methodTag, "WebView finished loading.");
         mPageLoadedCallback.onPageLoaded(url);
 
         //Supports UI Automation... informing that the webview resource is now idle
