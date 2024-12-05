82251aa4ba4f27647b727d0cc27dfdf3f0348966
pedro romero vargas <pedroro@microsoft.com>
2024-12-04 21:44:55 -0800

    nits

--- common/src/main/java/com/microsoft/identity/common/internal/providers/oauth2/AuthorizationActivityFactory.java
+++ common/src/main/java/com/microsoft/identity/common/internal/providers/oauth2/AuthorizationActivityFactory.java
@@ -111,6 +111,9 @@ public class AuthorizationActivityFactory {
         final LibraryConfiguration libraryConfig = LibraryConfiguration.getInstance();
         if (ProcessUtil.isBrokerProcess(context)) {
             intent = new Intent(context, BrokerAuthorizationActivity.class);
+            intent.setFlags(Intent.FLAG_ACTIVITY_CLEAR_TASK | Intent.FLAG_ACTIVITY_NEW_TASK);
+            // In the case of a DUNA flow, we need to transition from the browser to the WebView.
+            // These flags ensure that we have a new task stack that allows for this transition.
         } else if (libraryConfig.isAuthorizationInCurrentTask() && !authorizationAgent.equals(AuthorizationAgent.WEBVIEW)) {
         // We exclude the case when the authorization agent is already selected as WEBVIEW because of confusion
         // that results from attempting to use the CurrentTaskAuthorizationActivity in that case, because as webview
