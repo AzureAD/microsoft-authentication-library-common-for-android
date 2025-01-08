c69eb7d6fe51372b8986a38abd6b9c0e11bb0d44
Shahzaib <37125644+shahzaibj@users.noreply.github.com>
2025-01-08 11:11:31 -0800

    Add support for OneBox Environment, Fixes AB#3113751 (#2559)
    Fixes
    [AB#3113751](https://identitydivision.visualstudio.com/fac9d424-53d2-45c0-91b5-ef6ba7a6bf26/_workitems/edit/3113751)
    
    Changes to allow MSAL/Broker to talk to eSTS running locally. This
    basically does the following:
    
    - Add build config flag to bypass authority validation. This is required
    because local eSTS authorities are not discovered via cloud discovery.
    We can enable this flag locally in gradle project properties to target
    local eSTS.
    - Read the port number of the authority when constructing the cloud URL
    because targeting local eSTS requires supplying exact port. I've scoped
    the change to read port only when environment is specified as onebox.
    
    Related MSAL PR:
    https://github.com/AzureAD/microsoft-authentication-library-for-android/pull/2231

--- common4j/src/main/com/microsoft/identity/common/java/providers/microsoft/azureactivedirectory/AzureActiveDirectoryEnvironment.java
+++ common4j/src/main/com/microsoft/identity/common/java/providers/microsoft/azureactivedirectory/AzureActiveDirectoryEnvironment.java
@@ -3,4 +3,7 @@ package com.microsoft.identity.common.java.providers.microsoft.azureactivedirect
 public class AzureActiveDirectoryEnvironment {
     public static final String PRODUCTION_CLOUD_URL = "https://login.microsoftonline.com"; //Prod
     public static final String PREPRODUCTION_CLOUD_URL = "https://login.windows-ppe.net"; //PPE
+
+    public static final String ONEBOX_AUTHORITY = "zurich.test.dnsdemo1.test:8478";
+    public static final String ONEBOX_CLOUD_URL = "https://" + ONEBOX_AUTHORITY; // Local ESTS Deployment
 }
