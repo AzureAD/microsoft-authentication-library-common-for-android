5e267aea699ec934efda991fe856bdfb5325f099
pedro romero vargas <pedroro@microsoft.com>
2025-02-27 10:57:32 -0800

    rename CLAIM parame

--- common/src/main/java/com/microsoft/identity/common/adal/internal/AuthenticationConstants.java
+++ common/src/main/java/com/microsoft/identity/common/adal/internal/AuthenticationConstants.java
@@ -548,7 +548,7 @@ public final class AuthenticationConstants {
         /**
          * String Query parameter key to indicate support for SWITCH_BROWSER protocol.
          */
-        public static final String CLAIM = "switch_browser";
+        public static final String CLIENT_SUPPORTS_FLOW = "switch_browser";
 
         /**
          * String Query parameter key for the purpose token.
