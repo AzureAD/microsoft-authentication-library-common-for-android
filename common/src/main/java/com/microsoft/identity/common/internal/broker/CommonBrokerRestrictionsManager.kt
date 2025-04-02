dbd4e917f700e2bdde706699d12000d92fca8bca
p3dr0rv <pedroro@microsoft.com>
2025-04-01 22:35:04 -0700

    add logs

--- common/src/main/java/com/microsoft/identity/common/internal/broker/CommonBrokerRestrictionsManager.kt
+++ common/src/main/java/com/microsoft/identity/common/internal/broker/CommonBrokerRestrictionsManager.kt
@@ -23,42 +23,60 @@
 package com.microsoft.identity.common.internal.broker
 
 import android.os.Bundle
+import com.microsoft.identity.common.logging.Logger
 
+/**
+ * A class for managing broker restrictions in the common module.
+ */
 object CommonBrokerRestrictionsManager: IBrokerRestrictionsManager{
 
+    private const val TAG = "CommonBrokerRestrictionsManager"
     private var brokerRestrictionsManager: IBrokerRestrictionsManager? = null
-    override val defaultPackageName: String = "UNSET"
+    override val defaultPackageName: String = "LOCAL"
 
+    /**
+     * Initializes the CommonBrokerRestrictionsManager with the provided IBrokerRestrictionsManager.
+     *
+     * @param brokerRestrictionsManager The IBrokerRestrictionsManager to be used.
+     */
     fun initialize(brokerRestrictionsManager: IBrokerRestrictionsManager) {
             this.brokerRestrictionsManager = brokerRestrictionsManager
     }
 
     override fun getString(key: String, appPackageName: String, defaultValue: String?): String? {
+        val methodTag = "$TAG:getString"
         brokerRestrictionsManager?.let {
+            Logger.info(methodTag, "Getting string value for key: $key, appPackageName: $appPackageName")
             return if (appPackageName == defaultPackageName) {
                 it.getString(key = key, defaultValue = defaultValue)
             } else {
                 it.getString(key = key, defaultValue = defaultValue, appPackageName = appPackageName)
             }
         }
+        Logger.warn(methodTag, "BrokerRestrictionsManager is not initialized. Returning default value.")
         return defaultValue
     }
 
     override fun getBoolean(key: String, appPackageName: String, defaultValue: Boolean): Boolean {
+        val methodTag = "$TAG:getBoolean"
         brokerRestrictionsManager?.let {
+            Logger.info(methodTag, "Getting boolean value for key: $key, appPackageName: $appPackageName")
             return if (appPackageName == defaultPackageName) {
                 it.getBoolean(key = key, defaultValue = defaultValue)
             } else {
                 it.getBoolean(key = key, defaultValue = defaultValue, appPackageName = appPackageName)
             }
         }
+        Logger.warn(methodTag, "BrokerRestrictionsManager is not initialized. Returning default value.")
         return defaultValue
     }
 
     override fun getFilteredBundleFromLocalRestrictionManager(bundleOfKeys: Bundle): Bundle {
         brokerRestrictionsManager?.let {
-                return it.getFilteredBundleFromLocalRestrictionManager(bundleOfKeys)
+            Logger.info(TAG, "Getting filtered bundle from local restriction manager.")
+            return it.getFilteredBundleFromLocalRestrictionManager(bundleOfKeys)
         }
+        Logger.warn(TAG, "BrokerRestrictionsManager is not initialized. Returning empty bundle.")
         return Bundle()
     }
 }
