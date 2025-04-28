c0d7031647c3a099cd82099ca1919065e90e8a43
fadidurah <88730756+fadidurah@users.noreply.github.com>
2025-04-28 13:31:57 -0400

    Pass Work Profile existence, OS Version, and Manufacturer to ESTS, Fixes AB#3138815 (#2627)
    Feature Spec:
    https://microsoft-my.sharepoint-df.com/:w:/p/jukollin/EW3bntSipC1Ek8gY7sIX_yUBXip16NxHN-mge53O1V0K7Q?e=vxoEue
    
    Contract with ESTS for query parameter names:
    https://microsoft-my.sharepoint-df.com/:w:/r/personal/fadidurah_microsoft_com/_layouts/15/doc2.aspx?sourcedoc=%7B7F727CCE-ECB4-4790-B4B9-369B8BD14A5D%7D&file=Document.docx&action=editnew&mobileredirect=true&wdPreviousSession=640fdd8a-c8fd-4983-fd56-29908be02022&wdNewAndOpenCt=1740713103250&wdo=4&wdOrigin=TEAMS-MAGLEV.p2p_ns.rwc&wdPreviousCorrelation=3a1b8576-c30c-4b46-9f9e-7cfde7fb9cbe&wdnd=1&wdTpl=blankNew&share=IQHOfHJ_tOyQR7S5NpuL0UpdAYK9OGBqj-cKpGfw7kJbcPk&wdExp=TEAMS-TREATMENT&wdhostclicktime=1744341352978&web=1
    
    Msal-only Authorize request:
    
    /login.microsoftonline.com/organizations/oAuth2/v2.0/authorize?prompt=select_account&client-request-id=de149d0e-9a9e-4f42-accf-c43e209b1b35&x-client-CPU=arm64-v8a&x-client-DM=Pixel&**x-client-MN=Google**&**x-client-OS=29**&x-client-SKU=MSAL.Android&x-client-Ver=5.10.0&login_hint=&instance_aware=false&code_challenge=8sy252i6gSVaCyONbgVYS9qhatnWyybwGzqs35bPwYQ&code_challenge_method=S256&**x-client-WPAvailable=true**&claims=%7B%7D&client_id=4b0db8c2-9f26-4417-8bde-3f0e3656f8e0&redirect_uri=msauth%3A%2F%2Fcom.msft.identity.client.sample.local%2F1wIqXSqBj7w%252Bh11ZifsnqwgyKrY%253D&response_type=code&scope=user.read%20%20openid%20offline_access%20profile&state=NzM6ZmIzZWFkZTctNDRmNC00YjBkLWEzODItOGVhYzc2ZTlkM2E1LWRiNmNlMzg2LTQ2NzktNGE2Ni1hMWVhLTk0Y2IzYmQ1NWJlZg
    
    Msal-broker Authorize request:
    
    /login.microsoftonline.com/organizations/oAuth2/v2.0/authorize?prompt=login&client-request-id=ef4e33b5-b8ae-45e9-a9ff-d0ea51487db2&x-client-CPU=arm64-v8a&x-client-DM=Pixel&**x-client-MN=Google**&**x-client-OS=29**&x-client-SKU=MSAL.Android&x-client-Ver=6.0.0&login_hint=&instance_aware=true&code_challenge=rax8IWFnE_OtZlr1p7GKgWZk9cF2gl6o1fGKS3gmx5U&code_challenge_method=S256&**x-client-WPAvailable=true**&client_id=29d9ed98-a469-4536-ade2-f981bc1d605e&redirect_uri=msauth%3A%2F%2FMicrosoft.AAD.BrokerPlugin&response_type=code&scope=aza%20openid%20email%20profile%20offline_access%20urn%3Aaad%3Atb%3Aupdate%3Aprt%2F.default&state=MTI3OjVjNGIwMjFmLWM5NTYtNDk4Yy1iNTVhLThlNWVjZjcwZjg2OC03NzU2NjljOC05NTRhLTQ2MjktYjVlMC1kMmZiNDY5YWRhYWE&temp-param=Temporary&webauthn=1&brkr=1&x-client-brkrver=8.1.20250423-1ESdev.6&x-app-name=com.msft.identity.client.sample.local&x-app-ver=1.0-local&domain_hint=organizations&caller_app_client_id=4b0db8c2-9f26-4417-8bde-3f0e3656f8e0&caller_app_redirect_uri=msauth%3A%2F%2Fcom.msft.identity.client.sample.local%2F1wIqXSqBj7w%252Bh11ZifsnqwgyKrY%253D&prt_protocol_version=3.0
    
    Note `x-client-MN=Google`, `x-client-OS=29`, and
    `x-client-WPAvailable=true`
    
    We were already passing OS version (x-client-OS), this PR makes changes
    to common to include Device manufacturer and an additional parameter
    denoting if we are making a request from personal profile, but a work
    profile managed by clouddpc (NOT MANAGED BY COMPANY PORTAL, the intent
    will not be found if work profile is managed by Company Portal).
    
    Validation: Manual validation done with JAmes, setup Android Enterprise
    work profile on test device, and was getting a success when querying for
    intent. Did this for MSAL, MSAL/Broker, and OneAuth/Broker. Separate
    work must be done to get these parameters sent in oneuath only scenarios
    
    
    [AB#3138815](https://identitydivision.visualstudio.com/fac9d424-53d2-45c0-91b5-ef6ba7a6bf26/_workitems/edit/3138815)

---
+++ common/src/main/java/com/microsoft/identity/common/internal/util/WorkProfileUtil.java
@@ -0,0 +1,70 @@
+// Copyright (c) Microsoft Corporation.
+// All rights reserved.
+//
+// This code is licensed under the MIT License.
+//
+// Permission is hereby granted, free of charge, to any person obtaining a copy
+// of this software and associated documentation files(the "Software"), to deal
+// in the Software without restriction, including without limitation the rights
+// to use, copy, modify, merge, publish, distribute, sublicense, and / or sell
+// copies of the Software, and to permit persons to whom the Software is
+// furnished to do so, subject to the following conditions :
+//
+// The above copyright notice and this permission notice shall be included in
+// all copies or substantial portions of the Software.
+//
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+// THE SOFTWARE.
+package com.microsoft.identity.common.internal.util;
+
+import android.content.Context;
+import android.content.Intent;
+import android.content.pm.ResolveInfo;
+import android.os.Build;
+
+import com.microsoft.identity.common.logging.Logger;
+
+import java.util.List;
+
+import lombok.NonNull;
+
+public class WorkProfileUtil {
+    private static final String TAG = WorkProfileUtil.class.getSimpleName();
+
+    /**
+     * Helper method to check if we are in personal profile but a work profile managed by clouddpc
+     * is available.
+     * <a href="https://developers.google.com/android/management/work-profile-detection#detect_if_the_device_has_a_work_profile">Google Docs for intent used</a>
+     * @param context context needed to check for intent
+     * @return true if called in personal profile and a work profile managed by clouddpc exists, false otherwise
+     */
+    public static boolean checkIfIsInPersonalProfileButClouddpcWorkProfileAvailable(@NonNull final Context context) {
+        try {
+            final Intent intent = new Intent("com.google.android.apps.work.clouddpc.ACTION_DETECT_WORK_PROFILE");
+            final List<ResolveInfo> activities = context.getPackageManager().queryIntentActivities(intent, 0);
+
+            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.R) {
+                return activities.stream()
+                        .anyMatch(
+                                (ResolveInfo resolveInfo) -> resolveInfo.isCrossProfileIntentForwarderActivity());
+            } else {
+                if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.N) {
+                    return activities.stream()
+                            .anyMatch(
+                                    (ResolveInfo resolveInfo) -> resolveInfo.activityInfo.name.equals("com.android.internal.app.ForwardIntentToManagedProfile"));
+                }
+            }
+
+            return false;
+        } catch (Exception e) {
+            // If we run into exception for any reason, we'll just return false
+            Logger.warn(TAG, "Received an exception while trying to check if clouddpc work profile is available: " + e.getMessage());
+            return false;
+        }
+    }
+}
