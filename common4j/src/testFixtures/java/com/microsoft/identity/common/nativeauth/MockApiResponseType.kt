0e4cb1750ce9b9e7ff8745828e30b41ffe782f86
Danilo Raspa <daniloraspa@microsoft.com>
2025-05-07 17:29:54 +0100

    added integration tests for JIT continue and JIT challenge

--- common4j/src/testFixtures/java/com/microsoft/identity/common/nativeauth/MockApiResponseType.kt
+++ common4j/src/testFixtures/java/com/microsoft/identity/common/nativeauth/MockApiResponseType.kt
@@ -77,8 +77,6 @@ enum class MockApiResponseType(val stringValue: String) {
     REGISTRATION_REQUIRED("RegistrationRequired"),
     REGISTRATION_CONTINUE_SUCCESS("RegistrationContinueSuccess"),
     REGISTRATION_INVALID_OOB_VALUE("RegistrationInvalidOOBValue"),
-    SERVER_ERROR("ServerError"),
-    ACCESS_DENIED("AccessDenied"),
     // Typo below on Registraion is expected, on par with Mock API
     REGISTRATION_INTROSPECT_SUCCESS("RegistraionIntrospectSuccess"),
     REGISTRATION_CHALLENGE_SUCCESS("RegistraionChallengeSuccess"),
