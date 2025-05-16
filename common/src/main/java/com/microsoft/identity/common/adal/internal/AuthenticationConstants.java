ba5f6ef56342d6cead786429b5a7dca767323384
p3dr0rv <pedroro@microsoft.com>
2025-05-16 14:58:52 -0700

    update constants

--- common/src/main/java/com/microsoft/identity/common/adal/internal/AuthenticationConstants.java
+++ common/src/main/java/com/microsoft/identity/common/adal/internal/AuthenticationConstants.java
@@ -1467,13 +1467,25 @@ public final class AuthenticationConstants {
          */
         public static final String POWERLIFT_TENANT_ID = "powerLiftTenantId";
 
-
+        /**
+         * The App Link redirect URL for the Authenticator app.
+         */
         public static final String AUTHENTICATOR_APP_LINK_REDIRECT_URL = "https://login.microsoftonline.com/authenticator";
+
+        /**
+         * The App Link redirect URL for the LTW app.
+         */
         public static final String LTW_APP_LINK_REDIRECT_URL = "https://login.microsoftonline.com/ltw";
+
+        /**
+         * App Link redirect URL for the CP app.
+         */
         public static final String COMPANY_PORTAL_APP_LINK_REDIRECT_URL = "https://login.microsoftonline.com/cp";
+
+        /**
+         * App Link redirect URL for the BrokerHost app.
+         */
         public static final String  BROKER_HOST_APP_LINK_REDIRECT_URL = "https://login.microsoftonline.com/brokerhost";
-
-
 
         /**
          * Bundle identifiers for x-ms-clitelem info.
