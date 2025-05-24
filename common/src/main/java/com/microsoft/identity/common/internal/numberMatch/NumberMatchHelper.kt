c8acef317aa442d663c85b5a7bd40fc513cd94c6
fadidurah <88730756+fadidurah@users.noreply.github.com>
2025-05-23 22:50:43 -0400

    Expose a JavaScript API in brokered Webviews to facilitate Improved Same Device NumberMatch , Fixes AB#3203956 (#2617)
    SPEC:
    https://microsoft-my.sharepoint-df.com/:w:/r/personal/siddhijain_microsoft_com/_layouts/15/Doc.aspx?sourcedoc=%7BD1D944D5-2047-40AB-B8F9-98506BF212A3%7D&file=Engineering%20design%20-%20Number%20matching%20on%20same%20device.docx&action=default&mobileredirect=true&share=IQHVRNnRRyCrQLj5mFBr8hKjAbj81fEnpO6X-99laqs2j_M&wdOrigin=TEAMS-MAGLEV.p2p_ns.rwc&wdExp=TEAMS-TREATMENT&wdhostclicktime=1743094076241&web=1
    
    Word Doc: for JavaScript Api:
    https://microsoft-my.sharepoint-df.com/:w:/p/veenasoman/EY1AZIeT8X5KrXVz97Vx520B3Jj0fBLSPlklnoRvcmbh0Q?e=ZVVUrw&nav=eyJoIjoiMjEzMzE1Mzg5NSJ9
    
    Structure has changed a bit for this. To facilitate future work, we will
    have a generalized JavaScript API that takes in a json string payload.
    This is used to parse out a function name, and data field, both of which
    are used to call a specific function in broker code. This same
    functionality will be used next month for CA Block improvment work (I
    don't have a spec to this one yet).
    
    Expected method call in JavaScript is now something like this, we are
    working on finalizing json schema:
    `BrokerJS.postToBroker('{function: NUMBER_MATCH,data: {sessionID: id,
    numberMatch: number}}')`
    
    I added some unit tests in the broker PR, but primary validation will be
    when ests exposes a test slice that calls the JavaScript API. Did some
    testing in our webview class to call javascript code, and was able to
    prompt the numberMatch method.
    
    Broker PR: https://github.com/AzureAD/ad-accounts-for-android/pull/3073
    
    
    [AB#3203956](https://identitydivision.visualstudio.com/fac9d424-53d2-45c0-91b5-ef6ba7a6bf26/_workitems/edit/3203956)

---
+++ common/src/main/java/com/microsoft/identity/common/internal/numberMatch/NumberMatchHelper.kt
@@ -0,0 +1,75 @@
+// Copyright (c) Microsoft Corporation.
+// All rights reserved.
+//
+// This code is licensed under the MIT License.
+//
+// Permission is hereby granted, free of charge, to any person obtaining a copy
+// of this software and associated documentation files(the "Software"), to deal
+// in the Software without restriction, including without limitation the rights
+// to use, copy, modify, merge, publish, distribute, sublicense, and / or sell
+// copies of the Software, and to permit persons to whom the Software is
+// furnished to do so, subject to the following conditions :
+//
+// The above copyright notice and this permission notice shall be included in
+// all copies or substantial portions of the Software.
+//
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+// THE SOFTWARE.
+package com.microsoft.identity.common.internal.numberMatch
+
+import com.microsoft.identity.common.logging.Logger
+
+/**
+ * Helper to facilitate NumberMatchFlow. Used in conjunction with {@link AuthUxJavaScriptInterface}
+ * When authenticator is installed, and phone uses MFA or PSI in an interactive flow, a number
+ * matching challenge is issued, where used is given a number and asked to open authenticator and check
+ * for the same number in authenticator UI. This feature cuts out one UI step, where this API is used to
+ * supply the number match value and store it in ephemeral storage (kept as long as current broker
+ * process is alive), where Authenticator can call a broker API to fetch the number match, and immediately
+ * prompt user for consent, rather than first asking them to check the number match.
+ */
+class NumberMatchHelper {
+
+    // Store number matches in a static hash map
+    // No need to persist this storage beyond the current broker process, but we need to keep them
+    // long enough for AuthApp to call the broker api to fetch the number match
+    companion object {
+        val TAG = NumberMatchHelper::class.java.simpleName
+        val numberMatchMap: HashMap<String, String> = HashMap()
+        const val SESSION_ID_ATTRIBUTE_NAME = "sessionID"
+        const val NUMBER_MATCH_ATTRIBUTE_NAME = "numberMatch"
+
+        /**
+         * Method to add a key:value pair of sessionID:numberMatch to static hashmap. This hashmap will be accessed
+         * by broker api to get the number match for a particular sessionID.
+         */
+        fun storeNumberMatch(sessionId: String?, numberMatch: String?) {
+            val methodTag = "$TAG:storeNumberMatch"
+            Logger.info(methodTag,
+                "Adding entry in NumberMatch hashmap for session ID: $sessionId")
+
+            // If both parameters are non-null, add a new entry to the hashmap
+            if (sessionId != null && numberMatch != null) {
+                numberMatchMap[sessionId] = numberMatch
+            }
+            // If either parameter is null, do nothing
+            else {
+                Logger.warn(methodTag,
+                    "Either session ID or number match is null. Nothing to add for number match."
+                )
+            }
+        }
+
+        /**
+         * Clear existing number match key:value pairs
+         */
+        fun clearNumberMatchMap() {
+            numberMatchMap.clear()
+        }
+    }
+}
