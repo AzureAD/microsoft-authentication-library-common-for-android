df0f1b98d12ba50d1351541c982f4d317285a4c4
yuxin <yuxin@microsoft.com>
2025-06-03 14:54:07 +0100

    Address comment

--- common4j/src/main/com/microsoft/identity/common/java/nativeauth/providers/NativeAuthConstants.kt
+++ common4j/src/main/com/microsoft/identity/common/java/nativeauth/providers/NativeAuthConstants.kt
@@ -60,7 +60,7 @@ object NativeAuthConstants {
      * ChallengeType encapsulates various challenge types supported for authentication by Native Auth
      */
     object ChallengeType {
-        //password is required for authentication
+        //Password is required for authentication
         const val PASSWORD = "password"
         //Authentication is done by presenting an Out of band token
         const val OOB = "oob"
@@ -72,7 +72,7 @@ object NativeAuthConstants {
      * CapabilityType encapsulates various capability types supported for authentication by Native Auth
      */
     object Capabilities {
-        //The application can accommodate the capability specified by the user when MFA is required
+        //The application can accommodate the capability specified by the user when MFA is required.
         const val MFA_REQUIRED = "mfa_required"
         //The application can accommodate the capability specified by the user when the registration of a new strong authentication method is required.
         const val REGISTRATION_REQUIRED = "registration_required"
