771de8541536efafae3e026d018ab95093a25e57
Melissa Ahn <melissaahn@microsoft.com>
2025-07-28 11:17:21 -0700

    suggestion

--- common4j/src/main/com/microsoft/identity/common/java/opentelemetry/SpanExtension.java
+++ common4j/src/main/com/microsoft/identity/common/java/opentelemetry/SpanExtension.java
@@ -85,7 +85,7 @@ public class SpanExtension {
     public static Scope makeCurrentSpan(@NonNull final Span span) {
         try {
             return span.makeCurrent();
-        } catch (final AbstractMethodError | Exception | NoSuchMethodError exception) {
+        } catch (final AbstractMethodError | Exception exception) {
             Logger.error(TAG + ":makeCurrentSpan", exception.getMessage(), exception);
             return NoopScope.INSTANCE;
         }
