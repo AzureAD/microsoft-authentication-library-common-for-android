cc99ce449d0664f1039c1837d24396c99ca41790
Shahzaib <shahzaib.jameel@microsoft.com>
2025-08-04 17:22:47 -0700

    Fix for enrollment id not sent to /token from local controller

--- common4j/src/main/com/microsoft/identity/common/java/controllers/BaseController.java
+++ common4j/src/main/com/microsoft/identity/common/java/controllers/BaseController.java
@@ -872,6 +872,14 @@ public abstract class BaseController {
                 ((MicrosoftTokenRequest) refreshTokenRequest).setPKeyAuthHeaderAllowed(
                         ((BrokerSilentTokenCommandParameters) parameters).isPKeyAuthHeaderAllowed()
                 );
+
+                // set enrollment id if available
+                // This is used to support MAM scenarios where the broker needs to send the enrollment id to the service
+                // For BrokerLocalController, PRT is not used but RT may have device claim so MAM enrollment is still possible
+                // as long as the broker sends the enrollment id.
+                if (!StringUtil.isNullOrEmpty(parameters.getMamEnrollmentId())) {
+                    ((MicrosoftTokenRequest) refreshTokenRequest).setMicrosoftEnrollmentId(parameters.getMamEnrollmentId());
+                }
             }
         }
 
