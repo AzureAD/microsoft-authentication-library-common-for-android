1f5fe40ce08ae59b5b002dc54edf8b9e6abb3b0d
p3dr0rv <pedroro@microsoft.com>
2025-08-25 13:13:40 -0700

    Set SWITCH_BROWSER_PROTOCOL_REQUIRES_STATE to true for enhanced security

--- common4j/src/main/com/microsoft/identity/common/java/flighting/CommonFlight.java
+++ common4j/src/main/com/microsoft/identity/common/java/flighting/CommonFlight.java
@@ -104,7 +104,7 @@ public enum CommonFlight implements IFlightConfig {
     /**
      * Flight to make the state parameter required for the switch browser protocol.
      */
-    SWITCH_BROWSER_PROTOCOL_REQUIRES_STATE("SwitchBrowserProtocolRequiresState", false),
+    SWITCH_BROWSER_PROTOCOL_REQUIRES_STATE("SwitchBrowserProtocolRequiresState", true),
 
     /**
      * Flight to enable adding x-client-MN and x-client-WPAvailable extra query parameters
