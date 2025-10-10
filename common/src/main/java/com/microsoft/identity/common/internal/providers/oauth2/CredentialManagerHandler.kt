a6ec8734d5e62608e17657188e021914536504f1
p3dr0rv <pedroro@microsoft.com>
2025-10-10 16:42:04 -0700

    Refactor CredentialManagerHandler to improve passkey creation and retrieval logging; streamline credential request handling.

--- common/src/main/java/com/microsoft/identity/common/internal/providers/oauth2/CredentialManagerHandler.kt
+++ common/src/main/java/com/microsoft/identity/common/internal/providers/oauth2/CredentialManagerHandler.kt
@@ -1,8 +1,29 @@
+// Copyright (c) Microsoft Corporation.
+// All rights reserved.
+//
+// This code is licensed under the MIT License.
+//
+// Permission is hereby granted, free of charge, to any person obtaining a copy
+// of this software and associated documentation files(the "Software"), to deal
+// in the Software without restriction, including without limitation the rights
+// to use, copy, modify, merge, publish, distribute, sublicense, and / or sell
+// copies of the Software, and to permit persons to whom the Software is
+// furnished to do so, subject to the following conditions :
+//
+// The above copyright notice and this permission notice shall be included in
+// all copies or substantial portions of the Software.
+//
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+// THE SOFTWARE.
 package com.microsoft.identity.common.internal.providers.oauth2
 
 import android.app.Activity
 import android.os.Build
-import android.util.Log
 import androidx.credentials.*
 import androidx.credentials.exceptions.*
 import com.microsoft.identity.common.logging.Logger
@@ -27,10 +48,9 @@ class CredentialManagerHandler(private val activity: Activity) {
             Logger.info(methodTag, "Creating passkey with request: $request")
             val createRequest = CreatePublicKeyCredentialRequest(request)
             try {
-                Logger.info(methodTag, "Invoking CredentialManager.createCredential")
-                val response =  mCredMan.createCredential(activity, createRequest) as CreatePublicKeyCredentialResponse
-                Logger.info(methodTag, "Passkey created successfully.")
-                return response
+                return (mCredMan.createCredential(activity, createRequest) as CreatePublicKeyCredentialResponse).also {
+                    Logger.info(methodTag, "Passkey created successfully.")
+                }
             } catch (e: CreateCredentialException) {
                 // For error handling use guidance from https://developer.android.com/training/sign-in/passkeys
                 Logger.error(TAG, "Error creating credential: ErrMessage: ${e.errorMessage}, ErrType: ${e.type}", e)
@@ -51,10 +71,11 @@ class CredentialManagerHandler(private val activity: Activity) {
     suspend fun getPasskey(request: String): GetCredentialResponse {
         val methodTag = "$TAG:getPasskey"
         Logger.info(methodTag, "Getting passkey with request: $request")
-        val getRequest = GetCredentialRequest(listOf(GetPublicKeyCredentialOption(request, null)))
+        val getRequest = GetCredentialRequest(listOf(GetPublicKeyCredentialOption(request)))
         try {
-            Logger.info(methodTag, "Invoking CredentialManager.getCredential")
-            return mCredMan.getCredential(activity, getRequest)
+            return mCredMan.getCredential(activity, getRequest).also {
+                Logger.info(methodTag, "Passkey retrieved successfully.")
+            }
         } catch (e: GetCredentialException) {
             // For error handling use guidance from https://developer.android.com/training/sign-in/passkeys
             Logger.error(TAG, "Error retrieving credential: ${e.message}", e)
