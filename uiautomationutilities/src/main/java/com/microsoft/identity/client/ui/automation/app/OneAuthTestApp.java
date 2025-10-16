edb0f45234bbcc10971c90fee074e1a4c59675c7
fadidurah <fadidurah@microsoft.com>
2025-10-16 16:09:38 -0400

    change oneauthtestapp handle

--- uiautomationutilities/src/main/java/com/microsoft/identity/client/ui/automation/app/OneAuthTestApp.java
+++ uiautomationutilities/src/main/java/com/microsoft/identity/client/ui/automation/app/OneAuthTestApp.java
@@ -24,6 +24,7 @@ package com.microsoft.identity.client.ui.automation.app;
 
 import static com.microsoft.identity.client.ui.automation.utils.CommonUtils.FIND_UI_ELEMENT_TIMEOUT;
 
+import android.os.Build;
 import android.text.TextUtils;
 import android.widget.Button;
 import android.widget.EditText;
@@ -82,7 +83,14 @@ public class OneAuthTestApp extends App implements IFirstPartyApp {
 
     @Override
     public void handleFirstRun() {
-        CommonUtils.grantPackagePermission();
+        if (Build.VERSION.SDK_INT <= Build.VERSION_CODES.P) {
+            CommonUtils.grantPackagePermission();
+        } else {
+            // On later apis the permission prompt is different, we handle the button by text
+            UiAutomatorUtils.handleButtonClickForObjectWithText("Allow access to manage all files");
+            UiAutomatorUtils.pressBack();
+        }
+
         // Because switching the App Configuration will decide whether to truly enable the Broker,
         // it's essential to turn on the Broker beforehand.
         handlePreferBrokerSwitchButton();
