b2dea8e8b5444a3331a6b53dcac6fad1f3c6c94d
Mohit <mchand@microsoft.com>
2025-11-07 13:20:08 -0800

    address comment

--- common4j/src/main/com/microsoft/identity/common/java/opentelemetry/SpanName.java
+++ common4j/src/main/com/microsoft/identity/common/java/opentelemetry/SpanName.java
@@ -74,6 +74,7 @@ public enum SpanName {
     ProcessWebCpAuthorizeUrlRedirect,
     PersistToStorageAsync,
     InstallCertOnWpj,
+
     /**
      * Span name for fetching initial ECS flight configurations.
      */
