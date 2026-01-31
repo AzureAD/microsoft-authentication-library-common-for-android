1bddb26b81595dd2e4f2c36d848df0ca204e5744
p3dr0rv <pedroro@microsoft.com>
2026-01-30 22:16:59 -0800

    Add elapsed_time_keypair_generation attribute to OpenTelemetry spans

--- common4j/src/main/com/microsoft/identity/common/java/opentelemetry/AttributeName.java
+++ common4j/src/main/com/microsoft/identity/common/java/opentelemetry/AttributeName.java
@@ -347,6 +347,11 @@ public enum AttributeName {
     is_new_refresh_token_cred_header_attached,
 
     /**
+     * The time (in milliseconds) spent on generating a keypair.
+     */
+    elapsed_time_keypair_generation,
+
+    /**
      * Indicates the successful method used to generate a keypair.
      */
     key_pair_gen_successful_method,
