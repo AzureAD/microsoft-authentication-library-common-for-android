50bb8e07dd38f50e1588a1595382889496b4730a
p3dr0rv <pedroro@microsoft.com>
2026-05-14 15:53:05 -0700

    Remove content provider strategy addition from DeviceRegistrationIpcStrategiesProvider

--- common/src/main/java/com/microsoft/identity/deviceregistration/DeviceRegistrationIpcStrategiesProvider.kt
+++ common/src/main/java/com/microsoft/identity/deviceregistration/DeviceRegistrationIpcStrategiesProvider.kt
@@ -68,13 +68,13 @@ open class DeviceRegistrationIpcStrategiesProvider @JvmOverloads constructor(
         val contentProviderStrategy = ContentProviderStrategy(context, components)
         if (contentProviderStrategy.isSupportedByTargetedBroker(activeBrokerPackageName)) {
             Logger.info(methodTag, "Adding primary strategy: ${ContentProviderStrategy::class.java.simpleName}")
-            strategies.add(contentProviderStrategy)
+            //strategies.add(contentProviderStrategy)
         }
 
         val boundServiceStrategy = BoundServiceStrategy<IDeviceRegistrationService>(
             DeviceRegistrationServiceClient(context)
         )
-        if (boundServiceStrategy.isSupportedByTargetedBroker(activeBrokerPackageName) && supportsBoundService) {
+        if (boundServiceStrategy.isSupportedByTargetedBroker(activeBrokerPackageName)) {
             Logger.info(methodTag, "Adding fallback strategy: ${BoundServiceStrategy::class.java.simpleName}")
             strategies.add(boundServiceStrategy)
         }
